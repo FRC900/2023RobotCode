810117d17a7a550a5ecb7300e2d5804f0b5afa54
Kevin Jaget <kjaget@gmail.com>
2018-10-03 16:27:09 -0400

    Add some basic error checking to cross_build.sh

--- zebROS_ws/cross_build.sh
+++ zebROS_ws/cross_build.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+if [ -z $ROS_ROOT ]; then
+	echo "Sourcing cross build environment"
+	source /usr/arm-frc-linux-gnueabi/opt/ros/kinetic/setup.bash
+elif [[ ! $ROS_ROOT = "/usr/arm-frc-linux-gnueabi/opt/ros/kinetic/share/ros" ]]; then
+	echo "ROS is not configured for a cross build (maybe set up for a native build instead?)"
+	echo "Run ./cross_build.sh in a new terminal window"
+	exit 0
+fi
+
 catkin_make_isolated --install --use-ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=`pwd`/rostoolchain.cmake -DCATKIN_ENABLE_TESTING=OFF "$@"
