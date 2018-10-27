d376f7f8c912034be3ea71366186d4032c0edcb0
Vignesh Dhanasekaran <v.dhanasekaran0@gmail.com>
2018-10-27 15:43:24 -0400

    Adding some dummy test cases

--- zebROS_ws/src/swerve_math/test/mathtest.cpp
+++ zebROS_ws/src/swerve_math/test/mathtest.cpp
@@ -1,8 +1,20 @@
 #include <math.h>
 #include "swerve_math/900Math.h"
 #include <gtest/gtest.h>
+#include <gtest/gmock.h>
 
-int main(int argc, char ** argv) {
+TEST(x, y)
+{
+    EXPECT_EQ(2, 2);
+}
+
+TEST(DummyTest, Dummy)
+{
+    ASSERT_EQ(3, 3);
+}
+
+int main(int argc, char ** argv) 
+{
     testing::InitGoogleTest(&argc, argv);
 
     ros::init(argc, argv, "test");
