2159804bde5e7ccac087e6d40986c44a7b856941
Kevin Jaget <kjaget@gmail.com>
2018-11-02 22:27:57 -0400

    Fix ROSJetsonMaster.sh ROS_IP

--- zebROS_ws/ROSJetsonMaster.sh
+++ zebROS_ws/ROSJetsonMaster.sh
@@ -13,7 +13,7 @@ elif [ -f /home/ubuntu/2018Offseason/zebROS_ws/devel/setup.bash ] ; then
     source /opt/ros/kinetic/setup.bash
     source /home/ubuntu/2018Offseason/zebROS_ws/devel/setup.bash
     #export ROS_IP=10.9.0.8
-    export ROS_IP=`ip route get 10.9.0.1 | head -1 | cut -d ' ' -f 8`
+    export ROS_IP=`ip route get 10.9.0.1 | head -1 | cut -d ' ' -f 6`
 elif [ -f /home/ryan/2018Offseason/zebROS_ws/devel/setup.bash ] ; then
     # Jetson-specific configuration
     echo "Sourcing Ryan / native Linux environment"
@@ -35,7 +35,7 @@ else
     echo "Unknown environment! Trying to proceed anyway using local environment."
     source /opt/ros/kinetic/setup.bash
     source $HOME/2018Offseason/zebROS_ws/devel/setup.bash
-    export ROS_IP=`ip route get 10.9.0.1 | head -1 | cut -d ' ' -f 8`
+    export ROS_IP=`ip route get 10.9.0.1 | head -1 | cut -d ' ' -f 6`
 fi
 
 # Common configuration
