0bef84a9a4bc6f93aade06eb0a8b2fe8bd36c359
Eric Blau <eblau@eblau.com>
2018-12-18 23:50:07 +0000

    Added a check that makes sure it stays at the same result 10 in a row before it outputs for testhascube, to prevent noise

--- zebROS_ws/src/auto_preseason/scripts/auto_example.py
+++ zebROS_ws/src/auto_preseason/scripts/auto_example.py
@@ -20,9 +20,11 @@ class Init(smach.State):
             return 'failure'
         return 'success'
 
-
 # define state Bar
 class TestHasCube(smach.State):
+    streaklen = 0 #how many times has it done same output in a row
+    streaktype = True #what kind of streak is it on
+
     def __init__(self):
         smach.State.__init__(self, outcomes=['testTrue', 'testFalse'])
 
@@ -30,18 +32,24 @@ class TestHasCube(smach.State):
         self.sub = rospy.Subscriber('/frcrobot/joint_states',JointState,self.callback)
         
         self.test_result = "default"  #initialize variable to store received msgs
-
+        
     def callback(self,msg):
         sensor_index = 0
+
         for i in range(len(msg.position)):
             if msg.name[i] == "intake_line_break":
                 sensor_index = i
 
         self.test_result = msg.position[sensor_index]
 
+        if self.streaktype == (self.test_result==1.0):
+            self.streaklen += 1
+        else:
+            self.streaklen = 0
+            self.streaktype = self.test_result==1.0
     def execute(self, userdata):
-        rospy.loginfo("testhascube "+str(self.test_result))
-        
+        while self.streaklen < 10:
+            rospy.loginfo("testhascube "+str(self.test_result)+" "+str(self.streaktype)+str(self.streaklen))
         if self.test_result == 1.0: #line_break_sensor:
             return 'testTrue'
         else:
