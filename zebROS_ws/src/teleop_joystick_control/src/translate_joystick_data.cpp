2a4f27fd39d9b33a1d18b549e7c56fe0012dab41
Caleb Krellwitz <cwkrellwitz@gmail.com>
2019-01-13 21:32:07 +0000

    Fix press/release (change msg_last to processed_msg_last) and add Dpad press/release.

--- zebROS_ws/src/teleop_joystick_control/src/translate_joystick_data.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/translate_joystick_data.cpp
@@ -2,7 +2,7 @@
 #include <sensor_msgs/Joy.h>
 #include <ros_control_boilerplate/JoystickState.h>
 
-sensor_msgs::Joy msg_last;
+ros_control_boilerplate::JoystickState processed_msg_last;
 ros_control_boilerplate::JoystickState processed_msg;
 
 bool initialized = false;
@@ -11,91 +11,99 @@ void rawDataCB(const sensor_msgs::Joy::ConstPtr &msg)
 {
 	if(initialized == false)
 	{
-		msg_last = *msg;
+		processed_msg_last = processed_msg;
 		initialized = true;
 	}
 
-	processed_msg.rightStickY = msg->axes[4];
-	processed_msg.rightStickX = msg->axes[3];
-	processed_msg.leftStickY = msg->axes[1];
 	processed_msg.leftStickX = msg->axes[0];
+	processed_msg.leftStickY = msg->axes[1];
 	processed_msg.leftTrigger = msg->axes[2];
+	processed_msg.rightStickX = msg->axes[3];
+	processed_msg.rightStickY = msg->axes[4];
 	processed_msg.rightTrigger = msg->axes[5];
 
 	if(msg->axes[6] > 0)
 	{
 		processed_msg.directionLeftButton = true;
 	}
-	
+
 	else
 	{
 		processed_msg.directionLeftButton = false;
 	}
-	
+
 	if(msg->axes[6] < 0)
 	{
 		processed_msg.directionRightButton = true;
-	}	
-	
+	}
+
 	else
 	{
 		processed_msg.directionRightButton = false;
 	}
-	
+
 	if(msg->axes[7] > 0)
 	{
 		processed_msg.directionUpButton = true;
 	}
-	
+
 	else
 	{
 		processed_msg.directionUpButton = false;
-	}	
-	
+	}
+
 	if(msg->axes[7] < 0)
 	{
 		processed_msg.directionDownButton = true;
 	}
-	
+
 	else
 	{
 			processed_msg.directionDownButton = false;
 	}
 
+	processed_msg.buttonAButton = msg->buttons[0];
+	processed_msg.buttonBButton = msg->buttons[1];
 	processed_msg.buttonXButton = msg->buttons[2];
 	processed_msg.buttonYButton = msg->buttons[3];
 	processed_msg.bumperLeftButton = msg->buttons[4];
 	processed_msg.bumperRightButton = msg->buttons[5];
-	processed_msg.stickLeftButton = msg->buttons[9];
-	processed_msg.stickRightButton = msg->buttons[10];
-	processed_msg.buttonAButton = msg->buttons[0];
-	processed_msg.buttonBButton = msg->buttons[1];
 	processed_msg.buttonBackButton = msg->buttons[6];
 	processed_msg.buttonStartButton = msg->buttons[7];
+	processed_msg.stickLeftButton = msg->buttons[9];
+	processed_msg.stickRightButton = msg->buttons[10];
 
-	processed_msg.buttonAPress = !msg_last.buttons[0] && msg->buttons[0];
-	processed_msg.buttonBPress = !msg_last.buttons[1] && msg->buttons[1];
-	processed_msg.buttonXPress = !msg_last.buttons[2] && msg->buttons[2];
-	processed_msg.buttonYPress = !msg_last.buttons[3] && msg->buttons[3];
-	processed_msg.bumperLeftPress = !msg_last.buttons[4] && msg->buttons[4];
-	processed_msg.bumperRightPress = !msg_last.buttons[5] && msg->buttons[5];
-	processed_msg.buttonBackPress = !msg_last.buttons[6] && msg->buttons[6];
-	processed_msg.buttonStartPress = !msg_last.buttons[7] && msg->buttons[7];
-	processed_msg.stickLeftPress = !msg_last.buttons[9] && msg->buttons[9];
-	processed_msg.stickRightPress = !msg_last.buttons[10] && msg->buttons[10];
-	
-	processed_msg.buttonARelease = msg_last.buttons[0] && !msg->buttons[0];
-	processed_msg.buttonBRelease = msg_last.buttons[1] && !msg->buttons[1];
-	processed_msg.buttonXRelease = msg_last.buttons[2] && !msg->buttons[2];
-	processed_msg.buttonYRelease = msg_last.buttons[3] && !msg->buttons[3];
-	processed_msg.bumperLeftRelease = msg_last.buttons[4] && !msg->buttons[4];
-	processed_msg.bumperRightRelease = msg_last.buttons[5] && !msg->buttons[5];
-	processed_msg.buttonBackRelease = msg_last.buttons[6] && !msg->buttons[6];
-	processed_msg.buttonStartRelease = msg_last.buttons[7] && !msg->buttons[7];
-	processed_msg.stickLeftRelease = msg_last.buttons[9] && !msg->buttons[9];
-	processed_msg.stickRightRelease = msg_last.buttons[10] && !msg->buttons[10];
-	
-	msg_last = *msg;
+	processed_msg.buttonAPress = !processed_msg_last.buttonAButton && processed_msg.buttonAButton;
+	processed_msg.buttonBPress = !processed_msg_last.buttonBButton && processed_msg.buttonBButton;
+	processed_msg.buttonXPress = !processed_msg_last.buttonXButton && processed_msg.buttonXButton;
+	processed_msg.buttonYPress = !processed_msg_last.buttonYButton && processed_msg.buttonYButton;
+	processed_msg.bumperLeftPress = !processed_msg_last.bumperLeftButton && processed_msg.bumperLeftButton;
+	processed_msg.bumperRightPress = !processed_msg_last.bumperRightButton && processed_msg.bumperRightButton;
+	processed_msg.buttonBackPress = !processed_msg_last.buttonBackButton && processed_msg.buttonBackButton;
+	processed_msg.buttonStartPress = !processed_msg_last.buttonStartButton && processed_msg.buttonStartButton;
+	processed_msg.stickLeftPress = !processed_msg_last.stickLeftButton && processed_msg.stickLeftButton;
+	processed_msg.stickRightPress = !processed_msg_last.stickRightButton && processed_msg.stickRightButton;
+	processed_msg.directionLeftPress = !processed_msg_last.directionLeftButton && processed_msg.directionLeftButton;
+	processed_msg.directionRightPress = !processed_msg_last.directionRightButton && processed_msg.directionRightButton;
+	processed_msg.directionUpPress = !processed_msg_last.directionUpButton && processed_msg.directionUpButton;
+	processed_msg.directionUpPress = !processed_msg_last.directionDownButton && processed_msg.directionDownButton;
+
+	processed_msg.buttonARelease = processed_msg_last.buttonAButton && !processed_msg.buttonAButton;
+	processed_msg.buttonBRelease = processed_msg_last.buttonBButton && !processed_msg.buttonBButton;
+	processed_msg.buttonXRelease = processed_msg_last.buttonXButton && !processed_msg.buttonXButton;
+	processed_msg.buttonYRelease = processed_msg_last.buttonYButton && !processed_msg.buttonYButton;
+	processed_msg.bumperLeftRelease = processed_msg_last.bumperLeftButton && !processed_msg.bumperLeftButton;
+	processed_msg.bumperRightRelease = processed_msg_last.bumperRightButton && !processed_msg.bumperRightButton;
+	processed_msg.buttonBackRelease = processed_msg_last.buttonBackButton && !processed_msg.buttonBackButton;
+	processed_msg.buttonStartRelease = processed_msg_last.buttonStartButton && !processed_msg.buttonStartButton;
+	processed_msg.stickLeftRelease = processed_msg_last.stickLeftButton && !processed_msg.stickLeftButton;
+	processed_msg.stickRightRelease = processed_msg_last.stickRightButton && !processed_msg.stickRightButton;
+	processed_msg.directionLeftRelease = processed_msg_last.directionLeftButton && !processed_msg.directionLeftButton;
+	processed_msg.directionRightRelease = processed_msg_last.directionRightButton && !processed_msg.directionRightButton;
+	processed_msg.directionUpRelease = processed_msg_last.directionUpButton && !processed_msg.directionUpButton;
+	processed_msg.directionDownRelease = processed_msg_last.directionDownButton && !processed_msg.directionDownButton;
+
+	processed_msg_last = processed_msg;
 }
 
 int main(int argc, char ** argv)
