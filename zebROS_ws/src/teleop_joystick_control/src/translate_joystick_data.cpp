598bdad043ceda4aaf2a6398fb7347f9a23eedd5
Caleb Krellwitz <cwkrellwitz@gmail.com>
2019-01-13 01:30:41 +0000

    Add press and release to functionality. Make translator launch regardless of hw_or_sim.

--- zebROS_ws/src/teleop_joystick_control/src/translate_joystick_data.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/translate_joystick_data.cpp
@@ -2,10 +2,19 @@
 #include <sensor_msgs/Joy.h>
 #include <ros_control_boilerplate/JoystickState.h>
 
+sensor_msgs::Joy msg_last;
 ros_control_boilerplate::JoystickState processed_msg;
+
+bool initialized = false;
 
 void rawDataCB(const sensor_msgs::Joy::ConstPtr &msg)
 {
+	if(initialized == false)
+	{
+		msg_last = *msg;
+		initialized = true;
+	}
+
 	processed_msg.rightStickY = msg->axes[4];
 	processed_msg.rightStickX = msg->axes[3];
 	processed_msg.leftStickY = msg->axes[1];
@@ -63,6 +72,30 @@ void rawDataCB(const sensor_msgs::Joy::ConstPtr &msg)
 	processed_msg.buttonBButton = msg->buttons[1];
 	processed_msg.buttonBackButton = msg->buttons[6];
 	processed_msg.buttonStartButton = msg->buttons[7];
+
+	processed_msg.buttonAPress = !msg_last.buttons[0] && msg->buttons[0];
+	processed_msg.buttonBPress = !msg_last.buttons[1] && msg->buttons[1];
+	processed_msg.buttonXPress = !msg_last.buttons[2] && msg->buttons[2];
+	processed_msg.buttonYPress = !msg_last.buttons[3] && msg->buttons[3];
+	processed_msg.bumperLeftPress = !msg_last.buttons[4] && msg->buttons[4];
+	processed_msg.bumperRightPress = !msg_last.buttons[5] && msg->buttons[5];
+	processed_msg.buttonBackPress = !msg_last.buttons[6] && msg->buttons[6];
+	processed_msg.buttonStartPress = !msg_last.buttons[7] && msg->buttons[7];
+	processed_msg.stickLeftPress = !msg_last.buttons[9] && msg->buttons[9];
+	processed_msg.stickRightPress = !msg_last.buttons[10] && msg->buttons[10];
+	
+	processed_msg.buttonARelease = msg_last.buttons[0] && !msg->buttons[0];
+	processed_msg.buttonBRelease = msg_last.buttons[1] && !msg->buttons[1];
+	processed_msg.buttonXRelease = msg_last.buttons[2] && !msg->buttons[2];
+	processed_msg.buttonYRelease = msg_last.buttons[3] && !msg->buttons[3];
+	processed_msg.bumperLeftRelease = msg_last.buttons[4] && !msg->buttons[4];
+	processed_msg.bumperRightRelease = msg_last.buttons[5] && !msg->buttons[5];
+	processed_msg.buttonBackRelease = msg_last.buttons[6] && !msg->buttons[6];
+	processed_msg.buttonStartRelease = msg_last.buttons[7] && !msg->buttons[7];
+	processed_msg.stickLeftRelease = msg_last.buttons[9] && !msg->buttons[9];
+	processed_msg.stickRightRelease = msg_last.buttons[10] && !msg->buttons[10];
+	
+	msg_last = *msg;
 }
 
 int main(int argc, char ** argv)
