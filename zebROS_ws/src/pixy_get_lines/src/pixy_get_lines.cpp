6ab1d216fae46e35862cb6ab81164b4044bf30d3
Kevin Jaget <kjaget@gmail.com>
2019-01-16 13:03:58 -0500

    Initial commit for ROS node for pixy line follower code

---
+++ zebROS_ws/src/pixy_get_lines/src/pixy_get_lines.cpp
@@ -0,0 +1,89 @@
+#include "ros/ros.h"
+
+#include "libpixyusb2.h"
+#include "pixy_get_lines/PixyLine.h"
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "pixy_get_lines");
+	ros::NodeHandle n;
+	int rc;
+
+	Pixy2 pixy;
+	rc = pixy.init();
+	if (rc < 0)
+	{
+		ROS_ERROR("Error");
+		ROS_ERROR_STREAM("pixy.init() returned " << rc);
+		return rc;
+	}
+	ROS_INFO("pixy.init() success\n");
+
+#if 0
+	rc = pixy.getVersion();
+	if (rc < 0)
+	{
+		ROS_ERROR_STREAM("pixy.getVersion() returned " << rc);
+		return rc;
+	}
+	pixy.version->print();
+#endif
+
+	// Set Pixy2 to line feature identification program //
+	pixy.changeProg("line");
+
+	ros::Publisher pub = n.advertise<pixy_get_lines::PixyLine>("pixy_line", 1);
+
+	ros::Rate r(100);
+	while(ros::ok())
+	{
+		// Query Pixy for line features //
+		pixy.line.getAllFeatures();
+		pixy_get_lines::PixyLine msg;
+
+		for (uint8_t i= 0; i < pixy.line.numVectors; ++i)
+		{
+			pixy_get_lines::Vector vector;
+
+			vector.x0 = pixy.line.vectors[i].m_x0;
+			vector.y0 = pixy.line.vectors[i].m_y0;
+			vector.x1 = pixy.line.vectors[i].m_x1;
+			vector.y1 = pixy.line.vectors[i].m_y1;
+			vector.index = pixy.line.vectors[i].m_index;
+			vector.flags = pixy.line.vectors[i].m_flags;
+
+			msg.vectors.push_back(vector);
+		}
+
+		for (uint8_t i = 0; i < pixy.line.numIntersections; ++i)
+		{
+			pixy_get_lines::Intersection intersection;
+
+			intersection.x = pixy.line.intersections[i].m_x;
+			intersection.y = pixy.line.intersections[i].m_y;
+			intersection.n = pixy.line.intersections[i].m_n;
+			for (uint8_t n = 0; n < pixy.line.intersections[i].m_n; ++n)
+			{
+				pixy_get_lines::IntersectionLine intersection_line;
+				intersection_line.index =  pixy.line.intersections[i].m_intLines[n].m_index;
+				intersection_line.angle =  pixy.line.intersections[i].m_intLines[n].m_angle;
+				intersection.int_lines.push_back(intersection_line);
+			}
+			msg.intersections.push_back(intersection);
+		}
+
+		for (uint8_t i = 0; i < pixy.line.numBarcodes; ++i)
+		{
+			pixy_get_lines::Barcode barcode;
+			barcode.x = pixy.line.barcodes[i].m_x;
+			barcode.y = pixy.line.barcodes[i].m_y;
+			barcode.flags = pixy.line.barcodes[i].m_flags;
+			barcode.code = pixy.line.barcodes[i].m_code;
+			msg.barcodes.push_back(barcode);
+		}
+		pub.publish(msg);
+		r.sleep();
+	}
+
+	return EXIT_SUCCESS;
+}
