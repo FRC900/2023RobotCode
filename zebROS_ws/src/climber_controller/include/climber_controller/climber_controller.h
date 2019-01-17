a26387b6862831bbcd680cc350a997c6267c0524
o-fugi <o.fugikawa@gmail.com>
2019-01-17 02:43:25 +0000

    wrote climber controller code, needs sim testing

--- zebROS_ws/src/climber_controller/include/climber_controller/climber_controller.h
+++ zebROS_ws/src/climber_controller/include/climber_controller/climber_controller.h
@@ -1,36 +1,30 @@
-#ifndef CLIMBER_CONTROLLER
-#define CLIMBER_CONTROLLER
+#ifndef INTAKE_CONTROLLER
+#define INTAKE_CONTROLLER
 
 #include <ros/ros.h>
 #include <vector>
 #include <hardware_interface/joint_state_interface.h> //other than talon data
 #include <hardware_interface/joint_command_interface.h>
-#include <realtime_tools/realtime_publisher.h> //code for real-time buffer - stop multple things writing to same variable at same time
+#include <realtime_tools/realtime_buffer.h> //code for real-time buffer - stop multple things writing to same variable at same time
 #include <boost/shared_ptr.hpp>
 #include <controller_interface/multi_interface_controller.h>
 #include <controller_interface/controller.h> //for writing controllers
-#include <talon_interface/talon_state_interface.h> // "
-#include <talon_controllers/talon_controller.h> // "
-#include <talon_controllers/talon_controller_interface.h> // "
 #include <atomic>
 #include <std_msgs/Float64.h>
 #include <pluginlib/class_list_macros.h> //to compile as a controller
 #include <sensor_msgs/JointState.h>
 #include <std_msgs/Bool.h>
+#include <std_srvs/SetBool.h>
 
 namespace climber_controller
 {
-//this is the actual controller, so it stores all of the  update() functions and the actual handle from the joint interface
-//if it was only one type, controller_interface::Controller<TalonCommandInterface> here
-class ClimberController : public controller_interface::MultiInterfaceController<hardware_interface::TalonCommandInterface, hardware_interface::JointStateInterface, hardware_interface::PositionJointInterface>
+class ClimberController : public controller_interface::Controller<hardware_interface::PositionJointInterface>
 {
         public:
             ClimberController()
             {
             }
 
-            //should this be hardware_interface::TalonCommandInterface instead? What's the reason to import RobotHW then get CommandInterface from that instead of just importing TalonCommandIface?
-            //answer to my question: the TalonCommandInterface is passed in if it's not a multiInterfaceController, and just one kind of joint is made!
             virtual bool init(hardware_interface::RobotHW *hw,
                               ros::NodeHandle             &root_nh,
                               ros::NodeHandle             &controller_nh);
@@ -38,19 +32,16 @@ class ClimberController : public controller_interface::MultiInterfaceController<
             virtual void update(const ros::Time & time, const ros::Duration& period);
             virtual void stopping(const ros::Time &time);
 
-            /*virtual bool cmdService(climber_controller::ClimberSrv::Request &req,
-					                climber_controller::ClimberSrv::Response &res);*/
+            virtual bool activateSrv(std_srvs::SetBool::Request &req,
+					                 std_srvs::SetBool::Response &/*res*/);
 
         private:
             std::vector<std::string> joint_names_; //still not used, but we might have to for config file things?
-            talon_controllers::TalonPercentOutputControllerInterface intake_joint_; //interface for the talon joint
-            hardware_interface::JointHandle intake_in_; //interface for the in/out solenoid joint
+            hardware_interface::JointHandle climber_in_; //interface for the in/out solenoid joint
 
-            realtime_tools::RealtimeBuffer<double> spin_command_; //this is the buffer for percent output commands to be published
-            realtime_tools::RealtimeBuffer<double> intake_in_cmd_; //buffer for in/out commands
-            realtime_tools::RealtimeBuffer<double> timeout_; //buffer for timeout commands
+            realtime_tools::RealtimeBuffer<double> climber_in_cmd_; //buffer for in/out commands
 
-            ros::ServiceServer climber_service_; //service for receiving commands
+            ros::ServiceServer service_command_; //service for receiving commands
 }; //class
 
 } //namespace
--- zebROS_ws/src/climber_controller/src/climber_controller.cpp
+++ zebROS_ws/src/climber_controller/src/climber_controller.cpp
@@ -7,62 +7,33 @@ bool ClimberController::init(hardware_interface::RobotHW *hw,
                                                         ros::NodeHandle                 &root_nh,
                                                         ros::NodeHandle                 &controller_nh)
 {
-    hardware_interface::TalonCommandInterface *const talon_command_iface = hw->get<hardware_interface::TalonCommandInterface>();
     hardware_interface::PositionJointInterface *const pos_joint_iface = hw->get<hardware_interface::PositionJointInterface>();
-	/*
-    intake_in_ = pos_joint_iface->getHandle("clamp");
-    //read intake name from config file
-    XmlRpc::XmlRpcValue intake_params;
-    if (!controller_nh.getParam("intake_joint", intake_params))
-    {
-        ROS_ERROR_STREAM("Can not read intake name");
-        return false;
-    }
-    //initialize joint with that name
-    if (!intake_joint_.initWithNode(talon_command_iface, nullptr, controller_nh, intake_params))
-    {
-        ROS_ERROR("Cannot initialize intake joint!");
-        return false;
-    }
-    else
-    {
-        ROS_INFO("Initialized intake joint!");
-    }
-	*/
 
-    //service_command_ = controller_nh.advertiseService("intake_command", &ClimberController::cmdService, this);
+    climber_in_ = pos_joint_iface->getHandle("climber");
+
+    service_command_ = controller_nh.advertiseService("climber_command", &ClimberController::activateSrv, this);
 
     return true;
 }
 
 void ClimberController::starting(const ros::Time &/*time*/) {
+    //climber_in_.setCommand(0);
 }
 
 void ClimberController::update(const ros::Time &time, const ros::Duration &period) {
-    /*double spin_command = *(spin_command_.readFromRT());
-    bool intake_in_cmd = *(intake_in_cmd_.readFromRT());
-    double intake_in_cmd_double;
-    if(intake_in_cmd == true) {
-        //ROS_WARN("intake in");
-        intake_in_cmd_double = -1;
-    }
-    else if (intake_in_cmd == false) {
-        intake_in_cmd_double = 1;
-        //ROS_WARN("intake out");
-    }
-    intake_joint_.setCommand(spin_command); // set the command to the spinny part of the intake
-    intake_in_.setCommand(intake_in_cmd_double); // set the in/out command to the clampy part of the intake
-	*/
+    bool climber_in_cmd = *(climber_in_cmd_.readFromRT());
+    double climber_in_cmd_double;
+
+    climber_in_.setCommand(climber_in_cmd_double); // set the in/out command to the clampy part of the climber
 }
 
 void ClimberController::stopping(const ros::Time &time) {
 }
-/*
-bool ClimberController::cmdService(climber_controller::ClimberSrv::Request &req, climber_controller::ClimberSrv::Response &response) {
+
+bool ClimberController::activateSrv(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &/*response*/) {
     if(isRunning())
     {
-        spin_command_.writeFromNonRT(req.power); //take the service request for a certain amount of power (-1 to 1) and write it to the command variable
-        intake_in_cmd_.writeFromNonRT(req.intake_in); //take the service request for in/out (true/false???) and write to a command variable
+        climber_in_cmd_.writeFromNonRT(req.data);
     }
     else
     {
@@ -71,7 +42,7 @@ bool ClimberController::cmdService(climber_controller::ClimberSrv::Request &req,
     }
     return true;
 }
-*/
+
 }//namespace
 
 //DON'T FORGET TO EXPORT THE CLASS SO CONTROLLER_MANAGER RECOGNIZES THIS AS A TYPE
