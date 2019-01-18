ab5924f7379839521ac4d7800b1e22fc5e487c5a
Adam <adamkosinski631@gmail.com>
2019-01-18 03:48:21 +0000

    Cargo intake controller works in sim.

--- zebROS_ws/src/cargo_intake_controller/src/cargo_intake_controller.cpp
+++ zebROS_ws/src/cargo_intake_controller/src/cargo_intake_controller.cpp
@@ -9,16 +9,12 @@ bool CargoIntakeController::init(hardware_interface::RobotHW *hw,
 {
     hardware_interface::TalonCommandInterface *const talon_command_iface = hw->get<hardware_interface::TalonCommandInterface>();
     hardware_interface::PositionJointInterface *const pos_joint_iface = hw->get<hardware_interface::PositionJointInterface>();
-	
-    //read cargo intake actuator name from config file
-    XmlRpc::XmlRpcValue cargo_intake_actuator_params;
-    if (!controller_nh.getParam("cargo_intake_actuator_joint", cargo_intake_actuator_params))
-    {
-        ROS_ERROR_STREAM("Can not read cargo intake actuator name");
-        return false;
-    }
 
-    //read cargo intake name from config file
+	//initialize cargo intake arm joint (pneumatic piston that controls up/down of arm)
+	cargo_intake_arm_joint_ = pos_joint_iface->getHandle("cargo_intake_arm_joint");
+	
+	//initialize cargo_intake_joint (spinny thing)
+	//read cargo intake name from config file
     XmlRpc::XmlRpcValue cargo_intake_params;
     if (!controller_nh.getParam("cargo_intake_joint", cargo_intake_params))
     {
@@ -26,18 +22,7 @@ bool CargoIntakeController::init(hardware_interface::RobotHW *hw,
         return false;
     }
 
-    //initialize cargo actuator
-    if (!cargo_intake_actuator_joint_.initWithNode(talon_command_iface, nullptr, controller_nh, cargo_intake_actuator_params))
-    {
-        ROS_ERROR("Cannot initialize cargo intake actuator joint!");
-        return false;
-    }
-    else
-    {
-        ROS_INFO("Initialized cargo intake actuator joint!");
-    }
-
-    //initialize cargo intake joint
+	//initialize cargo intake joint
     if (!cargo_intake_joint_.initWithNode(talon_command_iface, nullptr, controller_nh, cargo_intake_params))
     {
         ROS_ERROR("Cannot initialize cargo intake joint!");
@@ -56,27 +41,27 @@ bool CargoIntakeController::init(hardware_interface::RobotHW *hw,
 
 void CargoIntakeController::starting(const ros::Time &/*time*/) {
     cargo_intake_joint_.setCommand(0.0); // set the command to the spinny part of the intake
-	cargo_actuator_setCommand(-1); // set the command to the up/down part of the intake
+	cargo_intake_arm_joint_.setCommand(-1); // set the command to the up/down part of the intake
 }
 
 void CargoIntakeController::update(const ros::Time &time, const ros::Duration &period) {
-	//  cargo_intake_joint_.setCommand( * spin_command_.readFromRT() ); // set the command to the spinny intake bar
-	double spin_cmd = *(spin_cmd_.readFromRT());
-	bool intake_in_cmd = *(intake_in_cmd_.readFromRT());
-   	double intake_in_cmd_double;
-	if(intake_in_cmd == true) {
-		ROS_WARN("intake in");
-		intake_in_cmd_double = -1;
+	//process input for the up/down part of the intake (pneumatic piston)
+	bool intake_arm_command = *(intake_arm_command_.readFromRT());
+   	double intake_arm_command_double; //to store processed input
+	if(intake_arm_command == true) {
+		ROS_WARN("cargo intake arm command: -1");
+		intake_arm_command_double = -1;
 	}
-	else if (intake_in_cmd == false) {
-		intake_in_cmd_double = 1;
-		ROS_WARN("intake out");
+	else if (intake_arm_command == false) {
+		intake_arm_command_double = 1;
+		ROS_WARN("cargo intake arm command: 1");
 	}
-
-	ROS_INFO_STREAM("spin command = " << spin_command << "; intake_in = " << intake_in_cmd);
-	intake_joint_.setCommand(spin_command); // set the command to the spinny part of the intake
-	intake_in_.setCommand(intake_in_cmd_double); // set the in/out command to the up/down part of the intake
-	intake_in_.setCommand(intake_in_cmd_double); // set the in/out command to the up/down part of the intake
+	
+	//read spin command
+	double spin_command = *(spin_command_.readFromRT());
+	ROS_INFO_STREAM("cargo spin command = " << spin_command << "; intake_arm = " << intake_arm_command);
+	cargo_intake_joint_.setCommand(spin_command); // set the command to the spinny part of the intake
+	cargo_intake_arm_joint_.setCommand(intake_arm_command_double); // set the in/out command to the up/down part of the intake
 }
 
 void CargoIntakeController::stopping(const ros::Time &time) {
@@ -86,7 +71,7 @@ bool CargoIntakeController::cmdService(cargo_intake_controller::CargoIntakeSrv::
     if(isRunning())
     {
         spin_command_.writeFromNonRT(req.power); //take the service request for a certain amount of power (-1 to 1) and write it to the command variable
-		intake_in_cmd_.writeFromNonRT(req.intake_in); //take the service request for in/out (true/false???) and write to a command variable
+		intake_arm_command_.writeFromNonRT(req.intake_arm); //take the service request for in/out (true/false???) and write to a command variable
     }
     else
     {
