0582c67b8017aabb32bf10d3455232b3c88062d2
E-Danforth <edward.w.danforth@gmail.com>
2019-01-22 01:22:45 +0000

    add callback functions

--- zebROS_ws/src/teleop_joystick_control/src/teleop_joystick_comp.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/teleop_joystick_comp.cpp
@@ -101,6 +101,13 @@ void evaluateCommands(const ros_control_boilerplate::JoystickState::ConstPtr &Jo
 		sendRobotZero = false;
 	}
 }
+void callback(const ros::MessageEvent<ros_control_boilerplate::JoystickStates const>& event)
+{
+const ros::M_strings& header = event.getConnectionHeader();
+std::string topic = header.at("topic");
+
+const ros_control_boilerplate:JoystickStates::ConstPtr& msg = event.getMessage();
+}
 
 int main(int argc, char **argv)
 {
@@ -110,6 +117,7 @@ int main(int argc, char **argv)
 	navX_angle = M_PI / 2;
 
 	ros::Subscriber joystick_sub  = n.subscribe("joystick_states", 1, &evaluateCommands);
+	ros::Subscriber joystick_sub1  = n.subscribe("joystick_states1", 1, &evaluateCommands);
 
 	std::map<std::string, std::string> service_connection_header;
 	service_connection_header["tcp_nodelay"] = "1";
