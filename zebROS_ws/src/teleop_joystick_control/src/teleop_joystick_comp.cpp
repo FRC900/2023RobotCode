e72c8af38cd10bfbec4294935753032470515e64
Caleb Krellwitz <cwkrellwitz@gmai.com>
2019-01-24 21:08:35 +0000

    Add logic in evaluateCommands to read only correct joystick

--- zebROS_ws/src/teleop_joystick_control/src/teleop_joystick_comp.cpp
+++ zebROS_ws/src/teleop_joystick_control/src/teleop_joystick_comp.cpp
@@ -27,6 +27,10 @@ rate_limiter::RateLimiter right_stick_y_rate_limit(-1.0, 1.0, drive_rate_limit_t
 rate_limiter::RateLimiter left_trigger_rate_limit(-1.0, 1.0, drive_rate_limit_time);
 rate_limiter::RateLimiter right_trigger_rate_limit(-1.0, 1.0, drive_rate_limit_time);
 
+int i;
+
+std::vector <ros_control_boilerplate::JoystickState> joystick_states;
+
 void dead_zone_check(double &val1, double &val2)
 {
 	if (fabs(val1) <= dead_zone && fabs(val2) <= dead_zone)
@@ -67,7 +71,6 @@ void evaluateCommands(const ros::MessageEvent<frc_msgs::JoystickState const>& ev
 
 	const ros::M_string &header = event.getConnectionHeader();
 
-<<<<<<< f04b764d37b265a03c9cc6769a08a24321066ca9
 	for(bool msg_assign = false; msg_assign == false; i++)
 	{
 		if(topic == topic_array[i])
