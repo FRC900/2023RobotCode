d817393b5095d5c7c60c891c1d6c2069b72d487a
Adam <adamkosinski631@gmail.com>
2019-01-24 03:37:49 +0000

    Added 2nd joint to cargo outtake controller

--- zebROS_ws/src/cargo_outtake_controller/src/cargo_outtake_controller.cpp
+++ zebROS_ws/src/cargo_outtake_controller/src/cargo_outtake_controller.cpp
@@ -3,54 +3,70 @@
 namespace cargo_outtake_controller
 {
 
-bool CargoOuttakeController::init(hardware_interface::RobotHW *hw,
-                                                        ros::NodeHandle                 &root_nh,
-                                                        ros::NodeHandle                 &controller_nh)
-{
-    hardware_interface::PositionJointInterface *const pos_joint_iface = hw->get<hardware_interface::PositionJointInterface>();
+	bool CargoOuttakeController::init(hardware_interface::PositionJointInterface *hw,
+			ros::NodeHandle                 &root_nh,
+			ros::NodeHandle                 &controller_nh)
+	{
+		//initialize cargo outtake kicker joint (pneumatic piston that controls kicker)
+		cargo_outtake_kicker_joint_ = hw->getHandle("cargo_outtake_kicker_joint");
+		cargo_outtake_clamp_joint_ = hw->getHandle("cargo_outtake_clamp_joint");
 
-    //initialize cargo intake arm joint (pneumatic piston that controls up/down of arm)
-    cargo_outtake_arm_joint_ = pos_joint_iface->getHandle("cargo_outtake_arm_joint");
-    
-    cargo_outtake_service_ = controller_nh.advertiseService("cargo_outtake_command", &CargoOuttakeController::cmdService, this);
-    
-    return true;
-}
+		cargo_outtake_service_ = controller_nh.advertiseService("cargo_outtake_command", &CargoOuttakeController::cmdService, this);
 
-void CargoOuttakeController::starting(const ros::Time &/*time*/) {
-    cargo_outtake_arm_joint_.setCommand(-1); // set the command to the up/down part of the outtake
-}
+		return true;
+	}
 
-void CargoOuttakeController::update(const ros::Time &time, const ros::Duration &period) {
-    //process input for the up/down part of the intake (pneumatic piston)
-    bool outtake_arm_command = *(outtake_arm_command_.readFromRT());
-    double outtake_arm_command_double; //to store processed input
-    if(outtake_arm_command == true) {
-        //ROS_WARN("cargo outtake arm command: -1");
-        outtake_arm_command_double = -1;
-    }
-    else if (outtake_arm_command == false) {
-        outtake_arm_command_double = 1;
-        //ROS_WARN("cargo outtake arm command: 1");
-    }
-    cargo_outtake_arm_joint_.setCommand(outtake_arm_command_double); // set the in/out command to the up/down part of the outtake
-}
-	
-void CargoOuttakeController::stopping(const ros::Time &time) {
-}
+	void CargoOuttakeController::starting(const ros::Time &/*time*/) {
+		cargo_outtake_kicker_joint_.setCommand(-1); // set the command to the kicker of the outtake
+		cargo_outtake_clamp_joint_.setCommand(-1); // set the command to the up/down part of the outtake
+	}
 
-bool CargoOuttakeController::cmdService(cargo_outtake_controller::CargoOuttakeSrv::Request &req, cargo_outtake_controller::CargoOuttakeSrv::Response &res) {
-    if(isRunning())
-    {
-        outtake_arm_command_.writeFromNonRT(req.outtake_arm); //take the service request for in/out (true/false???) and write to a command variable
-    }
-    else
-    {
-        ROS_ERROR_STREAM("Can't accept new commands. CargoOuttakeController is not running.");
-        return false;
-    }
-    return true;
-}
+	void CargoOuttakeController::update(const ros::Time &time, const ros::Duration &period) {
+		//process input for the up/down part of the intake (pneumatic piston)
+		bool kicker_command = *(kicker_command_.readFromRT());
+		double kicker_command_double; //to store processed input
+		if(kicker_command == true) {
+			//ROS_WARN("cargo outtake kicker command: -1");
+			kicker_command_double = -1;
+		}
+		else if (kicker_command == false) {
+			kicker_command_double = 1;
+			//ROS_WARN("cargo outtake kicker command: 1");
+		}
+
+		bool clamp_command = *(clamp_command_.readFromRT());
+		double clamp_command_double; //to store processed input
+		if(clamp_command == true) {
+			//ROS_WARN("cargo outtake kicker command: -1");
+			clamp_command_double = -1;
+		}
+		else if (clamp_command == false) {
+			clamp_command_double = 1;
+			//ROS_WARN("cargo outtake kicker command: 1");
+		}
+
+		cargo_outtake_kicker_joint_.setCommand(kicker_command_double); // set the in/out command to the up/down part of the outtake
+		cargo_outtake_clamp_joint_.setCommand(clamp_command_double); // set the in/out command to the up/down part of the outtake
+	}
+
+	void CargoOuttakeController::stopping(const ros::Time &time) {
+
+	}
+
+	bool CargoOuttakeController::cmdService(cargo_outtake_controller::CargoOuttakeSrv::Request &req, cargo_outtake_controller::CargoOuttakeSrv::Response &res) {
+		if(isRunning())
+		{
+			kicker_command_.writeFromNonRT(req.kicker_in); //take the service request for in/out (true/false???) and write to a command variable
+			clamp_command_.writeFromNonRT(req.clamp_in); //take the service request for in/out (true/false???) and write to a command variable
+		}
+		else
+		{
+			ROS_ERROR_STREAM("Can't accept new commands. CargoOuttakeController is not running.");
+			return false;
+		}
+		return true;
+	}
+>>>>>>> Added 2nd joint to cargo outtake controller
 
 }//namespace
 
