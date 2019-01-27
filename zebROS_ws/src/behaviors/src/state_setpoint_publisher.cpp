f5dc175b13211a8b7abe5932e360be03c2fe42c9
Kevin Jaget <kjaget@gmail.com>
2019-01-27 11:30:02 -0500

    Quick code review of state setpoint publisher

--- zebROS_ws/src/behaviors/src/state_setpoint_publisher.cpp
+++ zebROS_ws/src/behaviors/src/state_setpoint_publisher.cpp
@@ -16,12 +16,14 @@ bool publish;
 double navx_angle_state;
 double distance_state;
 
+// TODO : none of these should be global
 std_msgs::Float64 orient_state_msg;
 std_msgs::Float64 y_state_msg;
 std_msgs::Float64 orient_setpoint_msg;
 std_msgs::Float64 y_target_msg;
 std_msgs::Bool pid_enable_msg;
 
+// TODO : none of these either - make them local in main()
 ros::Subscriber world_vector_sub;
 ros::Publisher orient_setpoint;
 ros::Publisher orient_state;
@@ -37,11 +39,17 @@ double distance_between_sensors = 0.3937;
 void distanceCB(const teraranger_array::RangeArray& msg)
 {
 	if(msg.ranges[1].range == msg.ranges[1].range && msg.ranges[5].range == msg.ranges[5].range)
+		// TODO: Consider using atan2?
 		distance_state = atan((msg.ranges[1].range - msg.ranges[5].range) / distance_between_sensors);
 	else
 		ROS_WARN_STREAM("at least one of the distance centers is NAN");
 }
 
+// TODO : not sure which joint this is, but I don't know we directly
+// publish navX angle to joint states. There's an offset, which is the
+// value used to zero navX readings, but I don't think that is what we
+// want here. Check out the navX callback in the swerve drive controller
+// to see how to read navX heading
 void jointStatesCB(const sensor_msgs::JointState& msg)
 {
 	navx_angle_state = msg.position[2];
@@ -51,6 +59,7 @@ bool align_service(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response 
 {
 	publish = req.data;
 	res.success = true;
+	// TODO : shouldn't this return true?
 	return 0;
 }
 
@@ -66,14 +75,19 @@ int main(int argc, char ** argv)
 	//state subscribers
 	ros::Subscriber distance_sub = nh.subscribe("multiflex_1/ranges_raw", 1, distanceCB);
 	ros::Subscriber angle_sub = nh.subscribe("joint_states", 1, jointStatesCB);
-	
+
 	//required pid_enable topic for the pid node
+	// TODO : consider how namespaces are doing to work, since I assume
+	// we'll eventually have multiple PID controllers running.
+	// It might be easy enough to set up using namespaces, but
+	// prepare for it now?
 	pid_enable = nh.advertise<std_msgs::Bool>("pid_enable", 1);
 
 	//sets true/false to PID node
 	run_align = nh.advertiseService("run_align", align_service);
 
 	//setpoint and state publishers
+	// TODO : all of these should be locals
 	orient_setpoint = nh.advertise<std_msgs::Float64>("orient_setpoint", 1);
 	orient_state = nh.advertise<std_msgs::Float64>("orient_state", 1);
 	y_setpoint = nh.advertise<std_msgs::Float64>("y_setpoint", 1);
@@ -81,6 +95,8 @@ int main(int argc, char ** argv)
 	navx_setpoint = nh.advertise<std_msgs::Float64>("navx_setpoint", 1);
 	navx_state = nh.advertise<std_msgs::Float64>("navx_state", 1);
 
+	// TODO : set a ROS rate here which matches the fastest data
+	// is expected to show up
 	while(ros::ok())
 	{
 		orient_setpoint.publish(orient_setpoint_msg);
