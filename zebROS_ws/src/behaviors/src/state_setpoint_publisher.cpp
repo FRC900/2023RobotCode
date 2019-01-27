6d3bc257af988f4c1499ab8180dac290d2223826
o-fugi <o.fugikawa@gmail.com>
2019-01-27 00:38:09 +0000

    added navx functionality

--- zebROS_ws/src/behaviors/src/state_setpoint_publisher.cpp
+++ zebROS_ws/src/behaviors/src/state_setpoint_publisher.cpp
@@ -9,8 +9,12 @@
 #include "teraranger_array/RangeArray.h"
 #include "std_msgs/Bool.h"
 #include "std_srvs/SetBool.h"
+#include "sensor_msgs/JointState.h"
 
 bool publish;
+
+double navx_angle_state;
+double distance_state;
 
 std_msgs::Float64 orient_state_msg;
 std_msgs::Float64 y_state_msg;
@@ -23,6 +27,8 @@ ros::Publisher orient_setpoint;
 ros::Publisher orient_state;
 ros::Publisher y_setpoint;
 ros::Publisher y_state;
+ros::Publisher navx_setpoint;
+ros::Publisher navx_state;
 ros::Publisher pid_enable;
 ros::ServiceServer run_align;
 
@@ -30,14 +36,14 @@ double distance_between_sensors = 0.3937;
 
 void distanceCB(const teraranger_array::RangeArray& msg)
 {
-	orient_setpoint.publish(orient_setpoint_msg);
+	distance_state = atan((msg.ranges[1].range - msg.ranges[5].range) / distance_between_sensors);
+	ROS_INFO_STREAM("distance state = " << distance_state);
+}
 
-	double angle = atan((msg.ranges[1].range - msg.ranges[5].range) / distance_between_sensors);
-	orient_state_msg.data = angle;
-	orient_state.publish(orient_state_msg);
-
-	pid_enable_msg.data = publish;
-	pid_enable.publish(pid_enable_msg);
+void jointStatesCB(const sensor_msgs::JointState& msg)
+{
+	navx_angle_state = msg.position[2];
+	ROS_INFO_STREAM("navx state = " << navx_angle_state);
 }
 
 bool align_service(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
@@ -51,18 +57,53 @@ int main(int argc, char ** argv)
 {
 	ros::init(argc, argv, "distance_setpoint_pub");
 	ros::NodeHandle nh;
+	int sensor_used = 1; //0 for navx, 1 for distance sensors, sorry!!
 
+	//the orientation we want is 0
 	orient_setpoint_msg.data = 0;
 
+	//state subscribers
 	ros::Subscriber distance_sub = nh.subscribe("multiflex_1/ranges_raw", 1, distanceCB);
-	orient_setpoint = nh.advertise<std_msgs::Float64>("orient_setpoint", 1);
-	orient_state = nh.advertise<std_msgs::Float64>("orient_state", 1);
-	//y_setpoint = nh.advertise<std_msgs::Float64>("y_setpoint", 1);
-	//y_state = nh.advertise<std_msgs::Float64>("y_state", 1);
+	ros::Subscriber angle_sub = nh.subscribe("joint_states", 1, jointStatesCB);
+	
+	//required pid_enable topic for the pid node
 	pid_enable = nh.advertise<std_msgs::Bool>("pid_enable", 1);
+
+	//sets true/false to PID node
 	run_align = nh.advertiseService("run_align", align_service);
 
-	ros::spin();
+	//setpoint and state publishers
+	orient_setpoint = nh.advertise<std_msgs::Float64>("orient_setpoint", 1);
+	orient_state = nh.advertise<std_msgs::Float64>("orient_state", 1);
+	y_setpoint = nh.advertise<std_msgs::Float64>("y_setpoint", 1);
+	y_state = nh.advertise<std_msgs::Float64>("y_state", 1);
+	navx_setpoint = nh.advertise<std_msgs::Float64>("navx_setpoint", 1);
+	navx_state = nh.advertise<std_msgs::Float64>("navx_state", 1);
+
+	while(ros::ok())
+	{
+		orient_setpoint.publish(orient_setpoint_msg);
+
+		switch (sensor_used)
+		{
+			case 0:
+				orient_state_msg.data = navx_angle_state;
+				break;
+			case 1:
+				orient_state_msg.data = distance_state;
+				break;
+			default:
+				orient_state_msg.data = 0;
+				break;
+		}
+
+		orient_state.publish(orient_state_msg);
+
+		pid_enable_msg.data = publish;
+		pid_enable.publish(pid_enable_msg);
+
+		ros::spinOnce();
+	}
 
 	return 0;
 }
