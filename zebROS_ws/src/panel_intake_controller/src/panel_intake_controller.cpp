eae06255282f33ecd2e99816e9aefbb964f04626
Adam <adamkosinski631@gmail.com>
2019-02-02 21:51:12 +0000

    Panel intake controller now thinks it has a panel if one of the sensors says so

--- zebROS_ws/src/panel_intake_controller/src/panel_intake_controller.cpp
+++ zebROS_ws/src/panel_intake_controller/src/panel_intake_controller.cpp
@@ -61,21 +61,40 @@ namespace panel_intake_controller
 	void PanelIntakeController::jointStateCallback(const sensor_msgs::JointState &joint_state)
 	{
 		//get index of linebreak sensor for this actionlib server
-		static size_t linebreak_idx = std::numeric_limits<size_t>::max();
+		static size_t linebreak_idx_1 = std::numeric_limits<size_t>::max();
+		static size_t linebreak_idx_2 = std::numeric_limits<size_t>::max();
+		static size_t linebreak_idx_3 = std::numeric_limits<size_t>::max();
+		static size_t linebreak_idx_4 = std::numeric_limits<size_t>::max();
 
-		if ((linebreak_idx >= joint_state.name.size()))
+		if (linebreak_idx_1 >= joint_state.name.size()
+			|| linebreak_idx_2 >= joint_state.name.size()
+			|| linebreak_idx_3 >= joint_state.name.size()
+			|| linebreak_idx_4 >= joint_state.name.size() )
 		{
 			for (size_t i = 0; i < joint_state.name.size(); i++)
 			{
 				if (joint_state.name[i] == "cargo_intake_linebreak_1")
-					linebreak_idx = i;
+					linebreak_idx_1 = i;
+				if (joint_state.name[i] == "cargo_intake_linebreak_2")
+					linebreak_idx_2 = i;
+				if (joint_state.name[i] == "cargo_intake_linebreak_3")
+					linebreak_idx_3 = i;
+				if (joint_state.name[i] == "cargo_intake_linebreak_4")
+					linebreak_idx_4 = i;
 			}
 		}
 
 		//update linebreak counts based on the value of the linebreak sensor
-		if (linebreak_idx < joint_state.position.size())
+		if (linebreak_idx_1 < joint_state.position.size()
+			&& linebreak_idx_2 < joint_state.position.size()
+			&& linebreak_idx_3 < joint_state.position.size()
+			&& linebreak_idx_4 < joint_state.position.size() )
 		{
-			bool linebreak_true = (joint_state.position[linebreak_idx] != 0);
+			bool linebreak_true = ( joint_state.position[linebreak_idx_1] != 0
+									|| joint_state.position[linebreak_idx_2] != 0
+									|| joint_state.position[linebreak_idx_3] != 0
+									|| joint_state.position[linebreak_idx_4] != 0
+								);
 			if(linebreak_true)
 			{
 				linebreak_true_count += 1;
