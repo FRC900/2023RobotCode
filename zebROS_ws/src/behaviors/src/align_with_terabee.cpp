6b95760a30cb437aa460b75830a4055dcba721cd
o-fugi <o.fugikawa@gmail.com>
2019-02-03 01:36:22 +0000

    updates to align, but not all cases are covered

--- zebROS_ws/src/behaviors/src/align_with_terabee.cpp
+++ zebROS_ws/src/behaviors/src/align_with_terabee.cpp
@@ -12,7 +12,11 @@ void multiflexCB(const teraranger_array::RangeArray& msg)
 {
 	for(int i = 0; i < msg.ranges.size(); i++)
 	{
-		senses_panels[i] = msg.ranges[i].range < MIN_DIST;
+		ROS_INFO_STREAM("i = " << i << " range = " << msg.ranges[i].range);
+		if(msg.ranges[i].range == msg.ranges[i].range)
+			senses_panels[i] = (msg.ranges[i].range < 1.0);
+		else
+			senses_panels[i] = false;
 	}
 }
 
@@ -40,28 +44,34 @@ int main(int argc, char ** argv)
 
 	while(ros::ok())
 	{
+		ROS_INFO_STREAM("0: " << senses_panels[0] << "1: " << senses_panels[1] << "2: " << senses_panels[2] << "3: " << senses_panels[3]); 
+		ROS_INFO_STREAM("0: " << senses_panels[0] << "1: " << senses_panels[1] << "2: " << senses_panels[2] << "3: " << senses_panels[3]); 
 		bool cutout_found = false;
-		for(int i = 0; i <= 4; i++)
+		for(int i = 0; i < NUM_SENSORS - 1; i++)
 		{
 			if(!senses_panels[i] && !senses_panels[i+1])
 			{
 				location = i;
-				cutout_found = true;
 				if(cutout_found)
 					ROS_ERROR_STREAM_THROTTLE(1, "two cutouts found????");
+				cutout_found = true;
 			}
 		}
-
 		ROS_INFO_STREAM("location = " << location);
-		if(location == floor((NUM_SENSORS+1)/2))
+
+		if(location == floor((NUM_SENSORS-1)/2))
 		{
+			ROS_INFO_STREAM("PERFECT");
+			cmd_vel_msg.linear.y = 0;
 		}
-		else if(location < floor((NUM_SENSORS+1)/2))
+		else if(location < floor((NUM_SENSORS-1)/2))
 		{
+			ROS_INFO_STREAM("move left");
 			cmd_vel_msg.linear.y = 0.1;
 		}
 		else
 		{
+			ROS_INFO_STREAM("move right");
 			cmd_vel_msg.linear.y = -0.1;
 		}
 
