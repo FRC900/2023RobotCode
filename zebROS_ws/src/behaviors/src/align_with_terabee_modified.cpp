11d1a5196634d32aeeb83e3a089c1971eb8dde33
o-fugi <o.fugikawa@gmail.com>
2019-02-06 02:47:14 +0000

    updated align with terabee. modified doesn't work, but handles more cases.

--- zebROS_ws/src/behaviors/src/align_with_terabee_modified.cpp
+++ zebROS_ws/src/behaviors/src/align_with_terabee_modified.cpp
@@ -29,6 +29,8 @@ void multiflexCB(const teraranger_array::RangeArray& msg)
 {
 	for(int i = 0; i < msg.ranges.size(); i++)
 	{
+		//if(i == 2)
+		//	continue;
 		sensors_distances[i] = msg.ranges[i].range;
 		min_dist = std::min(min_dist, static_cast<double>(msg.ranges[i].range));
 
@@ -50,7 +52,7 @@ int main(int argc, char ** argv)
 	ternary_distances.resize(NUM_SENSORS);
 
 	int location;
-	double dist_to_back_panel = 0.2;
+	double dist_to_back_panel = 0.14;
 	double error_threshhold = 0.05;
 
 	ros::Subscriber terabee_sub = n.subscribe("multiflex_1/ranges_raw", 1, &multiflexCB);
@@ -68,7 +70,7 @@ int main(int argc, char ** argv)
 
 	while(ros::ok())
 	{
-		ROS_INFO_STREAM("0: " << senses_panels[0] << "1: " << senses_panels[1] << "2: " << senses_panels[2] << "3: " << senses_panels[3]); 
+		ROS_INFO_STREAM("0: " << sensors_distances[0] << "1: " << sensors_distances[1] << "2: " << sensors_distances[2] << "3: " << sensors_distances[3]); 
 
 		for(int i = 0; i < sensors_distances.size(); i++)
 		{
@@ -86,12 +88,15 @@ int main(int argc, char ** argv)
 				ternary_distances[i] = 0;
 			}
 			else
-				ROS_INFO_STREAM("very confused");
+			{
+				ROS_INFO_STREAM("very confused " << sensors_distances[i]);
+			}
 
 		}
+		ROS_INFO_STREAM("0: " << ternary_distances[0] << " 1: " << ternary_distances[1] << " 2: " << ternary_distances[2] << " 3: " << ternary_distances[3]); 
 
 		bool cutout_found = false;
-		for(int i = 0; i < NUM_SENSORS - 1; i++)
+		/*for(int i = 0; i < NUM_SENSORS - 1; i++)
 		{
 			if((ternary_distances[i] == 1) && (ternary_distances[i+1] == 1))
 			{
@@ -101,7 +106,6 @@ int main(int argc, char ** argv)
 				cutout_found = true;
 			}
 		}
-		ROS_INFO_STREAM("0: " << ternary_distances[0] << " 1: " << ternary_distances[1] << " 2: " << ternary_distances[2] << " 3: " << ternary_distances[3]); 
 		ROS_INFO_STREAM("location = " << location);
 
 		// TODO : calcs using only ints will result in an int,
@@ -120,8 +124,72 @@ int main(int argc, char ** argv)
 		{
 			ROS_INFO_STREAM("move right");
 			cmd_vel_msg.linear.x = -0.1;
+		}*/
+
+		//cutout seen in leftmost or rightmost two sensors, and panel seen in other two
+		if(ternary_distances[0] == 0 && ternary_distances[1] == 0 && ternary_distances[2] == 1 && ternary_distances[3] == 1)
+		{
+			ROS_INFO_STREAM("move left, either cargo or rocket");
+			cmd_vel_msg.linear.y = 0.1;
+			cutout_found = true;
+		}
+		else if(ternary_distances[0] == 1 && ternary_distances[1] == 1 && ternary_distances[2] == 0 && ternary_distances[3] == 0)
+		{
+			ROS_INFO_STREAM("move right, either cargo or rocket");
+			cmd_vel_msg.linear.y = -0.1;
+			cutout_found = true;
 		}
 
+		//only one sensor senses the cutout, and then the other three see the panel
+		if(!cutout_found)
+		{
+			if(ternary_distances[0] == 1 && (ternary_distances[1] == 0 && ternary_distances[2] == 0 && ternary_distances[3] == 0))
+			{
+				ROS_INFO_STREAM("move left, probably cargo ship");
+				cmd_vel_msg.linear.y = 0.1;
+				cutout_found = true;
+			}
+			else if (ternary_distances[0] == 0 && ternary_distances[1] == 0 && ternary_distances[2] == 0 && ternary_distances[3] == 1)
+			{
+				ROS_INFO_STREAM("move right, probably cargo ship");
+				cmd_vel_msg.linear.y = -0.1;
+				cutout_found = true;
+			}
+		}
+
+		//the robot senses a cutout with two sensors, and then the panel, and then really far
+		if(!cutout_found)
+		{
+			if(ternary_distances[0] == 1 && (ternary_distances[1] == 1 && ternary_distances[2] == 0 && ternary_distances[3] == 2))
+			{
+				ROS_INFO_STREAM("move left, probably rocket edge");
+				cmd_vel_msg.linear.y = 0.1;
+				cutout_found = true;
+			}
+			else if (ternary_distances[0] == 2 && ternary_distances[1] == 0 && ternary_distances[2] == 1 && ternary_distances[3] == 1)
+			{
+				ROS_INFO_STREAM("move right, probably rocket edge");
+				cmd_vel_msg.linear.y = -0.1;
+				cutout_found = true;
+			}
+		}
+
+		//the robot senses a cutout with two sensors, and then the panel, and then what SEEMS like a cutout but is actually the other side of the rocket
+		if(!cutout_found)
+		{
+			if(ternary_distances[0] == 1 && (ternary_distances[1] == 1 && ternary_distances[2] == 0 && ternary_distances[3] == 1))
+			{
+				ROS_INFO_STREAM("move left, probably rocket center");
+				cmd_vel_msg.linear.y = 0.1;
+				cutout_found = true;
+			}
+			else if (ternary_distances[0] == 1 && ternary_distances[1] == 0 && ternary_distances[2] == 1 && ternary_distances[3] == 1)
+			{
+				ROS_INFO_STREAM("move right, probably rocket center");
+				cmd_vel_msg.linear.y = -0.1;
+				cutout_found = true;
+			}
+		}
 
 		cmd_vel_pub.publish(cmd_vel_msg);
 
