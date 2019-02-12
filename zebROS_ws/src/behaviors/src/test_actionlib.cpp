9609c06fa7b07aa5481f767cc0257c564c36a978
Caleb Krellwitz <cwkrellwitz@gmail.com>
2019-02-12 01:52:32 +0000

    test_actionlib.cpp works now

--- zebROS_ws/src/behaviors/src/test_actionlib.cpp
+++ zebROS_ws/src/behaviors/src/test_actionlib.cpp
@@ -4,17 +4,21 @@
 #include <behaviors/PlaceAction.h>
 #include <behaviors/ElevatorAction.h>
 #include <behaviors/ClimbAction.h>
+#include <behaviors/enumerated_elevator_indices.h>
 
 int main (int argc, char **argv)
 {
 	ros::init(argc, argv, "test_actionlib");
 
+	bool finished_before_timeout;
+
 	// create the action client
 	// true causes the client to spin its own thread
-	actionlib::SimpleActionClient<behaviors::IntakeAction> intake_cargo_ac("cargo_intake_server", true);
-	actionlib::SimpleActionClient<behaviors::IntakeAction> intake_hatch_panel_ac("intake_hatch_panel_server", true);
-	actionlib::SimpleActionClient<behaviors::PlaceAction> outtake_cargo_ac("cargo_outtake_server", true);
-	actionlib::SimpleActionClient<behaviors::PlaceAction> outtake_hatch_panel_ac("outtake_hatch_panel_server", true);
+	actionlib::SimpleActionClient<behaviors::IntakeAction> intake_cargo_ac("/cargo_intake/cargo_intake_server", true);
+	actionlib::SimpleActionClient<behaviors::IntakeAction> intake_hatch_panel_ac("/hatch_intake/intake_hatch_panel_server", true);
+	actionlib::SimpleActionClient<behaviors::PlaceAction> outtake_cargo_ac("/cargo_outtake/cargo_outtake_server", true);
+	actionlib::SimpleActionClient<behaviors::PlaceAction> outtake_hatch_panel_ac("/hatch_outtake/outtake_hatch_panel_server", true);
+	actionlib::SimpleActionClient<behaviors::ElevatorAction> elevator_ac("/elevator/elevator_server", true);
 
 	ROS_INFO("Waiting for cargo intake server to start.");
 	intake_cargo_ac.waitForServer(); //will wait for infinite time
@@ -28,23 +32,105 @@ int main (int argc, char **argv)
 	ROS_INFO("Waiting for panel outtake to start.");
 	outtake_hatch_panel_ac.waitForServer();
 
-	ROS_INFO("Action servers started, sending goal.");
+	for( int i = 0; i < 5; i++)
+	{
+		ROS_INFO("Sending goal to elevator server, setpoint = %d", i);
+		// send a goal to the action
+		behaviors::ElevatorGoal goal;
+		goal.setpoint_index = i;
+		elevator_ac.sendGoal(goal);
+
+		//wait for the action to return
+		finished_before_timeout = elevator_ac.waitForResult(ros::Duration(30.0));
+
+		if (finished_before_timeout)
+		{
+			actionlib::SimpleClientGoalState state = elevator_ac.getState();
+			ROS_INFO("Action finished: %s",state.toString().c_str());
+		}
+		else
+		{
+			ROS_INFO("Action did not finish before the time out.");
+
+		}
+	}
+
+	ROS_INFO("Sending goal to intake hatch panel server.");
 	// send a goal to the action
-	behaviors::IntakeGoal goal;
-	goal.motor_power = 0;
-	intake_hatch_panel_ac.sendGoal(goal);
-/*
+	behaviors::IntakeGoal intake_hatch_panel_goal;
+	intake_hatch_panel_goal.motor_power = 0;
+	intake_hatch_panel_ac.sendGoal(intake_hatch_panel_goal);
+
 	//wait for the action to return
-	bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+	finished_before_timeout = intake_hatch_panel_ac.waitForResult(ros::Duration(30.0));
 
 	if (finished_before_timeout)
 	{
-		actionlib::SimpleClientGoalState state = ac.getState();
+		actionlib::SimpleClientGoalState state = intake_hatch_panel_ac.getState();
 		ROS_INFO("Action finished: %s",state.toString().c_str());
 	}
 	else
+	{
 		ROS_INFO("Action did not finish before the time out.");
+	}
 
-	//exit*/
+	ROS_INFO("Sending goal to outtake hatch panel server.");
+	// send a goal to the action
+	behaviors::PlaceGoal outtake_hatch_panel_goal;
+	outtake_hatch_panel_goal.setpoint_index = CARGO_SHIP;
+	outtake_hatch_panel_ac.sendGoal(outtake_hatch_panel_goal);
+
+	//wait for the action to return
+	finished_before_timeout = outtake_hatch_panel_ac.waitForResult(ros::Duration(30.0));
+
+	if (finished_before_timeout)
+	{
+		actionlib::SimpleClientGoalState state = outtake_hatch_panel_ac.getState();
+		ROS_INFO("Action finished: %s",state.toString().c_str());
+	}
+	else
+	{
+		ROS_INFO("Action did not finish before the time out.");
+	}
+
+	ROS_INFO("Sending goal to intake cargo server.");
+	// send a goal to the action
+	behaviors::IntakeGoal intake_cargo_goal;
+	intake_cargo_goal.motor_power = 1;
+	intake_cargo_ac.sendGoal(intake_cargo_goal);
+
+	//wait for the action to return
+	finished_before_timeout = intake_cargo_ac.waitForResult(ros::Duration(30.0));
+
+	if (finished_before_timeout)
+	{
+		actionlib::SimpleClientGoalState state = intake_cargo_ac.getState();
+		ROS_INFO("Action finished: %s",state.toString().c_str());
+	}
+	else
+	{
+		ROS_INFO("Action did not finish before the time out.");
+	}
+
+	ROS_INFO("Sending goal to outtake cargo server.");
+	// send a goal to the action
+	behaviors::PlaceGoal outtake_cargo_goal;
+	outtake_cargo_goal.setpoint_index = CARGO_SHIP;
+	outtake_cargo_ac.sendGoal(outtake_cargo_goal);
+
+	//wait for the action to return
+	finished_before_timeout = outtake_cargo_ac.waitForResult(ros::Duration(30.0));
+
+	if (finished_before_timeout)
+	{
+		actionlib::SimpleClientGoalState state = outtake_cargo_ac.getState();
+		ROS_INFO("Action finished: %s",state.toString().c_str());
+	}
+	else
+	{
+		ROS_INFO("Action did not finish before the time out.");
+	}
+
+	//exit
 	return 0;
 }
