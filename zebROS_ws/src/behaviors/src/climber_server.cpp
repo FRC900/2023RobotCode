b2d029a1add7114a4e56925d6114cd053b51ba02
FalconWarriorr <niall.p.mullane@gmail.com>
2019-03-28 01:58:08 +0000

    tons of changes creating a base align server to be inherited by specific align servers

--- zebROS_ws/src/behaviors/src/climber_server.cpp
+++ zebROS_ws/src/behaviors/src/climber_server.cpp
@@ -209,7 +209,6 @@ class ClimbAction {
 					behaviors::ElevatorGoal goal;
 					goal.setpoint_index = ELEVATOR_DEPLOY;
 					goal.place_cargo = 0; //doesn't actually do anything
-					goal.raise_intake_after_success = true;
 					//send the goal
 					ae_.sendGoal(goal);
 					const bool finished_before_timeout = ae_.waitForResult(ros::Duration(elevator_deploy_timeout)); //wait until the action finishes, whether it succeeds, times out, or is preempted
@@ -273,7 +272,6 @@ class ClimbAction {
 					behaviors::ElevatorGoal goal;
 					goal.setpoint_index = ELEVATOR_CLIMB;
 					goal.place_cargo = 0; //doesn't actually do anything
-					goal.raise_intake_after_success = true;
 					//send the goal
 					ae_.sendGoal(goal);
 					const bool finished_before_timeout = ae_.waitForResult(ros::Duration(elevator_deploy_timeout)); //wait until the action finishes, whether it succeeds, times out, or is preempted
@@ -370,7 +368,6 @@ class ClimbAction {
 					behaviors::ElevatorGoal goal;
 					goal.setpoint_index = ELEVATOR_CLIMB_LOW;
 					goal.place_cargo = 0; //doesn't actually do anything 
-					goal.raise_intake_after_success = true;
 					//send the goal
 					ae_.sendGoal(goal);
 					const bool finished_before_timeout = ae_.waitForResult(ros::Duration(elevator_climb_timeout));
@@ -407,7 +404,6 @@ class ClimbAction {
 				behaviors::ElevatorGoal goal;
 				goal.setpoint_index = ELEVATOR_RAISE;
 				goal.place_cargo = 0; //doesn't actually do anything
-				goal.raise_intake_after_success = true;
 				//send the goal
 				ae_.sendGoal(goal);
 				const bool finished_before_timeout = ae_.waitForResult(ros::Duration(elevator_deploy_timeout));
