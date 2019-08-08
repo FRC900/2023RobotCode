79c91cce55d65228f1f5d743279a75e67220ccab
Kevin Jaget <kjaget@gmail.com>
2019-08-08 18:05:09 -0400

    Maybe fix race condition in hwi

--- zebROS_ws/src/ros_control_boilerplate/src/frcrobot_hw_interface.cpp
+++ zebROS_ws/src/ros_control_boilerplate/src/frcrobot_hw_interface.cpp
@@ -241,9 +241,6 @@ void FRCRobotHWInterface::init(void)
 										  can_talons_[i], talon_read_thread_states_[i],
 										  talon_read_state_mutexes_[i],
 										  talon_thread_tracers_[i]));
-			std::stringstream s;
-			s << "ctre_read_" << can_talon_srx_can_ids_[i];
-			pthread_setname_np(talon_read_threads_.back().native_handle(), s.str().c_str());
 		}
 		else
 		{
@@ -483,9 +480,6 @@ void FRCRobotHWInterface::init(void)
 					pcm_thread_.push_back(std::thread(&FRCRobotHWInterface::pcm_read_thread, this,
 								compressors_[i], compressor_pcm_ids_[i], pcm_read_thread_state_[i],
 								pcm_read_thread_mutexes_[i], pcm_thread_tracers_[i]));
-					std::stringstream s;
-					s << "pcm_read_" << i;
-					pthread_setname_np(pcm_thread_[i].native_handle(), s.str().c_str());
 					HAL_Report(HALUsageReporting::kResourceType_Compressor, compressor_pcm_ids_[i]);
 				}
 				else
@@ -537,9 +531,6 @@ void FRCRobotHWInterface::init(void)
 					pdp_thread_.push_back(std::thread(&FRCRobotHWInterface::pdp_read_thread, this,
 										  pdps_[i], pdp_read_thread_state_[i], pdp_read_thread_mutexes_[i],
 										  pdp_thread_tracers_[i]));
-					std::stringstream s;
-					s << "pdp_read_" << i;
-					pthread_setname_np(pdp_thread_[i].native_handle(), s.str().c_str());
 					HAL_Report(HALUsageReporting::kResourceType_PDP, pdp_modules_[i]);
 				}
 			}
@@ -586,6 +577,7 @@ void FRCRobotHWInterface::talon_read_thread(std::shared_ptr<ctre::phoenix::motor
 											std::shared_ptr<std::mutex> mutex,
 											Tracer tracer)
 {
+	pthread_setname_np(pthread_self(), "ctre_mc_read");
 	ros::Duration(2).sleep(); // Sleep for a few seconds to let CAN start up
 	ros::Rate rate(100); // TODO : configure me from a file or
 						 // be smart enough to run at the rate of the fastest status update?
@@ -839,6 +831,7 @@ void FRCRobotHWInterface::pdp_read_thread(int32_t pdp,
 		std::shared_ptr<std::mutex> mutex,
 		Tracer tracer)
 {
+	pthread_setname_np(pthread_self(), "pdp_read");
 	ros::Duration(2).sleep(); // Sleep for a few seconds to let CAN start up
 	ros::Rate r(20); // TODO : Tune me?
 	int32_t status = 0;
@@ -889,6 +882,7 @@ void FRCRobotHWInterface::pcm_read_thread(HAL_CompressorHandle compressor_handle
 										  std::shared_ptr<std::mutex> mutex,
 										  Tracer tracer)
 {
+	pthread_setname_np(pthread_self(), "pcm_read");
 	ros::Duration(2).sleep(); // Sleep for a few seconds to let CAN start up
 	ros::Rate r(20); // TODO : Tune me?
 	int32_t status = 0;
