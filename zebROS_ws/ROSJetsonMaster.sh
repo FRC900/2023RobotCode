04bb47772cacfb9adb64e63953e9d1cbc1fc53b5
Kevin Jaget <kjaget@gmail.com>
2019-09-02 15:43:43 -0400

    Again try fixing setting ROS_IP in .sh files

--- zebROS_ws/ROSJetsonMaster.sh
+++ zebROS_ws/ROSJetsonMaster.sh
@@ -6,14 +6,14 @@ if [ -f /.dockerenv ] ; then
     echo "Sourcing Docker environment"
     source /opt/ros/melodic/setup.bash
     source /home/ubuntu/2019Offseason/zebROS_ws/devel/setup.bash
-    export ROS_IP=`ip route get 10.9.0.1 | head -1 | cut -d ' ' -f 7`
+	export ROS_IP=`ip route get 10.9.0.1 | sed 's/ via [[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+//' | sed 's/lo //' | head -1 | cut -d ' ' -f 5`
 elif [ -f /home/ubuntu/2019Offseason/zebROS_ws/devel/setup.bash ] ; then
     # Jetson-specific configuration
     echo "Sourcing Jetson / native Linux environment"
     source /opt/ros/melodic/setup.bash
     source /home/ubuntu/2019Offseason/zebROS_ws/devel/setup.bash
     #export ROS_IP=10.9.0.8
-    export ROS_IP=`ip route get 10.9.0.1 | head -1 | cut -d ' ' -f 7`
+	export ROS_IP=`ip route get 10.9.0.1 | sed 's/ via [[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+//' | sed 's/lo //' | head -1 | cut -d ' ' -f 5`
 elif [ -f /home/ofugikawa/2019Offseason/zebROS_ws/devel/setup.bash ] ; then
     # Jetson-specific configuration
     echo "Sourcing Olivia / native Linux environment"
@@ -30,13 +30,12 @@ else
     echo "Unknown environment! Trying to proceed anyway using local environment."
     source /opt/ros/melodic/setup.bash
     source $HOME/2019Offseason/zebROS_ws/devel/setup.bash
-    export ROS_IP=`ip route get 10.9.0.1 | head -1 | cut -d ' ' -f 7`
+	export ROS_IP=`ip route get 10.9.0.1 | sed 's/ via [[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+//' | sed 's/lo //' | head -1 | cut -d ' ' -f 5`
 fi
 
 # Common configuration
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 export ROS_MASTER_URI=http://10.9.0.8:5802
-#export ROS_IP=`/bin/hostname -I | tr -d ' ' | tr -d '\n'`
 export ROSLAUNCH_SSH_UNKNOWN=1
 echo "ROS_IP set to $ROS_IP"
 
