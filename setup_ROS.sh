cf8d3420288fd300a010b7ea7d9a7c50628a86f4
Kevin Jaget <kjaget@gmail.com>
2019-09-05 20:40:27 -0400

    Update to realsense 2.27 in ros setup script

--- setup_ROS.sh
+++ setup_ROS.sh
@@ -23,14 +23,14 @@ sudo apt update
 # From source for the Jetson
 sudo apt install -y git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev libglfw3-dev libgl1-mesa-dev libglu1-mesa-dev 
 mkdir realsense_src && cd realsense_src
-wget https://github.com/IntelRealSense/librealsense/archive/v2.25.0.zip
-unzip v2.25.0.zip
-cd librealsense-2.25.0
+wget https://github.com/IntelRealSense/librealsense/archive/v2.27.0.zip
+unzip v2.27.0.zip
+cd librealsense-2.27.0
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=true ..
-sudo make uninstall && make clean && make -j4 && sudo make install
+sudo make uninstall && make clean && make -j6 && sudo make install
 
 # Add Individual Packages here
 # You can install a specific ROS package (replace underscores with dashes of the package name):
