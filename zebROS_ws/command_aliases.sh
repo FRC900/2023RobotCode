15afc60f304039a7d23b0a52d9ab87d02e2884cc
Caleb Krellwitz <43455398+CalebKrellwitz@users.noreply.github.com>
2019-10-11 12:48:50 -0400

    Add various bash aliases for common commands (#5)
    * Add various bash aliases for common commands
    
    * Change "launch" to "hwlaunch"
    
    * 'build' to 'natbuild' and add 'rosjet'
    
    * Re-establish alphabetical order

---
+++ zebROS_ws/command_aliases.sh
@@ -0,0 +1,9 @@
+# Aliases are short commands to replace long ones.
+
+alias crossbuild='~/2019Offseason/zebROS_ws/cross_build.sh'
+alias deploy='~/2019Offseason/deploy.sh'
+alias hwlaunch='roslaunch controller_node 2019_compbot_combined.launch'
+alias natbuild='~/2019Offseason/zebROS_ws/native_build.sh'
+alias rosjet='source ~/2019Offseason/zebROS_ws/ROSJetsonMaster.sh'
+alias rosstd='source ~/2019Offseason/zebROS_ws/ROSStandard.sh'
+alias simlaunch='roslaunch controller_node 2019_compbot_combined.launch hw_or_sim:=sim joy_or_key:=key'
