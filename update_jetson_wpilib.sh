87f318183de4742b1540b3fcc85b3a62c7321570
Kevin Jaget <kjaget@gmail.com>
2019-12-03 17:07:53 -0500

    Don't use compression for update_jetson_wpilib.sh rsync calls

--- update_jetson_wpilib.sh
+++ update_jetson_wpilib.sh
@@ -9,9 +9,9 @@ ssh -p 22 ubuntu@$1 'rm -rf /home/ubuntu/frc2019'
 ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/include/ctre'
 ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/include/navx'
 ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/include/wpilib'
-rsync -avzru $HOME/frc2019/roborio/arm-frc2019-linux-gnueabi/include/ $1:/home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/include/
+rsync -avru $HOME/frc2019/roborio/arm-frc2019-linux-gnueabi/include/ $1:/home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/include/
 
 ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/lib/ctre'
 ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/lib/navx'
 ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/lib/wpilib'
-rsync -avrzu --exclude '*.debug' --exclude 'athena' $HOME/frc2019/roborio/arm-frc2019-linux-gnueabi/lib/ $1:/home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/lib/
+rsync -avru --exclude '*.debug' --exclude 'athena' $HOME/frc2019/roborio/arm-frc2019-linux-gnueabi/lib/ $1:/home/ubuntu/frc2019/roborio/arm-frc2019-linux-gnueabi/lib/
