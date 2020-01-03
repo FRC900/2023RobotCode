e3a22d35e06665a1eb9a18e6445607093ac9c7f2
Kevin Jaget <kjaget@nc.rr.com>
2020-01-03 14:42:25 -0500

    setup_environment.sh : update to latest ctre libs and headers

--- setup_environment.sh
+++ setup_environment.sh
@@ -276,7 +276,14 @@ sudo apt remove --purge -y thunderbird libreoffice-* unattended-upgrade
 mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include 
 mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ctre 
 cd /home/ubuntu 
-wget -e robots=off -U mozilla -r -np http://devsite.ctr-electronics.com/maven/release/com/ctre/phoenix/ -A "*5.14.1*,firmware-sim*zip" -R "md5,sha1,pom,jar,*windows*"
+wget -e robots=off -U mozilla -r -np http://devsite.ctr-electronics.com/maven/release/com/ctre/phoenix/ -A "*5.16.0*,firmware-sim*zip" -R "md5,sha1,pom,jar,*windows*"
+cd /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include 
+find /home/ubuntu/devsite.ctr-electronics.com -name \*headers\*zip | xargs -n 1 unzip -o 
+cd /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ctre 
+find /home/ubuntu/devsite.ctr-electronics.com -name \*linux\*zip | xargs -n 1 unzip -o 
+rm -rf /home/ubuntu/devsite.ctr-electronics.com 
+cd /home/ubuntu 
+wget -e robots=off -U mozilla -r -np http://devsite.ctr-electronics.com/maven/release/com/ctre/phoenix/ -A "*5.17.2*,firmware-sim*zip" -R "md5,sha1,pom,jar,*windows*"
 cd /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include 
 find /home/ubuntu/devsite.ctr-electronics.com -name \*headers\*zip | xargs -n 1 unzip -o 
 cd /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ctre 
