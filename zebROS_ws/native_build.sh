e3039d28d866d83690a16e5e9d335378a91f6d44
o-fugi <o.fugikawa@gmail.com>
2020-01-17 22:10:12 +0000

    added the realsense packages back in

--- zebROS_ws/native_build.sh
+++ zebROS_ws/native_build.sh
@@ -21,8 +21,6 @@ fi
 
 catkin config --blacklist \
 	velocity_controllers \
-        realsense2_camera \
-        realsense2_description \
 	zed_ar_track_alvar_example \
 	zed_display_rviz \
 	zed_depth_sub_tutorial \
