18901c36b0e8faf4a3fef3bac61d714883758f18
Adam <adamkosinski631@gmail.com>
2020-01-30 23:55:53 +0000

    Added a missing line to the template controller

--- zebROS_ws/src/template_controller/src/template.cpp
+++ zebROS_ws/src/template_controller/src/template.cpp
@@ -13,7 +13,7 @@ namespace mech_controller
 	{
 		//get interface
 		hardware_interface::PositionJointInterface *const pos_joint_iface = hw->get<hardware_interface::PositionJointInterface>();
-
+		hardware_interface::TalonCommandInterface *const talon_command_iface = hw->get<hardware_interface::TalonCommandInterface>();
 		//Initialize piston joints
 		/* Ex:
 		push_joint_ = pos_joint_iface->getHandle("joint_name"); //joint_name comes from ros_control_boilerplate/config/[insert_year]_compbot_base_jetson.yaml
