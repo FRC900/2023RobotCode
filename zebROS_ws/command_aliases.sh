8113ee90e296b5df7c5cdb00701daffdcc49346c
adam-kosinski <40772553+adam-kosinski@users.noreply.github.com>
2020-02-05 23:21:02 -0500

    Updated launch aliases for 2020 (#49)

--- zebROS_ws/command_aliases.sh
+++ zebROS_ws/command_aliases.sh
@@ -2,9 +2,9 @@
 
 alias crossbuild='~/2020RobotCode/zebROS_ws/cross_build.sh'
 alias deploy='~/2020RobotCode/deploy.sh'
-alias hwlaunch='roslaunch controller_node 2019_compbot_combined.launch'
+alias hwlaunch='roslaunch controller_node 2020_compbot_combined.launch'
 alias natbuild='~/2020RobotCode/zebROS_ws/native_build.sh'
 alias rosjet='source ~/2020RobotCode/zebROS_ws/ROSJetsonMaster.sh'
 alias rosstd='source ~/2020RobotCode/zebROS_ws/ROSStandard.sh'
-alias simlaunch='roslaunch controller_node 2019_compbot_combined.launch hw_or_sim:=sim joy_or_key:=key'
+alias simlaunch='roslaunch controller_node 2020_compbot_combined.launch hw_or_sim:=sim joy_or_key:=key'
 alias ls='ls --color'
