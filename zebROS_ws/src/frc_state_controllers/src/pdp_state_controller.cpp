402fe2e67d1954ec632c3bac4f42c7a6d784b3b9
Clara Wang <43455615+wang20c@users.noreply.github.com>
2020-02-11 09:38:52 -0500

    Pdp channel labels (#57)
    * create channels array, add to pdp_state_controller
    
    * changed name of config value and moved to pdp_state_controller
    works in sim
    
    * removed strange swl/m/n files that seemed to be commited on accident
    
    * Undo unused changed to pdp_state_interface.h
    
    * Minor formatting and cleanup in pdp_state_controller
    
    Co-authored-by: Olivia Fugikawa <o.fugikawa@gmail.com>
    Co-authored-by: Kevin Jaget <v9i1j2r5u4o3s7n2@frc900.slack.com>

--- zebROS_ws/src/frc_state_controllers/src/pdp_state_controller.cpp
+++ zebROS_ws/src/frc_state_controllers/src/pdp_state_controller.cpp
@@ -1,14 +1,12 @@
 #include "frc_state_controllers/pdp_state_controller.h"
 #include "frc_msgs/PDPData.h"
-#include <cstddef>
-#include <algorithm>
 
-namespace pdp_state_controller 
+namespace pdp_state_controller
 {
 
 bool PDPStateController::init(hardware_interface::PDPStateInterface *hw,
-								ros::NodeHandle 					&root_nh,
-								ros::NodeHandle 					&controller_nh)
+								ros::NodeHandle						&root_nh,
+								ros::NodeHandle						&controller_nh)
 {
 	ROS_INFO_STREAM_NAMED("pdp_state_controller", "init is running");
 
@@ -35,28 +33,26 @@ bool PDPStateController::init(hardware_interface::PDPStateInterface *hw,
 	m.totalPower = 0.0;
 	m.totalEnergy = 0.0;
 
-        /*int count = 0;
-        while(count < 10)
-        {
-            print(count);
-            count = count + 1;
-            //some code here
-        }
+    XmlRpc::XmlRpcValue thingsPluggedIn;
+    if(!controller_nh.getParam("things_plugged_in_pdp_channel", thingsPluggedIn)){
+        ROS_ERROR("No things plugged in specified");
+	}
 
-        for(int count = 0; count < 10; count++)
-        {
-            print(count);
-        }*/
-	
 	for(int channel = 0; channel <= 15; channel++)
 	{
 		m.current[channel] = 0;
+		XmlRpc::XmlRpcValue thing_plugged_in = thingsPluggedIn[channel];
+		if(!thing_plugged_in.valid() || thing_plugged_in.getType() != XmlRpc::XmlRpcValue::TypeString)
+			ROS_ERROR("An invalid thing_plugged_in name was specified (expecting a string)");
+		else {
+			std::string thing_string = thing_plugged_in;
+			m.thingsPluggedIn[channel] = thing_string;
+		}
 	}
 
 	pdp_state_ = hw->getHandle(pdp_name);
 
 	return true;
-	
 }
 
 void PDPStateController::starting(const ros::Time &time)
@@ -72,32 +68,31 @@ void PDPStateController::update(const ros::Time &time, const ros::Duration & )
 		if (realtime_pub_->trylock())
 		{
 			last_publish_time_ = last_publish_time_ + ros::Duration(1.0 / publish_rate_);
-			
+
 			auto &m = realtime_pub_->msg_;
 
 			m.header.stamp = time;
 
 			auto &ps = pdp_state_;
-			
+
 			//read from the object and stuff it in a msg
 			m.voltage = ps->getVoltage();
 			m.temperature = ps->getTemperature();
 			m.totalCurrent = ps->getTotalCurrent();
 			m.totalPower = ps->getTotalPower();
 			m.totalEnergy = ps->getTotalEnergy();
-	
+
 			for(int channel = 0; channel <= 15; channel++)
 			{
 				m.current[channel] = ps->getCurrent(channel);
 			}
-	
+
 			realtime_pub_->unlockAndPublish();
-			
 		}
 	}
 }
 
-void PDPStateController::stopping(const ros::Time & ) 
+void PDPStateController::stopping(const ros::Time & )
 {}
 }
 
