73ff8c47b673957eee33846be3ad096678790c95
Caleb Krellwitz <cwkrellwitz@gmail.com>
2020-03-07 22:40:54 +0000

    Fix no negative turns

--- zebROS_ws/src/teleop_joystick_control/include/teleop_joystick_control/TeleopCmdVel.h
+++ zebROS_ws/src/teleop_joystick_control/include/teleop_joystick_control/TeleopCmdVel.h
@@ -74,7 +74,7 @@ class TeleopCmdVel
 			// of the slower settings. Use copysign to preserve the sign
 			// of the original input (keeps the direction correct)
 			double rotation = pow(rightStickX, config.rotation_pow);
-			rotation  = copysign(rotation, rightStickX);
+			rotation  = copysign(rotation, event.rightStickX);
 			rotation *= max_rot;
 
 			// Rate-limit changes in rotation
