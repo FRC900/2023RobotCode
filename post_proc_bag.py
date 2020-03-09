95072b65f1744e4afd1d54e0d04d67b53b636df1
Kevin Jaget <kjaget@nc.rr.com>
2020-03-09 10:16:48 -0400

    post_proc_bag.py - rename to get rid of .active and leading _ Redo output file format for better sorting (start with time/date, etc)

--- post_proc_bag.py
+++ post_proc_bag.py
@@ -33,13 +33,27 @@ def mkdir_p(path):
 
 #Bagfile name from python script, assuming bagfile is in ../bagfiles
 bagfile_name = sys.argv[1]
-bagfile_path = str(bagfile_name)
+
+# Rename .bag.active to .bag, strip leading _ if present
+filename, extension = os.path.splitext(bagfile_name)
+if filename[0] == '_':
+    filename = filename[1:]
+
+if (extension == '.active') or (bagfile_name[0] == '_'):
+    if os.path.isfile(filename):
+        print 'Both ' + bagfile_name + ' and ' + filename + ' exist, exiting instead of overwiting on rename'
+        sys.exit(-1)
+
+    os.rename(bagfile_name, filename)
+    bagfile_name = filename
+    
+# Open bagfile, reindex if necessary
 try: 
-    bag = rosbag.Bag(bagfile_path)
+    bag = rosbag.Bag(bagfile_name)
 except rosbag.bag.ROSBagUnindexedException:
     print "Reindexing bag file " + str(bagfile_name)
     os.system("rosbag reindex " + str(bagfile_name))
-    bag = rosbag.Bag(bagfile_path)
+    bag = rosbag.Bag(bagfile_name)
 
 match_topic = '/frcrobot_rio/match_data'
 
@@ -71,7 +85,7 @@ for topic, msg, t in bag.read_messages([match_topic]):
             elapsed_time = disable_times[-1] - enable_times[-1]
             if (elapsed_time > 13) and (elapsed_time < 17) and ((this_time - disable_times[-1]) < 3) and (int(match_types[-1]) > 0):
                 disable_times.pop()
-                print 'Combining auto and teleop'
+                print('Combining auto and teleop')
                 continue
 
         enable_times.append(this_time)
@@ -84,13 +98,13 @@ for topic, msg, t in bag.read_messages([match_topic]):
             match_types.append(str(m['matchType']))
             replay_numbers.append(str(m['replayNumber']))
             event_names.append(str(m['eventName']))
-            print "Enable at " + str(enable_times[-1]) + " " + str(match_numbers[-1]) + " " + str(match_types[-1]) + " " + str(replay_numbers[-1]) + " " + str(event_names[-1])
+            print("Enable at " + str(enable_times[-1]) + " " + str(match_numbers[-1]) + " " + str(match_types[-1]) + " " + str(replay_numbers[-1]) + " " + str(event_names[-1]))
 
     if(isEnabled == True and m['Enabled']==False):
         isEnabled = False
         disable_times.append(this_time)
         elapsed_time = disable_times[-1] - enable_times[-1]
-        print "Disable at " + str(disable_times[-1]) + " elapsed = " + str(elapsed_time)
+        print("Disable at " + str(disable_times[-1]) + " elapsed = " + str(elapsed_time))
 
 # If we're still enabled at the end of the bag file, tag the last
 # message time as the end of the bag file to copy
@@ -102,6 +116,8 @@ if len(enable_times) != len(disable_times):
 mkdir_p('trimmed')
 mkdir_p('done')
 for i in range(len(enable_times)):
+    # Any one should indicate an enable when the robot was not
+    # connected to the field, but check all to be sure
     if int(match_types[i]) == 0:
        continue
     if int(match_numbers[i]) == 0:
@@ -111,36 +127,36 @@ for i in range(len(enable_times)):
     if len(event_names[i]) == 0:
         continue
 
-    #Script input
     date_str = datetime.datetime.utcfromtimestamp(enable_times[i]).strftime("%Y%m%d_%H%M%S");
 
     # Replay 1 is the normal case of a match not being replayed - don't include that in the filename 
     replay_str = ""
     if (int(replay_numbers[i]) > 1):
         replay_str = "R"+replay_numbers[i]
+
+    # Pad match number to two characters so sorting by filename puts e.g. match 2 before match 10
     if (len(match_numbers[i]) < 2):
         match_numbers[i] = '0' + match_numbers[i]
-    file_name = "match_"+matchType(match_types[i])+match_numbers[i]+replay_str+"_"+event_names[i]+"_"+date_str+".bag"
+    file_name = date_str + "_" + event_names[i] + "_" + matchType(match_types[i]) + match_numbers[i] + replay_str + ".bag"
     print(file_name)
     print('Enable at  ' + str(enable_times[i]) + " (or " + str(enable_times[i]-first_time) + "s)") 
     print('Disable at ' + str(disable_times[i]) + " (or " + str(disable_times[i]-first_time) + "s)")
-    elapsed_time = disable_times[i] - enable_times[i]
-    print "Elapsed time = " + str(elapsed_time)
+    print("Elapsed time = " + str(disable_times[i] - enable_times[i]))
 
     if os.path.isfile(os.path.join('trimmed', file_name)):
         print "Output file exists - exiting instead of overwriting"
         sys.exit()
 
-    #Context output
     print("Time of Enable: " + str(enable_times[i]) + " (or " + str(enable_times[i]-first_time) + "s)")
     print("Match Number: " + str(match_numbers[i]))
     print("Event Name: " + str(event_names[i]))
 
-    sys_call = 'rosbag filter ' + bagfile_path + ' ' + os.path.join('trimmed', file_name) + ' \"t.to_sec() >= ' + str(enable_times[i] - 10.0) + ' and t.to_sec() <= ' + str(disable_times[i] + 10.0) + '\"'
+    # Extract match plus 10 seconds padding at start and end to separate bag file
+    sys_call = 'rosbag filter ' + bagfile_name + ' ' + os.path.join('trimmed', file_name) + ' \"t.to_sec() >= ' + str(enable_times[i] - 10.0) + ' and t.to_sec() <= ' + str(disable_times[i] + 10.0) + '\"'
     print (sys_call)
     os.system(sys_call)
 
 if len(enable_times) == 0:
-    print 'Note - robot not enabled in bag data'
+    print('Note - robot not enabled in bag data')
 
-os.rename(bagfile_path, os.path.join('done', bagfile_path))
+os.rename(bagfile_name, os.path.join('done', bagfile_name))
