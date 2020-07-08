0b622aa2688c04dff6e470e586cb2dc2877b68ea
Kevin Jaget <kjaget@nc.rr.com>
2020-07-07 22:42:22 -0400

    Revert "Update to latest librealsense"
    This reverts commit 7f098e952c323a55a6e1ef967eaf7636e2b829e4.

--- zebROS_ws/ROSJetsonMaster.sh
+++ zebROS_ws/ROSJetsonMaster.sh
@@ -23,7 +23,7 @@ elif [ -f /home/admin/rio_bashrc.sh ] ; then
     # roboRIO-specific configuration
     echo "Sourcing roboRIO environment"
     source /home/admin/rio_bashrc.sh
-    export ROS_IP=192.168.0.169
+    export ROS_IP=10.9.0.2
     export LD_LIBRARY_PATH=/home/admin/wpilib:$LD_LIBRARY_PATH
     swapon /dev/sda5
 	ulimit -r unlimited
@@ -36,7 +36,7 @@ fi
 
 # Common configuration
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-export ROS_MASTER_URI=http://192.168.0.171:5802
+export ROS_MASTER_URI=http://10.9.0.8:5802
 export ROSLAUNCH_SSH_UNKNOWN=1
 echo "ROS_IP set to $ROS_IP"
 
