abf8dbade1e111273bbd239578e7aee3c38c93e0
Kevin Jaget <kjaget@nc.rr.com>
2020-09-08 20:56:41 -0400

    Update to latest librealsense install

--- setup_ROS.sh
+++ setup_ROS.sh
@@ -25,9 +25,9 @@ sudo apt install -y git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev libg
 cd
 mkdir realsense_src
 cd realsense_src
-wget https://github.com/IntelRealSense/librealsense/archive/v2.35.2.zip
-unzip v2.35.2.zip
-cd librealsense-2.35.2
+wget https://github.com/IntelRealSense/librealsense/archive/v2.38.1.zip
+unzip v2.38.1.zip
+cd librealsense-2.38.1
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && udevadm trigger
 mkdir build
