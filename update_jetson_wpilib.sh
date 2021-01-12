66329724fc11de0ab728ec1e333c70bb998c4a26
Kevin Jaget <kjaget@nc.rr.com>
2021-01-12 09:59:46 -0500

    Various updates to scripts for 2021 release

--- update_jetson_wpilib.sh
+++ update_jetson_wpilib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run me with IP address of the Jetson as argument
-#   wpilib/2020 has to be installed on local machine - run from docker
+#   wpilib/2021 has to be installed on local machine - run from docker
 #   env to make sure that the local build env is correct one
 #   to push to the Jetson
 
@@ -10,13 +10,13 @@ if [ $? -ne 1 ] ; then
 	echo "This script must be run from inside a docker conatiner"
 fi
 
-ssh -p 22 ubuntu@$1 'rm -rf /home/ubuntu/wpilib/2020'
-ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/ctre'
-ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/navx'
-ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/wpilib'
-rsync -avru $HOME/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/ $1:/home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/include/
+ssh -p 22 ubuntu@$1 'rm -rf /home/ubuntu/wpilib/2021'
+ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/include/ctre'
+ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/include/navx'
+ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/include/wpilib'
+rsync -avru $HOME/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/include/ $1:/home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/include/
 
-ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ctre'
-ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/navx'
-ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/wpilib'
-rsync -avzu --exclude '*.debug' --exclude 'athena' $HOME/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/ $1:/home/ubuntu/wpilib/2020/roborio/arm-frc2020-linux-gnueabi/lib/
+ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/lib/ctre'
+ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/lib/navx'
+ssh -p 22 ubuntu@$1 'mkdir -p /home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/lib/wpilib'
+rsync -avzu --exclude '*.debug' --exclude 'athena' $HOME/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/lib/ $1:/home/ubuntu/wpilib/2021/roborio/arm-frc2021-linux-gnueabi/lib/
