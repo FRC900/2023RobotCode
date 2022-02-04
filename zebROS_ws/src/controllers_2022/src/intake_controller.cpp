bd737c33d6759198c7f7e1ea5e9cb642fdae8468
Pranav Narala <56051957+PranavNarala1@users.noreply.github.com>
2022-02-04 18:54:27 -0500

    Created 2022 Intake controller (#22)
    * Adding the IMU node to the teleop launch file
    
    * Making 2022 Intake Controller from 2020
    
    * Formatting the 2022 intake controller file.
    
    * Changed sensor positions on the stage robot inc file
    
    * Making PR requested changes and debugging

---
+++ zebROS_ws/src/controllers_2022/src/intake_controller.cpp
@@ -0,0 +1,139 @@
+#include <ros/ros.h>
+#include <realtime_tools/realtime_buffer.h> //code for real-time buffer - stop multple things writing to same variable at same time
+
+//controller interfaces
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <talon_controllers/talon_controller_interface.h>
+
+
+#include <controllers_2022_msgs/IntakeArmSrv.h>
+#include <controllers_2022_msgs/IntakeRollerSrv.h>
+#include <std_srvs/SetBool.h>
+#include <pluginlib/class_list_macros.h> //to compile as a controller
+
+namespace intake_controller
+{
+class IntakeController : public controller_interface::MultiInterfaceController<hardware_interface::PositionJointInterface, hardware_interface::TalonCommandInterface>
+{
+
+public:
+    IntakeController()
+    {
+    }
+
+    bool init(hardware_interface::RobotHW *hw,
+                                ros::NodeHandle                 &/*root_nh*/,
+                                ros::NodeHandle                 &controller_nh)
+    {
+        hardware_interface::TalonCommandInterface *const talon_command_iface = hw->get<hardware_interface::TalonCommandInterface>();
+        hardware_interface::PositionJointInterface *const pos_joint_iface = hw->get<hardware_interface::PositionJointInterface>();
+
+        //Initialize intake piston joint
+        intake_arm_joint_ = pos_joint_iface->getHandle("intake_arm_joint"); //read from ros_control_boilerplate/config/[insert_year]_compbot_base_jetson.yaml
+
+        //Initialize motor joints
+        //get params from config file
+        XmlRpc::XmlRpcValue intake_params;
+        if ( !controller_nh.getParam("intake_joint", intake_params)) //grabbing the config value under the controller's section in the main config file
+        {
+            ROS_ERROR_STREAM("Could not read intake_params");
+            return false;
+        }
+
+        //initialize motor joint using those config values
+        if (!intake_joint_.initWithNode(talon_command_iface, nullptr, controller_nh, intake_params)) {
+            ROS_ERROR("Cannot initialize intake_joint!");
+            return false;
+        }
+        else
+        {
+            ROS_INFO("Initialized intake joint");
+        }
+
+        //Initialize your ROS server
+        intake_arm_service_ = controller_nh.advertiseService("intake_arm_command", &IntakeController::cmdServiceArm, this);
+        intake_roller_service_ = controller_nh.advertiseService("intake_roller_command", &IntakeController::cmdServiceRoller, this);
+
+        return true;
+    }
+
+    void starting(const ros::Time &/*time*/) {
+        //give command buffer(s) an initial value
+        arm_extend_cmd_buffer_.writeFromNonRT(true);
+        percent_out_cmd_buffer_.writeFromNonRT(0.0);
+
+        forward_disabled_.writeFromNonRT(false);
+    }
+
+    void update(const ros::Time &/*time*/, const ros::Duration &/*period*/) {
+        //grab value from command buffer(s)
+        const bool arm_extend_cmd = *(arm_extend_cmd_buffer_.readFromRT());
+        double arm_extend_double;
+        if(arm_extend_cmd == true) {
+            arm_extend_double = 0.0;
+        }
+        else {
+            arm_extend_double = 1.0;
+        }
+
+        //if moving forwards was disabled by the indexer server, don't allow forward movement
+        double percent_out_cmd = *percent_out_cmd_buffer_.readFromRT();
+        /*if(*forward_disabled_.readFromRT() && percent_out_cmd > 0)
+          {
+               percent_out_cmd = 0.0;
+             }*/
+
+        intake_joint_.setCommand(percent_out_cmd);
+        intake_arm_joint_.setCommand(arm_extend_double);
+    }
+
+    void stopping(const ros::Time &/*time*/) {
+    }
+
+private:
+
+    talon_controllers::TalonPercentOutputControllerInterface intake_joint_;//intake for intake motor
+    hardware_interface::JointHandle intake_arm_joint_;//interface for intake arm solenoid
+    realtime_tools::RealtimeBuffer<bool> arm_extend_cmd_buffer_;
+    realtime_tools::RealtimeBuffer<double> percent_out_cmd_buffer_;
+    ros::ServiceServer intake_arm_service_;
+    ros::ServiceServer intake_roller_service_;
+
+    realtime_tools::RealtimeBuffer<bool> forward_disabled_; //set to true by the indexer server when it's finishing up properly storing a ball, to ensure the proper gap
+
+
+    bool cmdServiceArm(controllers_2022_msgs::IntakeArmSrv::Request &req, controllers_2022_msgs::IntakeArmSrv::Response &/*response*/) {
+        if(isRunning())
+        {
+            //assign request value to command buffer(s)
+            arm_extend_cmd_buffer_.writeFromNonRT(req.intake_arm_extend);
+        }
+        else
+        {
+            ROS_ERROR_STREAM("Can't accept new commands. IntakeController is not running.");
+            return false;
+        }
+        return true;
+    }
+
+    bool cmdServiceRoller(controllers_2022_msgs::IntakeRollerSrv::Request &req, controllers_2022_msgs::IntakeRollerSrv::Response &/*response*/) {
+        if(isRunning())
+        {
+            //assign request value to command buffer(s)
+            percent_out_cmd_buffer_.writeFromNonRT(req.percent_out);
+        }
+        else
+        {
+            ROS_ERROR_STREAM("Can't accept new commands. IntakeController is not running.");
+            return false;
+        }
+        return true;
+    }
+
+
+}; //class
+}//namespace
+
+//DON'T FORGET TO EXPORT THE CLASS SO CONTROLLER_MANAGER RECOGNIZES THIS AS A TYPE
+PLUGINLIB_EXPORT_CLASS(intake_controller::IntakeController, controller_interface::ControllerBase)
