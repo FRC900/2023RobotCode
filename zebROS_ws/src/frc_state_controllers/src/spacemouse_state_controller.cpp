4fb04dc07cb9678367c1154c5176e3c14e376a2d
Kevin Jaget <kjaget@gmail.com>
2022-07-11 16:52:41 -0400

    Add state controller to publish spacemouse axises  (#167)
    * Add state controller to publish spacemouse axises mapped as
    if they were gamepad sticks
    
    * Tested on robot in a box
    
    * Fix axis assignments and inverts (not sure it matters, the thing
    	is radially symmetric)

---
+++ zebROS_ws/src/frc_state_controllers/src/spacemouse_state_controller.cpp
@@ -0,0 +1,144 @@
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <realtime_tools/realtime_buffer.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <frc_msgs/JoystickState.h>
+#include <frc_interfaces/joystick_interface.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace spacemouse_state_controller
+{
+class SpacemouseStateController: public controller_interface::Controller<hardware_interface::JoystickStateInterface>
+{
+bool init(hardware_interface::JoystickStateInterface *hw,
+		  ros::NodeHandle							 &root_nh,
+		  ros::NodeHandle							 &controller_nh) override
+{
+	ROS_INFO_STREAM_NAMED("spacemouse_state_controller", "init is running");
+	std::string name;
+	if (!controller_nh.getParam("name", name))
+	{
+		ROS_ERROR("Could not read joystick name parameter in Spacemouse State Controller");
+		return false;
+	}
+
+	std::vector<std::string> joystick_names = hw->getNames();
+	const auto it = std::find(joystick_names.begin(), joystick_names.end(), name);
+	if (it == joystick_names.cend())
+	{
+		ROS_ERROR_STREAM("Could not find requested name " << name << " in joystick interface list");
+		return false;
+	}
+	joystick_state_ = hw->getHandle(*it);
+	const auto id = joystick_state_->getId();
+	std::stringstream pub_name;
+	// TODO : maybe use pub_names instead, or joy id unconditionally?
+	pub_name << "js" << id;
+
+	realtime_pub_.reset(new realtime_tools::RealtimePublisher<frc_msgs::JoystickState>(root_nh, pub_name.str(), 1));
+
+	if (!controller_nh.getParam("publish_rate", publish_rate_))
+		ROS_WARN_STREAM("Could not read publish_rate in Spacemouse State Controller, using default " << publish_rate_);
+
+	return true;
+}
+
+void starting(const ros::Time &time) override
+{
+	last_publish_time_ = time;
+}
+
+void update(const ros::Time &time, const ros::Duration &period) override
+{
+	if (period < ros::Duration{0})
+	{
+		last_publish_time_ = time;
+	}
+	if ((publish_rate_ > 0.0) && (last_publish_time_ + ros::Duration(1.0 / publish_rate_) < time))
+	{
+		if (realtime_pub_->trylock())
+		{
+			last_publish_time_ = time;
+
+			const auto &js = joystick_state_;
+			auto &m = realtime_pub_->msg_;
+
+			m.header.stamp = time;
+
+			m.leftStickX         = -js->getAxis(1);
+			m.leftStickY         = js->getAxis(0);
+			m.leftTrigger        = 0;
+			m.rightTrigger       = 0;
+			m.rightStickX        = js->getAxis(5);
+			m.rightStickY        = 0;
+
+			m.buttonAButton      = false;
+			m.buttonBButton      = false;
+			m.buttonXButton      = false;
+			m.buttonYButton      = false;
+			m.bumperLeftButton   = js->getAxis(2) < 0.75; // hack - default to slow mode on z-axis push?
+			m.bumperRightButton  = false;
+			m.buttonBackButton   = false;
+			m.buttonStartButton  = false;
+			m.stickLeftButton    = false;
+			m.stickRightButton   = false;
+
+			m.buttonAPress       = false;
+			m.buttonBPress       = false;
+			m.buttonXPress       = false;
+			m.buttonYPress       = false;
+			m.bumperLeftPress    = !prev_left_bumper_ && m.bumperLeftButton;
+			m.bumperRightPress   = false;
+			m.buttonBackPress    = false;
+			m.buttonStartPress   = false;
+			m.stickLeftPress     = false;
+			m.stickRightPress    = false;
+
+			m.buttonARelease     = false;
+			m.buttonBRelease     = false;
+			m.buttonXRelease     = false;
+			m.buttonYRelease     = false;
+			m.bumperLeftRelease  = prev_left_bumper_ && !m.bumperLeftButton;
+			m.bumperRightRelease = false;
+			m.buttonBackRelease  = false;
+			m.buttonStartRelease = false;
+			m.stickLeftRelease   = false;
+			m.stickRightRelease  = false;
+
+			m.directionUpButton  = false;
+			m.directionUpPress   = false;
+			m.directionUpRelease = false;
+
+			m.directionDownButton  = false;
+			m.directionDownPress   = false;
+			m.directionDownRelease = false;
+
+			m.directionLeftButton  = false;
+			m.directionLeftPress   = false;
+			m.directionLeftRelease = false;
+
+			m.directionRightButton  = false;
+			m.directionRightPress   = false;
+			m.directionRightRelease = false;
+
+			realtime_pub_->unlockAndPublish();
+
+			prev_left_bumper_ = m.bumperLeftButton;
+		}
+	}
+}
+
+void stopping(const ros::Time & ) override
+{}
+
+private:
+		hardware_interface::JoystickStateHandle joystick_state_;
+		std::shared_ptr<realtime_tools::RealtimePublisher<frc_msgs::JoystickState>> realtime_pub_;
+		ros::Time last_publish_time_;
+		double publish_rate_{50};
+		bool prev_left_bumper_{false};
+}; // class
+
+} // namespace
+
+PLUGINLIB_EXPORT_CLASS(spacemouse_state_controller::SpacemouseStateController, controller_interface::ControllerBase)
