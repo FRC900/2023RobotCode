a75565113eec1a1f2048a6e91e88a45f2840747e
Kevin Jaget <kjaget@gmail.com>
2022-11-12 10:16:04 -0500

    Fix for post-processing compile_commands.json

--- zebROS_ws/merge_compile_commands.sh
+++ zebROS_ws/merge_compile_commands.sh
@@ -3,6 +3,6 @@ rm -f ~/2022RobotCode/zebROS_ws/build/compile_commands.json
 printf '[' > ~/2022RobotCode/zebROS_ws/compile_commands.json
 find ~/2022RobotCode/zebROS_ws/build -type f -name 'compile_commands.json' -exec sh -c "cat {} | tail -n+2 | head -n-1 && printf ','" >> compile_commands.json \;
 sed -i '$s/.$//' ~/2022RobotCode/zebROS_ws/compile_commands.json
-sed -i 's/.2022RobotCode.readonly/2022RobotCode/' ~/2022RobotCode/zebROS_ws/compile_commands.json
+sed -i 's/.2022RobotCode.readonly/2022RobotCode/g' ~/2022RobotCode/zebROS_ws/compile_commands.json
 printf '\n]\n' >> ~/2022RobotCode/zebROS_ws/compile_commands.json
 mv ~/2022RobotCode/zebROS_ws/compile_commands.json ~/2022RobotCode/zebROS_ws/build 
