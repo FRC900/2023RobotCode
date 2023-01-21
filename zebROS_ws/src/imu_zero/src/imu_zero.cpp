4c7708e63ac6a2260a6dc7a149291b4efc8c8805
Kevin Jaget <kjaget@gmail.com>
2023-01-21 10:21:49 -0500

    Update imu zero to transform from imu frame to base link (#196)
    - add transform code
     - fix case where multiple calls to zero service were required to zero
     - add test node

--- zebROS_ws/src/imu_zero/src/imu_zero.cpp
+++ zebROS_ws/src/imu_zero/src/imu_zero.cpp
@@ -16,20 +16,25 @@ Other IMU data is passed through.
 Zero point in degrees is set using service call.
 */
 
+#include <string>
 
 #include <ros/ros.h>
+#include <angles/angles.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <sensor_msgs/Imu.h>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <string>
+#include "tf2_sensor_msgs/tf2_sensor_msgs.h"
+
+#include <tf2_ros/transform_listener.h>
+
 #include <imu_zero/ImuZeroAngle.h>
 #include <std_srvs/Trigger.h>
 #include <robot_localization/SetPose.h>
 #include <zed_interfaces/reset_tracking.h>
 #include <zed_interfaces/reset_odometry.h>
 
-constexpr double pi = 3.14159;
 const std::string sub_topic = "imu/data";
 const std::string pub_topic = "zeroed_imu";
 const std::string service_name = "set_imu_zero";
@@ -45,38 +50,120 @@ ros::ServiceClient ukf_zero_pos;
 ros::ServiceClient zed_reset_odometry;
 ros::ServiceClient zed_reset_tracking;
 
+tf2_ros::Buffer tfBuffer;
+std::unique_ptr<tf2_ros::TransformListener> tfListener;
 
-double degToRad(double deg) {
-  const double rad = (deg / 180) * pi;
-  return rad;
+// From https://github.com/ros-perception/imu_pipeline/blame/noetic-devel/imu_transformer/include/imu_transformer/tf2_sensor_msgs.h
+namespace tf2
+{
+
+/**********/
+/** IMU  **/
+/**********/
+
+/**
+* method to extract timestamp from object
+*/
+/**
+* Transforms a covariance array from one frame to another
+*/
+  inline
+  void transformCovariance(const boost::array<double, 9>& in, boost::array<double, 9>& out, Eigen::Quaternion<double> r){
+
+    Eigen::Map<const Eigen::Matrix<double, 3, 3, Eigen::RowMajor> > cov_in(in.data());
+    Eigen::Map<Eigen::Matrix<double, 3, 3, Eigen::RowMajor> > cov_out(out.data());
+    cov_out = r * cov_in * r.inverse();
+
+  }
+
+/**
+* Transforms sensor_msgs::Imu data from one frame to another
+*/
+  template <>
+  inline
+  void doTransform(const sensor_msgs::Imu &imu_in, sensor_msgs::Imu &imu_out, const geometry_msgs::TransformStamped& t_in)
+  {
+
+    imu_out.header = t_in.header;
+
+    // Discard translation, only use orientation for IMU transform
+    Eigen::Quaternion<double> r(
+        t_in.transform.rotation.w, t_in.transform.rotation.x, t_in.transform.rotation.y, t_in.transform.rotation.z);
+    Eigen::Transform<double,3,Eigen::Affine> t(r);
+
+    Eigen::Vector3d vel = t * Eigen::Vector3d(
+        imu_in.angular_velocity.x, imu_in.angular_velocity.y, imu_in.angular_velocity.z);
+
+    imu_out.angular_velocity.x = vel.x();
+    imu_out.angular_velocity.y = vel.y();
+    imu_out.angular_velocity.z = vel.z();
+
+    transformCovariance(imu_in.angular_velocity_covariance, imu_out.angular_velocity_covariance, r);
+
+    Eigen::Vector3d accel = t * Eigen::Vector3d(
+        imu_in.linear_acceleration.x, imu_in.linear_acceleration.y, imu_in.linear_acceleration.z);
+
+
+    imu_out.linear_acceleration.x = accel.x();
+    imu_out.linear_acceleration.y = accel.y();
+    imu_out.linear_acceleration.z = accel.z();
+
+    transformCovariance(imu_in.linear_acceleration_covariance, imu_out.linear_acceleration_covariance, r);
+
+    // Orientation expresses attitude of the new frame_id in a fixed world frame. This is why the transform here applies
+    // in the opposite direction.
+    Eigen::Quaternion<double> orientation = Eigen::Quaternion<double>(
+        imu_in.orientation.w, imu_in.orientation.x, imu_in.orientation.y, imu_in.orientation.z) * r.inverse();
+
+    imu_out.orientation.w = orientation.w();
+    imu_out.orientation.x = orientation.x();
+    imu_out.orientation.y = orientation.y();
+    imu_out.orientation.z = orientation.z();
+
+    // Orientation is measured relative to the fixed world frame, so it doesn't change when applying a static
+    // transform to the sensor frame.
+    imu_out.orientation_covariance = imu_in.orientation_covariance;
+
+  }
+
 }
 
-double getYaw(const geometry_msgs::Quaternion &q)
+double getYaw(const tf2::Quaternion &q)
 {
 	double roll, pitch, yaw;
-	tf2::Quaternion tf_q(
-		q.x,
-		q.y,
-		q.z,
-		q.w);
-	tf2::Matrix3x3(tf_q).getRPY(roll, pitch, yaw);
+	tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
 	return yaw;
 }
 
 void zeroCallback(const sensor_msgs::Imu::ConstPtr& raw_msg) {
-  tf2::convert(raw_msg -> orientation, last_raw);
+  geometry_msgs::TransformStamped transformStamped;
+  try
+  {
+    transformStamped = tfBuffer.lookupTransform("base_link", raw_msg->header.frame_id, ros::Time(0));
+  }
+  catch (tf2::TransformException &ex)
+  {
+    ROS_WARN_STREAM_THROTTLE(1.0, "imu_zero : error looking up transform from "
+                               << raw_msg->header.frame_id
+                               << " to base_link : "
+                               << ex.what());
+    return;
+  }
+  sensor_msgs::Imu zeroed_imu = *raw_msg;
+  tf2::doTransform(zeroed_imu, zeroed_imu, transformStamped);
+  tf2::convert(zeroed_imu.orientation, last_raw);
   tf2::Quaternion zeroed = zero_rot * last_raw;
   zeroed.normalize();
-  sensor_msgs::Imu zeroed_imu = *raw_msg;
+
   zeroed_imu.orientation = tf2::toMsg(zeroed);
-  if (!std::isfinite(getYaw(zeroed_imu.orientation)))
+  if (!std::isfinite(getYaw(zeroed)))
   {
 	  ROS_WARN_STREAM_THROTTLE(5., "zeroCallback : NaN yaw result"
 			  << "\n\traw_msg.orientation = " << raw_msg->orientation
+			  << "\n\tzeroed_imu.orientation = " << zeroed_imu.orientation
 			  << "\n\tlast_raw = " << last_raw[0] << " " << last_raw[1] << " " << last_raw[2] << " " << last_raw[3]
 			  << "\n\tzero_rot = " << zero_rot[0] << " " << zero_rot[1] << " " << zero_rot[2] << " " << zero_rot[3]
-			  << "\n\tzeroed = " << zeroed[0] << " " << zeroed[1] << " " << zeroed[2] << " " << zeroed[3]
-			  << "\n\tzeroed_imu.orientation = " << zeroed_imu.orientation);
+			  << "\n\tzeroed = " << zeroed[0] << " " << zeroed[1] << " " << zeroed[2] << " " << zeroed[3]);
   }
   else
   {
@@ -91,29 +178,38 @@ bool zeroSet(imu_zero::ImuZeroAngle::Request& req,
 
   // takes zero angle in degrees, converts to radians
   ROS_INFO_STREAM("Setting IMU zero angle = " << req.angle << " degrees");
-  const double a = degToRad(req.angle);
+  const double a = angles::from_degrees(req.angle);
 
-  zero_rot.setRPY(0.0, 0.0, a - yaw);
+  zero_rot.setRPY(0.0, 0.0, angles::shortest_angular_distance(yaw, a));
+  zero_rot.normalize();
 
   if(bias_estimate.exists())
   {
-	std_srvs::Trigger biasCall;
-	if (!bias_estimate.call(biasCall))
-		ROS_ERROR("imu_zero : bias_estimate call failed");
-	ROS_INFO("Bias estimate: %s", biasCall.response.message.c_str());
+    std_srvs::Trigger biasCall;
+    if (!bias_estimate.call(biasCall))
+    {
+      ROS_ERROR("imu_zero : bias_estimate call failed");
+    }
+    ROS_INFO("Bias estimate: %s", biasCall.response.message.c_str());
   }
-  if(zed_reset_odometry.exists())
+  #if 0
+  if (zed_reset_odometry.exists())
   {
-  zed_interfaces::reset_odometry resetOdomCall;
-	if (!zed_reset_odometry.call(resetOdomCall))
-		ROS_ERROR("imu_zero : zed_reset_odometry call failed");
+    zed_interfaces::reset_odometry resetOdomCall;
+    if (!zed_reset_odometry.call(resetOdomCall))
+    {
+      ROS_ERROR("imu_zero : zed_reset_odometry call failed");
+    }
   }
-  if(zed_reset_tracking.exists())
+  if (zed_reset_tracking.exists())
   {
-  zed_interfaces::reset_tracking resetTrackingCall;
-	if (!zed_reset_tracking.call(resetTrackingCall))
-		ROS_ERROR("imu_zero : zed_reset_tracking call failed");
+    zed_interfaces::reset_tracking resetTrackingCall;
+    if (!zed_reset_tracking.call(resetTrackingCall))
+    {
+      ROS_ERROR("imu_zero : zed_reset_tracking call failed");
+    }
   }
+  #endif
   if (ukf_zero_pos.exists())
   {
 	  robot_localization::SetPose zeroPose;
@@ -137,6 +233,7 @@ bool zeroSet(imu_zero::ImuZeroAngle::Request& req,
 int main(int argc, char* argv[]) {
   ros::init(argc, argv, "imu_zero_node");
   ros::NodeHandle node;
+  tfListener = std::make_unique<tf2_ros::TransformListener>(tfBuffer);
   pub = node.advertise<sensor_msgs::Imu>(pub_topic, 1);
   ros::Subscriber sub = node.subscribe(sub_topic, 1, zeroCallback);
   ros::ServiceServer svc = node.advertiseService(service_name, zeroSet);
