83ec7ad4f2a96c039143fcdec63b577766c58425
BenBean18 <beakerthespeaker@gmail.com>
2023-02-15 20:25:59 -0500

    recognizing cones/cubes works pretty well!

--- zebROS_ws/src/behaviors/src/2023_game_piece_state.py
+++ zebROS_ws/src/behaviors/src/2023_game_piece_state.py
@@ -1,78 +1,85 @@
+#!/usr/bin/env python3
 import rospy
 import sensor_msgs.msg
 import frc_msgs.msg
 import std_msgs.msg
 from message_filters import ApproximateTimeSynchronizer, Subscriber as SyncSubscriber
-import behavior_actions.msg
+from behavior_actions.msg import *
 import math
 from enum import Enum
 
-
-class GamePieces(Enum):
-
-    CUBE=0
-    VERTICAL_CONE=1
-    BASE_TOWARDS_US_CONE=2 
-    BASE_AWAY_US_CONE=3
-    NONE=4 # for sure no game piece
-    UNKNOWN=5 # we have a game piece, but not sure what it is right now
-
+from dynamic_reconfigure.server import Server
+from behaviors.cfg import GamePieceState2023Config
 
 class GamePieceState:
 
     def __init__(self) -> None:
-        self.state = GamePieces.NONE # not actually true, we will start with one, something in auto will update this
+        self.state = GamePieceState2023.NONE
         self.offset_from_center = math.inf
 
-        # CHECK TOPICS
         self.button_box_sub = rospy.Subscriber("/frcrobot_jetson/button_box_state_controller", frc_msgs.msg.ButtonBoxState, queue_size=1)
-        # for real robot
-        self.left_intake_sub = SyncSubscriber("/frcrobot_jetson/intake_terabee_left_x", sensor_msgs.msg.Range)
-        self.right_intake_sub = SyncSubscriber("/frcrobot_jetson/intake_terabee_right_x", sensor_msgs.msg.Range)
-        # for sim
-        self.left_intake_sub = SyncSubscriber("/terabee/terabee_x", sensor_msgs.msg.Range)
-        self.right_intake_sub = SyncSubscriber("/terabee/terabee_y", sensor_msgs.msg.Range)
-        ats = ApproximateTimeSynchronizer([self.left_intake_sub, self.right_intake_sub], queue_size=1, slop=0.1)
-        ats.registerCallback(self.terabeeCB)
+        
+        self.terabees_sub = rospy.Subscriber("/intake/reader/state", IntakeState2023, callback=self.terabeeCB, queue_size=1)
 
-        self.game_piece_pub = rospy.Publisher("game_piece_state", behavior_actions.msg.GamePieceState2023, queue_size=1)
-        self.game_piece_offset_pub = rospy.Publisher("game_piece_center_offset", std_msgs.msg.Float64, queue_size=1) 
+        self.game_piece_pub = rospy.Publisher("game_piece_state", GamePieceState2023, queue_size=1)
+        self.game_piece_offset_pub = rospy.Publisher("game_piece_center_offset", std_msgs.msg.Float64, queue_size=1)
+
+        server = Server(GamePieceState2023Config, self.drCallback)
+
+        # meters
+        self.intake_width = rospy.get_param("intake_width")
+        self.cube_threshold = rospy.get_param("cube_threshold")
+        rospy.loginfo(f"game_piece_state_2023 : intake_width is {self.intake_width}, cube_threshold is {self.cube_threshold}")
+
+        server.update_configuration({"intake_width": self.intake_width, "cube_threshold": self.cube_threshold})
 
     def publish_states(self):
-        fmsg = std_msgs.msg.Float64()
-        fmsg.data = self.offset_from_center
-        self.game_piece_offset_pub.publish(fmsg)
+        gpmsg = GamePieceState2023()
+        gpmsg.header.frame_id = "intake"
+        gpmsg.header.stamp = rospy.Time.now()
+        gpmsg.offset_from_center = self.offset_from_center
+        gpmsg.game_piece = self.state
+        self.game_piece_pub.publish(gpmsg)
 
-        gpmsg = behavior_actions.msg.GamePieceState2023()
-        self.game_piece_pub()
+    def drCallback(self, config, level):
+        self.intake_width = config.intake_width
+        self.cube_threshold = config.cube_threshold
+        return config
 
-    def terabeeCB(self, left: sensor_msgs.msg.Range, right: sensor_msgs.msg.Range):
-        rospy.loginfo("Terabee callback in 2023 game piece state")
-        for msg in (left, right):
-            if msg.range == 0 or not math.isfinite(msg.range):
-                self.offset_from_center = math.inf
-                self.state = GamePieces.NONE
-                self.publish_states()
-                self.game_piece_offset_pub.publish(self.offset_from_center)
-                return
-            
-        # right x is positive, left is negative
-        self.offset_from_center = left.range - right.range
-        self.game_piece_offset_pub.publish(self.offset_from_center)
+    def terabeeCB(self, msg: IntakeState2023):
+        if msg.leftDistance != msg.leftDistance or msg.rightDistance != msg.rightDistance:
+            self.offset_from_center = math.nan
+            self.state = GamePieceState2023.NONE
+            self.publish_states()
+        else:
+            # ex.       1
+            # 0.........0
+            # ..2...6....
+            # ->2   4<---
+            # left reading = 2
+            # right reading = 4
+            # both relative to left = 2, 6
+            # average is 4
+            # center of intake is 5 (10/2)
+            # return center - average (positive is left) = 1
+
+            width_of_object = max((self.intake_width - msg.rightDistance), msg.leftDistance) - min((self.intake_width - msg.rightDistance), msg.leftDistance)
+            if width_of_object < self.cube_threshold:
+                self.state = GamePieceState2023.BASE_TOWARDS_US_CONE # actually it's any cone (I think vertical looks the same as base towards) but we have so many cone states
+            else:
+                self.state = GamePieceState2023.CUBE
+
+            self.offset_from_center = (self.intake_width / 2) - (((self.intake_width - msg.rightDistance) + msg.leftDistance) / 2)
+            self.publish_states()
 
     def buttonBoxCB(self, button_box):
+        pass
         
     
 if __name__ == "__main__":
     print("started")
-    rospy.init_node("game_piece_state_2023")
+    rospy.init_node("game_piece_state_2023", anonymous=True)
     rospy.loginfo("Starting 2023 game piece state server!")
     game_piece_state = GamePieceState()
-    r = rospy.Rate(10)
-    msg = std_msgs.msg.Float64()
-    msg.data = math.inf 
-    while True:
-        game_piece_state.game_piece_offset_pub.publish(msg)
-        r.sleep()
     rospy.spin()
     rospy.loginfo("Successfully started: 2023 game piece state server!")
