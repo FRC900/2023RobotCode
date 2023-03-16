8690bfd5988c41349845d7323ca3d29ccd86cf42
BenBean18 <beakerthespeaker@gmail.com>
2023-03-16 11:13:48 -0400

    teensy terabees work!

--- scripts/jetson_install/setup_environment.sh
+++ scripts/jetson_install/setup_environment.sh
@@ -214,7 +214,7 @@ sudo chmod 640 /root/.ssh/authorized_keys
 sudo chmod 700 /root/.ssh
 
 cd ~/2023RobotCode/scripts
-sudo cp ./jetson_setup/10-local.rules ./jetson_setup/99-gpio.rules ./jetson_setup/99-terabee-pico.rules /etc/udev/rules.d/
+sudo cp ./jetson_setup/10-local.rules ./jetson_setup/99-gpio.rules ./jetson_setup/99-terabee-pico.rules ./jetson_setup/99-terabee-teensy.rules /etc/udev/rules.d/
 sudo service udev reload
 sleep 2
 sudo service udev restart
