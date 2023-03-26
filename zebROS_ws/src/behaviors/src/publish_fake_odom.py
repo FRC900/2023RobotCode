6f1563fde5ba730a80693e6354c9468190dae905
BenBean18 <beakerthespeaker@gmail.com>
2023-03-26 13:46:43 -0400

    use actual zed transform for fake odom publisher

--- zebROS_ws/src/behaviors/src/publish_fake_odom.py
+++ zebROS_ws/src/behaviors/src/publish_fake_odom.py
@@ -2,6 +2,8 @@
 import rospy
 import tf2_ros
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseStamped
+import tf2_geometry_msgs
 
 rospy.init_node('fake_odom_pub')
 
@@ -14,12 +16,12 @@ pub = rospy.Publisher("/fake_odometry", Odometry, queue_size=1)
 import time
 time.sleep(0.25)
 
-# run this: rosrun tf2_ros static_transform_publisher 0.1 0.1 0.0 0.0 0.0 0.0 base_link odom_frame
+# need to publish zed urdf at the same time
 while not rospy.is_shutdown():
-    trans = tfBuffer.lookup_transform("base_link", "odom", rospy.Time(), rospy.Duration(0.1))
+    trans = tfBuffer.lookup_transform("odom", "zed_objdetect_base_link", rospy.Time(), rospy.Duration(0.1))
     msg = Odometry()
     msg.header.frame_id = "odom"
-    msg.child_frame_id = "odom_frame"
+    msg.child_frame_id = "zed_objdetect_base_link"
     msg.pose.pose.position.x = trans.transform.translation.x
     msg.pose.pose.position.y = trans.transform.translation.y
     msg.pose.pose.position.z = trans.transform.translation.z
