441a7a386796ae6415b24f4efe0ab04b1f944d06
BenBean18 <beakerthespeaker@gmail.com>
2023-04-15 16:23:02 -0400

    more bag scripts

--- scripts/dump_rosout.py
+++ scripts/dump_rosout.py
@@ -7,18 +7,14 @@ import sys
 #Bagfile name from command line
 bagfile_name = sys.argv[1]
 
+node = sys.argv[2]
+
 bag = rosbag.Bag(bagfile_name)
 
 out = "/rosout"
-goal = "/align_and_place_grid/goal"
-auto = False
 
-last_match_time = 0
-
-for topic, msg, t in bag.read_messages([out, goal]):
+for topic, msg, t in bag.read_messages([out]):
     m = message_converter.convert_ros_message_to_dictionary(msg)
     if topic == out:
-        if "align_to_grid" in m["file"]:
-            print(f'{m["msg"]} at {t.to_sec()} = {last_match_time}. auto = {auto}')
-    elif topic == goal:
-        print(f"{m['goal']['grid_id']} sent ****************************************************************")
+        if node in m["file"]:
+            print(f'{m["msg"]} at {t.to_sec()}')
