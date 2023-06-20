8ca5b237fbc91aaa20a37fa4609d4fec8cf9e1ca
Kevin Jaget <kjaget@nc.rr.com>
2023-06-20 15:08:15 -0400

    Fixes after testing

--- docker/new-install.sh
+++ docker/new-install.sh
@@ -4,52 +4,45 @@
 # Should only have to run this once
 
 sudo apt update
-sudo apt upgrade
-sudo apt install -y terminator
 
 # Docker setup
-#sudo apt remove docker docker-engine docker.io containerd runc
-#sudo apt install -y \
-    #apt-transport-https \
-    #ca-certificates \
-    #curl \
-    #gnupg-agent \
-    #software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository \
-   #"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   #$(lsb_release -cs) \
-   #stable"
-#sudo apt update
-#sudo apt install -y docker-ce docker-ce-cli containerd.io
-
+sudo apt install -y curl
 curl https://get.docker.com | sh
 sudo systemctl --now enable docker
 
 # Allow non-root users to run docker commands
 sudo gpasswd -a $USER docker
 
+# Random updates
+sudo apt upgrade -y
+sudo apt install -y terminator
+
 # log out, log back in (probably also need a restart anyway after apt upgrade)
 
 # Install git-lfs
+sudo apt install -y wget
 cd &&\
     wget https://github.com/git-lfs/git-lfs/releases/download/v3.2.0/git-lfs-linux-amd64-v3.2.0.tar.gz &&\
 	mkdir git-lfs-install &&\
 	cd git-lfs-install &&\
 	tar -xzf ../git-lfs-linux-amd64-v3.2.0.tar.gz &&\
+	cd git-lfs-3.2.0 &&\
 	sudo ./install.sh &&\
 	cd &&\
 	rm -rf git-lfs-linux-amd64-v3.2.0.tar.gz git-lfs-install &&\
 	git lfs install
 
+# Set up student's git ssh keys here
+
 # Install repo - perhaps ssh version inside container, worry about SSH keys?
 cd 
 git clone https://github.com/FRC900/2023RobotCode.git
 cd 2023RobotCode
-git submodule update --init --recursive
-docker run --net=host -v  /tmp/.X11-unix:/tmp/.X11-unix \
- -v $HOME/2023RobotCode:/home/ubuntu/2023RobotCode \
+git submodule update --init
+docker run --net=host -v /tmp/.X11-unix:/tmp/.X11-unix \
+ -v $HOME/2023RobotCode:/home/ubuntu/.2023RobotCode.readonly \
+ --ipc=host \
+ --shm-size=8G \
  -e DISPLAY=$DISPLAY --privileged --user ubuntu frc900/zebros-2023-dev:latest "wstool update -t /home/ubuntu/2023RobotCode/zebROS_ws/src -j2"
 
-sudo apt remove thunderbird
 sudo apt autoremove
