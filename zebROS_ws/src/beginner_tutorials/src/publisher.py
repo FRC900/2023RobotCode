c6054f4d5c4e76dbe22dcc480752f2ee63efeb5a
BenBean18 <beakerthespeaker@gmail.com>
2023-10-17 10:29:01 -0400

    Add publisher
    (from the October 16 meeting)

---
+++ zebROS_ws/src/beginner_tutorials/src/publisher.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import rospy
+from beginner_tutorials.msg import ZebraData
+
+def main():
+    pub = rospy.Publisher("/clara/jeremy/claremy", ZebraData, queue_size=1)
+    rospy.init_node("joeren", anonymous=True)
+
+    while not rospy.is_shutdown():
+        message = ZebraData()
+        message.num_stripes = int(input("What is the number of stripes? "))
+        message.has_horn = (input("Does the zebra have a horn (y/n)?") == "y")
+        message.location.position.x = 0.0
+        message.location.position.y = 0.0
+        message.location.position.z = 0.0
+        message.name = input("What's its name? ")
+        
+        pub.publish(message)
+
+        print("Published message, yay!")
+
+main()
