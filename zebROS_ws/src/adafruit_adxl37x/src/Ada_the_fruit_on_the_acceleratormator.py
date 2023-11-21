3bc4e99d2276a5653740850dd7aae8cb6c864689
Kevin Jaget <kjaget@nc.rr.com>
2023-11-21 11:06:37 -0500

    fix code for enabling full sensor range

--- zebROS_ws/src/adafruit_adxl37x/src/Ada_the_fruit_on_the_acceleratormator.py
+++ zebROS_ws/src/adafruit_adxl37x/src/Ada_the_fruit_on_the_acceleratormator.py
@@ -17,11 +17,8 @@ def rezero_accelerator(req):
 pub = rospy.Publisher("vroom",Imu,queue_size=1)
 s = rospy.Service('Zane', Empty, rezero_accelerator)
 
-
 rospy.init_node("vroom", anonymous=True)
 
-
-import time
 import board
 import adafruit_adxl37x
 from adafruit_adxl34x import DataRate
@@ -31,25 +28,19 @@ global accelerometer
 accelerometer = adafruit_adxl37x.ADXL375(i2c)
 accelerometer.data_rate  = DataRate.RATE_800_HZ
 
+_REG_DATA_FORMAT: int = 0x31
 
-
-
-
-
-_REG_DATA_FORMAT: int = const(0x31)
-
-_INT_ACT: int = const(0b00010000)
+#_INT_ACT: int = 0b00010000
 
 #so this maybe?
 active_change = accelerometer._read_register_unpacked(_REG_DATA_FORMAT) 
 #accelerometer._write_resgiter_byte(_REG_INT_ENABLE, 0x0) #disables interrupts, so that we can write new things to registers without messing other things up?
-accelerometer._write_register_byte(_REG_DATA_FORMAT, 0b00001011) #0b00001011 comes from the adafruit arduino library linked by ty which supposedly fixes the bits at _reg_data_format for 25+ g
-accelerometer._write_register_byte(_REG_DATA_FORMAT, _INT_ACT) # ACT bit
-active_change |= _INT_ACT
-accelerometer._write_register_byte(_REG_DATA_FORMAT, active_change)
+#accelerometer._write_register_byte(_REG_DATA_FORMAT, 0b00001011) #0b00001011 comes from the adafruit arduino library linked by ty which supposedly fixes the bits at _reg_data_format for 25+ g
+#accelerometer._write_register_byte(_REG_DATA_FORMAT, _INT_ACT) # ACT bit
+#active_change |= _INT_ACT
+accelerometer._write_register_byte(_REG_DATA_FORMAT, active_change | 0b00001011) #0b00001011 comes from the adafruit arduino library linked by ty which supposedly fixes the bits at _reg_data_format for 25+ g
 
 r = rospy.Rate(800)
-
 
 while not rospy.is_shutdown():
     message=Imu()
@@ -60,11 +51,3 @@ while not rospy.is_shutdown():
     message.linear_acceleration.z = z
     pub.publish(message)
     r.sleep()
-
-    
-
-
-
-
-
-
