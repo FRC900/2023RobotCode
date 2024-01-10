cfb61759c624c14a5a3c212faa97ee3ae22720e9
May <MejeC10@gmail.com>
2024-01-10 18:41:17 -0500

    Update path follower to use velocity

--- zebROS_ws/src/path_follower/src/path_follower.cpp
+++ zebROS_ws/src/path_follower/src/path_follower.cpp
@@ -1,13 +1,17 @@
-/* 1. information you have: current position and velcocity of the robot, waypoints already limited for velocity and acceleration
+/* 1. information you have: current position and velocity of the robot, waypoints already limited for velocity and acceleration
  * 2. find path point closest to the position of the robot
  * 3. find the goal point -- that is, point which the robot should drive to (based on time_offset setting to grab a target point in the future)
  */
 #include "path_follower/path_follower.h"
 #include "ros/ros.h"
 
-bool PathFollower::loadPath(const nav_msgs::Path &path, double &final_distace)
+bool PathFollower::loadPath(const nav_msgs::Path &path, const nav_msgs::Path &velocites, double &final_distace)
 {
 	path_ = path;
+	velocities_ = velocities;
+	if (path_.poses.size() != velocities_.poses.size()) {
+		return false;
+	}
 	num_waypoints_ = path_.poses.size();
 	if (num_waypoints_ == 0)
 	{
@@ -63,20 +67,13 @@ double PathFollower::interpolate(double start_t, double end_t, double start_x, d
 // position and passing it in to run. run would then return a pose (x_pos, y_pos,
 // orientation) and whoever called run would be responsible for sending that
 // where it needs to go.
-geometry_msgs::Pose PathFollower::run(double &total_distance_travelled, int &current_index)
+
+std::optional<PositionVelocity> PathFollower::run(double &total_distance_travelled, int &current_index)
 {
 	if (num_waypoints_ == 0)
 	{
 		ROS_ERROR_STREAM("No waypoints in path");
-		geometry_msgs::Pose target_pos;
-		target_pos.position.x = 0;
-		target_pos.position.y = 0;
-		target_pos.position.z = 0;
-		target_pos.orientation.x = 0;
-		target_pos.orientation.y = 0;
-		target_pos.orientation.z = 0;
-		target_pos.orientation.w = 1;
-		return target_pos; //TODO: better way to handle errors? This will just time out the server
+		return {};
 	}
 
 	// Timestamps in path are relative to when they were created.
@@ -84,7 +81,7 @@ geometry_msgs::Pose PathFollower::run(double &total_distance_travelled, int &cur
 	const ros::Time current_time = ros::Time::now() - start_time_offset_ + ros::Duration(path_.poses[0].header.stamp.toSec());
 
 	const size_t last_index = num_waypoints_ - 1;
-	double final_x, final_y, final_orientation;
+	double final_x, final_y, final_x_velocity, final_y_velocity, final_orientation, final_velocity_orientation;
 
 	// Find point in path closest to odometry reading
 	ROS_INFO_STREAM("num_waypoints = " << num_waypoints_);
@@ -109,18 +106,44 @@ geometry_msgs::Pose PathFollower::run(double &total_distance_travelled, int &cur
 			path_.poses[index - 1].pose.position.y,
 			path_.poses[index].pose.position.y,
 			current_time.toSec());
+
+	final_x_velocity = interpolate(velocities_.poses[index - 1].header.stamp.toSec(),
+			velocities_.poses[index].header.stamp.toSec(),
+			// The position here is actually a velocity 
+			velocities_.poses[index - 1].pose.position.x, 
+			velocities_.poses[index].pose.position.x,
+			current_time.toSec());
+
+	final_y_velocity = interpolate(velocities_.poses[index - 1].header.stamp.toSec(),
+			velocities_.poses[index].header.stamp.toSec(),
+			// The position here is actually a velocity 
+			velocities_.poses[index - 1].pose.position.y, 
+			velocities_.poses[index].pose.position.y,
+			current_time.toSec());
+
 	final_orientation = interpolate(path_.poses[index - 1].header.stamp.toSec(),
 			path_.poses[index].header.stamp.toSec(),
 			getYaw(path_.poses[index - 1].pose.orientation),
 			getYaw(path_.poses[index].pose.orientation),
 			current_time.toSec());
+	
+	final_velocity_orientation = interpolate(velocities_.poses[index - 1].header.stamp.toSec(),
+			velocities_.poses[index].header.stamp.toSec(),
+			getYaw(velocities_.poses[index - 1].pose.orientation),
+			getYaw(velocities_.poses[index].pose.orientation),
+			current_time.toSec());
 
 	ROS_INFO_STREAM("drive to coordinates: " << index << " (" << final_x << ", " << final_y << ", " << final_orientation << ")");
+	// We're not outputting infor about velocity just because I don't know how we would want that formatted. We definitely should though
+	// TODO: Output velocity information
 
 	// This is strictly for the debugging ROS_INFO below
 	double now_x;
 	double now_y;
+	double now_x_velocity;
+	double now_y_velocity;
 	double now_orientation;
+	double now_velocity_orientation;
 	if (now_index > 0)
 	{
 		now_x = interpolate(path_.poses[now_index - 1].header.stamp.toSec(),
@@ -133,24 +156,48 @@ geometry_msgs::Pose PathFollower::run(double &total_distance_travelled, int &cur
 				path_.poses[now_index - 1].pose.position.y,
 				path_.poses[now_index].pose.position.y,
 				current_time.toSec());
+		now_x_velocity = interpolate(velocity_.poses[now_index - 1].header.stamp.toSec(),
+				velocity_.poses[now_index].header.stamp.toSec(),
+				velocity_.poses[now_index - 1].pose.position.x,
+				velocity_.poses[now_index].pose.position.x,
+				current_time.toSec());
+		now_y_velocity = interpolate(velocity_.poses[now_index - 1].header.stamp.toSec(),
+				velocity_.poses[now_index].header.stamp.toSec(),
+				velocity_.poses[now_index - 1].pose.position.y,
+				velocity_.poses[now_index].pose.position.y,
+				current_time.toSec());	
+
 		now_orientation = interpolate(path_.poses[now_index - 1].header.stamp.toSec(),
 				path_.poses[now_index].header.stamp.toSec(),
 				getYaw(path_.poses[now_index - 1].pose.orientation),
 				getYaw(path_.poses[now_index].pose.orientation),
+				current_time.toSec());
+
+		now_velocity_orientation = interpolate(velocity_.poses[now_index - 1].header.stamp.toSec(),
+				velocity_.poses[now_index].header.stamp.toSec(),
+				getYaw(velocity_.poses[now_index - 1].pose.orientation),
+				getYaw(velocity_.poses[now_index].pose.orientation),
 				current_time.toSec());
 	}
 	else
 	{
 		now_x = path_.poses[0].pose.position.x;
 		now_y = path_.poses[0].pose.position.y;
+		now_x_velocity = velocity_.poses[0].pose.position.x;
+		now_y_velocity = velocity_.poses[0].pose.position.y;
 		now_orientation = getYaw(path_.poses[0].pose.orientation);
+		now_velocity_orientation = getYaw(velocity_.poses[0].pose.orientation);
 	}
 
 	ROS_INFO_STREAM("now coordinates: " << now_index << " (" << now_x << ", " << now_y << ", " << now_orientation << ")");
+	// Again, no info yet for reason stated above
 
 	// Convert back to quaternion
 	tf2::Quaternion q_final_tf = tf2::Quaternion(tf2Scalar(0), tf2Scalar(0), tf2Scalar(final_orientation));
 	geometry_msgs::Quaternion q_final = tf2::toMsg(q_final_tf);
+
+	tf2::Quaternion q_final_tf_velocity = tf2::Quaternion(tf2Scalar(0), tf2Scalar(0), tf2Scalar(final_velocity_orientation));
+	geometry_msgs::Quaternion q_final_velocity = tf2::toMsg(q_final_tf_velocity);
 
 	// Return Pose of target position
 	geometry_msgs::Pose target_pos;
@@ -159,10 +206,20 @@ geometry_msgs::Pose PathFollower::run(double &total_distance_travelled, int &cur
 	target_pos.position.z = 0;
 	target_pos.orientation = q_final;
 
+	geometry_msgs::Pose target_velocity;
+	target_pos.position.x = final_x_velocity;
+	target_pos.position.y = final_y_velocity;
+	target_pos.position.z = 0;
+	target_pos.orientation = q_final_velocity;
+
+	PositionVelocity target;
+	target.position = target_pos;
+	target.velocity = target_velocity;
+
 	// Should actually return total distance now
 	total_distance_travelled = vec_path_length_[now_index - 1];
 	current_index = now_index - 1;
 	ROS_INFO_STREAM("Successfully returned target position and orientation");
 
-	return target_pos;
+	return target;
 }
