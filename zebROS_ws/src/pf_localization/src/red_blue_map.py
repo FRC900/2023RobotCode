9e191b248a1d270a4c7076a6401cb1a3773d156f
Tealchopper42@gmail.com <Tealchopper42@gmail.com>
2024-01-22 18:47:49 -0500

    Basic Mapping Code

---
+++ zebROS_ws/src/pf_localization/src/red_blue_map.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3  
+import rospy
+
+# Because of transformations
+
+import tf2_ros
+import geometry_msgs.msg
+
+
+rospy.init_node('tf2_turtle_broadcaster')
+map_broadcaster = tf2_ros.StaticTransformBroadcaster()
+t = geometry_msgs.msg.TransformStamped()
+
+t.header.stamp = rospy.Time.now()
+t.header.frame_id = "blue_map"
+t.child_frame_id = "red_map"
+t.transform.translation.x = 0
+t.transform.translation.y = 0
+t.transform.translation.z = 0.0
+t.transform.rotation.x = 0
+t.transform.rotation.y = 0
+t.transform.rotation.z = 0
+t.transform.rotation.w = 1
+
+map_broadcaster.sendTransform(t)
+
+rospy.spin()
