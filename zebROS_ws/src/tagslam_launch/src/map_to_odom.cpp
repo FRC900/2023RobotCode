6abbed3a0e81e5baec96472be744c97fbe4e53e4
Torchtopher <torchtopher@gmail.com>
2024-01-23 13:52:04 -0500

    map relative path following

--- zebROS_ws/src/tagslam_launch/src/map_to_odom.cpp
+++ zebROS_ws/src/tagslam_launch/src/map_to_odom.cpp
@@ -7,6 +7,7 @@
 #include <ros/ros.h>
 #include <vector>
 
+#include <std_srvs/Empty.h>
 
 
 namespace tf2
@@ -51,8 +52,10 @@ std::string tagslam_baselink = "frc_robot";
 std::unique_ptr<tf2_ros::TransformBroadcaster> tfbr;
 ros::Duration tf_tolerance = ros::Duration(0.1);
 tf2_ros::Buffer tf_buffer;
+geometry_msgs::TransformStamped map_odom_tf;
 
-void timerCallback(const ros::TimerEvent &event) {
+
+void updateMapOdomTf() {
     // lookup odom->base_link transform
     geometry_msgs::TransformStamped base_link_to_odom_tf;
     // lookup map->base_link transform
@@ -118,23 +121,39 @@ void timerCallback(const ros::TimerEvent &event) {
       tf2::Transform odom_to_map_tf;
       tf2::convert(odom_to_map.pose, odom_to_map_tf);
       tf2::convert(odom_to_map_tf.inverse(), transformStamped.transform);
-
-      tfbr->sendTransform(transformStamped);
+      map_odom_tf = transformStamped;
+      //tfbr->sendTransform(transformStamped);
     } catch (const tf2::TransformException &ex) {
       ROS_ERROR_STREAM_THROTTLE(5, "Line 99 Map to odom : transform from base_link to " << odom_frame_id << " failed in map->odom broadcaser : " << ex.what());
     }
-
 }
 
+void timerCallback(const ros::TimerEvent &event) {
+  map_odom_tf.header.stamp = ros::Time::now();
+  tfbr->sendTransform(map_odom_tf);
+  ROS_WARN_STREAM_THROTTLE(2, "Publishing map odom tf in timer");
+}
+
+bool service_cb(std_srvs::Empty::Request &/*req*/, std_srvs::Empty::Response &/*res*/) {
+  updateMapOdomTf();
+  tfbr->sendTransform(map_odom_tf);
+  ROS_WARN_STREAM("Publishing map odom tf in service");
+  return true;
+}
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "map_to_odom_node");
   ros::NodeHandle nh_;
   tf2_ros::TransformListener tf_listener(tf_buffer);
+  static tf2_ros::StaticTransformBroadcaster static_broadcaster;
 
+  static_broadcaster.sendTransform(static_transformStamped);
+  map_odom_tf.transform.rotation.w = 1; 
   tfbr = std::make_unique<tf2_ros::TransformBroadcaster>();
   // write a timer that gets called at 25Hz
-  ros::Timer timer = nh_.createTimer(ros::Duration(0.04), timerCallback);
+  ros::Timer timer = nh_.createTimer(ros::Duration(0.1), timerCallback);
+  // write a service that takes in an empty message and publishes the tf
+  ros::ServiceServer service = nh_.advertiseService("tagslam_pub_map_to_odom", service_cb);
   ros::spin();
 
   return 0;
