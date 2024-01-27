72c1c752276578c66bfc426a4ae23c2cb085f482
Tealchopper42@gmail.com <Tealchopper42@gmail.com>
2024-01-27 09:34:59 -0500

    fixed issues with if statements

--- zebROS_ws/src/pf_localization/src/map_to_alliance_origin.py
+++ zebROS_ws/src/pf_localization/src/map_to_alliance_origin.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 
 import rospy
+import geometry_msgs
 from frc_msgs.msg import MatchSpecificData
-import red_to_blue_map.py
-import blue_to_red_map.py
+import tf2_ros
 
-def callback(MatchSpecificData):
-    if MatchSpecificData.allianceColor == -1:
-        if MatchSpecificData.allianceColor == 1:
-            t.child_frame_id = "blue_map"
-            t.transform.translation.x = 0
-            t.transform.translation.y = 0
-            t.transform.translation.z = 0
-            t.transform.rotation.x = 0
-            t.transform.rotation.y = 0
-            t.transform.rotation.z = 0
-            t.transform.rotation.w = 1
-        if MatchSpecificData.allianceColor == 0:
-            t.child_frame_id = "red_map"
-            t.transform.translation.x = 16.541
-            t.transform.translation.y = 0
-            t.transform.translation.z = 0.0
-            t.transform.rotation.x = 0
-            t.transform.rotation.y = 1
-            t.transform.rotation.z = 0
-            t.transform.rotation.w = 0
+rospy.init_node('map_chooser', anonymous=True) #Initializes the node
+
+map_broadcaster = tf2_ros.StaticTransformBroadcaster() #defines the broadcaster as map_broadcaster
+t = geometry_msgs.msg.TransformStamped()
+t.header.stamp = rospy.Time.now()
+t.header.frame_id = "map"
+
+def callback(msg):
+    
+    #rotates and moves the map if the aliance color is blue
+    if msg.allianceColor == 1:
+        t.child_frame_id = "blue_map"
+        t.transform.translation.x = 0
+        t.transform.translation.y = 0
+        t.transform.translation.z = 0
+        t.transform.rotation.x = 0
+        t.transform.rotation.y = 0
+        t.transform.rotation.z = 0
+        t.transform.rotation.w = 1
+
+    #rotates and moves the map if the alliance color is red
+    elif msg.allianceColor == 0:
+        t.child_frame_id = "red_map"
+        t.transform.translation.x = 16.541
+        t.transform.translation.y = 0
+        t.transform.translation.z = 0.0
+        t.transform.rotation.x = 0
+        t.transform.rotation.y = 1
+        t.transform.rotation.z = 0
+        t.transform.rotation.w = 0
+    
+    map_broadcaster.sendTransform(t) #sends the transform
+
+rospy.Subscriber('/frcrobot_rio/match_data', MatchSpecificData, callback) #subscribes to MatchSpecificData
+rospy.spin()
