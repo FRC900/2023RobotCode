f6e0915170c34717b3f54aeaafe49d3875b8d72d
cienors <nathanlibutan37@gmail.com>
2024-01-27 15:54:27 -0500

    using goal definitons in py file

--- zebROS_ws/src/behaviors/src/2024_shooter_server.py
+++ zebROS_ws/src/behaviors/src/2024_shooter_server.py
@@ -14,11 +14,32 @@ global left_joint_control_velocity
 global right_joint_velocity
 global right_joint_control_velocty
 
+_goal = Shooter2024Goal()
 _feedback = Shooter2024Feedback()
+
+
+
+def callback(data):
+    global left_joint_velocity
+    global left_joint_control_velocity
+    global right_joint_velocity
+    global right_joint_control_velocty
+    for i in range(len(data.name)):
+        if (data.name[i] == "left_joint"):
+            left_joint_velocity = data.velocity[i]
+            left_joint_control_velocity = data.control_velocity[i]
+        elif (data.name[i] == "right_joint"):
+            right_joint_velocity = data.velocity[i]
+            right_joint_control_velocty = data.control_velocity[i]
+    return
 
 
 class ShooterServer2024:
     def __init__(self):
+
+
+        
+
         self.server = actionlib.SimpleActionServer('shoot_notes', Shooter2024Action, self.execute_cb, auto_start = False)
         #figure out how client stuff works
         rospy.Subscriber('/frcrobot_jetson/talonfxpro_states', TalonFXProState, callback)
@@ -32,6 +53,10 @@ class ShooterServer2024:
         global left_joint_control_velocity
         global right_joint_velocity
         global right_joint_control_velocty
+
+        #values are taken from the callback function of the subscriber
+        #_goal.left_shooter_speed
+        #_goal.right_shooter_speed
         
         
         if (((left_joint_velocity / left_joint_control_velocity)  >= .95) and ((right_joint_velocity / right_joint_control_velocty) >= 1.05)):
@@ -39,36 +64,12 @@ class ShooterServer2024:
             #log speed
             #roslog.info
             rospy.loginfo("Shooter Velocity is within 5 percent difference of set value")
+            rospy.loginfo("Set control velocity on left: %s Set control velocity on right: %s" % (_goal.left_shooter_speed, _goal.right_shooter_speed))
             rospy.loginfo("Logging left_joint_velocity: %s Logging right_joint_velocity: %s" % (left_joint_velocity,  right_joint_velocity))
             _feedback.is_shooting_at_speed = True
 
 
             #set feedback to true because velocity vs control is within error margin
-
-
-
-        #action left shooter speeed and right shooter speed are asccessible via goal.left ...
-
-        
-    
-    def callback(data):
-        global left_joint_velocity
-        global left_joint_control_velocity
-        global right_joint_velocity
-        global right_joint_control_velocty
-        for i in range(len(data.name)):
-            if (data.name[i] == "left_joint"):
-                left_joint_velocity = data.velocity[i]
-                left_joint_control_velocity = data.control_velocity[i]
-            elif (data.name[i] == "right_joint"):
-                right_joint_velocity = data.velocity[i]
-                right_joint_control_velocty = data.control_velocity[i]
-        return
-    
-
-
-
-
 
 
 
