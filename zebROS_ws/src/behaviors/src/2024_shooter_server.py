2fd855391629b30dc610681fe3971f116eab2a4a
cienors <nathanlibutan37@gmail.com>
2024-01-27 22:27:44 -0500

    shooter server now publishes input to the motors based on the client inputs for left and right shooter speed, though percentage values are not present yet

--- zebROS_ws/src/behaviors/src/2024_shooter_server.py
+++ zebROS_ws/src/behaviors/src/2024_shooter_server.py
@@ -1,5 +1,4 @@
 #! /usr/bin/env python
-# not the actual shooter controller, but probably the fastest way to get running
 
 import roslib
 import rospy
@@ -9,10 +8,17 @@ from talon_state_msgs.msg import TalonFXProState
 from behavior_actions.msg import Shooter2024Action, Shooter2024Goal, Shooter2024Feedback
 
 
+
+#todo: Figure out how to set acutal motors to the control velocity given? Probably involves pid tuning from the inputs given to said motors
+#figure out the pivot position angles etc 
+
+
 global left_joint_velocity
 global left_joint_control_velocity
 global right_joint_velocity
 global right_joint_control_velocty
+global left_joint_shooter_pub
+global right_joint_shooter_pub
 
 _goal = Shooter2024Goal()
 _feedback = Shooter2024Feedback()
@@ -25,24 +31,46 @@ def callback(data):
     global right_joint_velocity
     global right_joint_control_velocty
     for i in range(len(data.name)):
-        if (data.name[i] == "left_joint"):
+        if (data.name[i] == "left_joint_shooter_motor"):     #pretty sure its just the joint name
             left_joint_velocity = data.velocity[i]
             left_joint_control_velocity = data.control_velocity[i]
-        elif (data.name[i] == "right_joint"):
-            right_joint_velocity = data.velocity[i]
+        elif (data.name[i] == "right_joint_shooter_motor"):
+            right_joint_velocity = data.velocity[i]    #pretty sure its just hte point name as well should make these more descriptive
             right_joint_control_velocty = data.control_velocity[i]
     return
 
 
 class ShooterServer2024:
     def __init__(self):
+        self.server = actionlib.SimpleActionServer('shoot_notes', Shooter2024Action, self.execute_cb, auto_start = False)
+        global left_joint_shooter_pub
+        global right_joint_shooter_pub
 
+        #figure out how client stuff works
+        rospy.Subscriber('/frcrobot_jetson/talonfxpro_states', TalonFXProState, callback)
+        left_joint_shooter_pub = rospy.Publisher("/frcrobot_jetson/left_shooter_voltage_velocity_controller/command", std_msgs.msg.Float64, queue_size=1)
+        right_joint_shooter_pub = rospy.Publisher("/frcrobot_jetson/right_shooter_voltage_velocity_controller/command", std_msgs.msg.Float64, queue_size=1)
+        #TALON_SHOT
+        #rospy.loginfo(f"Left speed {left_speed}, Right speed {right_speed}, percent {percent}")
+        #_goal.left_shooter_speed
+        #_goal.right_shooter_speed
+        # So setting the left and right publishing...    
+        # left_joint_shooter_pub.publish(std_msgs.msg.Float64(_goal.left_shooter_speed))
+        # right_joint_shooter_pub.publish(std_msgs.msg.FLoat64(_goal.right_shooter_speed))
+        #
+        #left_pub.publish(std_msgs.msg.Float64(left_speed))
+        #right_pub.publish(std_msgs.msg.Float64(right_speed))
 
         
 
-        self.server = actionlib.SimpleActionServer('shoot_notes', Shooter2024Action, self.execute_cb, auto_start = False)
-        #figure out how client stuff works
-        rospy.Subscriber('/frcrobot_jetson/talonfxpro_states', TalonFXProState, callback)
+
+        #TALON_SHOT
+
+
+
+
+        #left_joint_shooter_motor = rospy.Publisher("/frcrobot_jetson/left_shooter_voltage_velocity_controller/command", std_msgs.msg.Float64, queue_size=1)
+        #right_joint_shooter_motor = rospy.Publisher("/frcrobot_jetson/right_shooter_voltage_velocity_controller/command", std_msgs.msg.Float64, queue_size=1)
 
 
         
@@ -53,10 +81,18 @@ class ShooterServer2024:
         global left_joint_control_velocity
         global right_joint_velocity
         global right_joint_control_velocty
+        global left_joint_shooter_pub
+        global right_joint_shooter_pub
 
         #values are taken from the callback function of the subscriber
         #_goal.left_shooter_speed
         #_goal.right_shooter_speed
+        
+        #inside cb, use the passed goal values and set them to the motor values? though is this a client interaction or a server interaction?
+        #like, when we send our goal to the ros thingy, like do we sned goal values thoruhg hte client or this server?
+
+        left_joint_shooter_pub.publish(std_msgs.msg.Float64(_goal.left_shooter_speed))
+        right_joint_shooter_pub.publish(std_msgs.msg.Float64(_goal.right_shooter_speed))
         
         
         if (((left_joint_velocity / left_joint_control_velocity)  >= .95) and ((right_joint_velocity / right_joint_control_velocty) >= 1.05)):
@@ -70,12 +106,6 @@ class ShooterServer2024:
 
 
             #set feedback to true because velocity vs control is within error margin
-
-
-
-
-
-
 
     
 if __name__ == '__main__':
