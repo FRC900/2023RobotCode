42c2d2e34246627814ebb77017a42e679d5afcdf
EliasA0123 <elias.adali@gmail.com>
2024-01-30 20:12:54 -0500

    logic set up except for specifics of reading when switch is pressed

---
+++ zebROS_ws/src/behaviors/src/2024_claw_server.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import rospy
+
+import actionlib
+
+from behavior_actions.msg import Claw2024Feedback, Claw2024Result, Claw2024Goal, Claw2024Action
+from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64
+
+from time import sleep
+
+switch = 0
+class Claw2024ActionServer(object):
+    # create messages that are used to publish feedback/result
+    _result = Claw2024Result()
+
+    def __init__(self, name):
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(self._action_name, Claw2024Action, execute_cb=self.execute_cb, auto_start = False)
+        self._as.start()
+
+    def execute_cb(self, goal: Claw2024Goal):
+        pct_out = Float64()
+        if goal.mode == goal.INTAKE_CLAW:
+            pct_out.data = intake_speed
+            claw_pub.publish(pct_out)
+            while switch == 0:
+                rospy.Subscriber("/frcrobot_rio/joint_states", JointState, callback)
+
+            pct_out.data = 0
+            claw_pub.publish(pct_out)
+            self._result.has_game_piece = True
+
+        else:
+            pct_out.data = outtake_speed
+            claw_pub.publish(pct_out)
+            while switch != 0:
+                rospy.Subscriber("/frcrobot_rio/joint_states", JointState, callback)
+
+            sleep(delay)
+            pct_out.data = 0
+            claw_pub.publish(pct_out)
+            self._result.has_game_piece = False
+        
+        rospy.loginfo('%s: Succeeded' % self._action_name)
+        self._as.set_succeeded(self._result)
+
+def callback(data):
+    global switch
+    switch = data.position[data.name.index("claw_limit_switch")]  # Or whatever actually says when it's pressed
+
+if __name__ == '__main__':
+    rospy.init_node('claw_server_2024')
+    intake_speed = rospy.get_param("intake_speed")
+    outtake_speed = rospy.get_param("outtake_speed")
+    delay = rospy.get_param("outtake_stop_delay")
+    #intake_speed = 1
+    #outtake_speed = 1
+    #delay = 0.5
+    claw_pub = rospy.Publisher("/frcrobot_jetson/claw_controller/command", Float64, queue_size=1)
+    server = Claw2024ActionServer(rospy.get_name())
+    rospy.spin()
