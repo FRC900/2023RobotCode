5fa4567ca873da3e440bac9d40feef95c6f0cad7
Adele McS <adelemcshane@Adeles-MacBook-Air.local>
2024-02-05 19:46:36 -0500

    oopsies two file deleted one renamed the other

--- zebROS_ws/src/behaviors/src/2024_move_arm_server.py
+++ zebROS_ws/src/behaviors/src/2024_move_arm_server.py
@@ -3,14 +3,14 @@
 # Importing stuff, self-explanatory, but I'll explain it anyway because I'm bored :D
 import rospy # If you don't know what this is, either you're on mechanical or VERY behind
 import actionlib # Lets us do server stuff
-from behavior_actions import Arm, ArmGoal, ArmFeedback, ArmResult # The ".msg" of action servers
+from behavior_actions.msg import Arm2024Action, Arm2024Goal, Arm2024Feedback, Arm2024Result # The ".msg" of action servers
 from std_msgs.msg import Float64
 import math
 
 class ArmAction(): # Creates ArmAction class
     # Defining our feedback and result stuff (Don't ask why there's an underscore at the front because I don't know either)
-    _feedback = ArmFeedback()
-    _result = ArmResult()
+    _feedback = Arm2024Feedback()
+    _result = Arm2024Result()
 
 
     def __init__(self, name):
@@ -19,7 +19,7 @@ class ArmAction(): # Creates ArmAction class
         self.trap_position = rospy.get_param("trap_position")
         self.pub = rospy.Publisher('/frcrobot_jetson/arm_controller/command', Float64, queue_size=1)
         self._action_name = name # Give it a name, used as a namespace (For people confuse [like me :D], a namespace is pretty much a dictionary that stores different stuff)
-        self._as = actionlib.SimpleActionServer(self._action_name, Arm.ArmAction, execute_cb=self.execute_cb, auto_start = False) # Create the Action Server, and set autostart to False bc ros says so
+        self._as = actionlib.SimpleActionServer(self._action_name, Arm2024Action, execute_cb=self.execute_cb, auto_start = False) # Create the Action Server, and set autostart to False bc ros says so
         self._as.start() # "Start up" the server 
 
         
@@ -30,7 +30,7 @@ class ArmAction(): # Creates ArmAction class
         success = True
 
         self._feedback.percent_complete = 0 # Set the initial feedback (percent_complete) equal to 0
-        rospy.loginfo(f"{self._action_name}: Executing. Moving arm to {ArmGoal}") # This will give info to the person running the server of whats going on
+        rospy.loginfo(f"{self._action_name}: Executing. Moving arm to {Arm2024Goal}") # This will give info to the person running the server of whats going on
         
         """
         I'm supposed to be ✨moving the motor✨ here
@@ -65,6 +65,8 @@ class ArmAction(): # Creates ArmAction class
 
 # Run everything
 if __name__ == '__main__': 
-    rospy.init_node('move_arm_server_2024', anonymous=True) # Initiates the node (wow really?)
+    rospy.init_node('2024_move_arm_server', anonymous=True) # Initiates the node (wow really?)
     server = ArmAction(rospy.get_name()) # Pretty much ros finds the name of the code automagically
     rospy.spin() # SpInNnNn (A fancy "while: True" loop)
+
+#testing
