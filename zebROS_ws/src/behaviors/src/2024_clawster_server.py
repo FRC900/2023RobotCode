15b7b0c5236569479ccdb4474695e9ae36a37afb
Nathan Libutan <91843333+NathanLibutan@users.noreply.github.com>
2024-02-20 11:13:13 -0500

    Preshooter 678 (#131)
    * lab changes before leaving
    
    * claw and preshooter -> clawster changes, as of now works in sim
    
    * some cleanup, works in sim
    
    ---------
    
    Co-authored-by: cienors <nathanlibutan37@gmail.com>
    Co-authored-by: BenBean18 <beakerthespeaker@gmail.com>

--- zebROS_ws/src/behaviors/src/2024_clawster_server.py
+++ zebROS_ws/src/behaviors/src/2024_clawster_server.py
@@ -5,26 +5,26 @@ import actionlib
 
 from ddynamic_reconfigure_python.ddynamic_reconfigure import DDynamicReconfigure
 
-from behavior_actions.msg import Claw2024Result, Claw2024Goal, Claw2024Action
+from behavior_actions.msg import Clawster2024Result, Clawster2024Goal, Clawster2024Action
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64
 
-class Claw2024ActionServer(object):
+class Clawster2024ActionServer(object):
     # create messages that are used to publish feedback/result
-    _result = Claw2024Result()
+    _result = Clawster2024Result()
 
     def __init__(self, name):
-        self.claw_pub = rospy.Publisher("/frcrobot_jetson/claw_controller/command", Float64, queue_size=1)
+        self.claw_pub = rospy.Publisher(f"/frcrobot_jetson/{rospy.get_param('controller_name')}/command", Float64, queue_size=1)
         self.switch_sub = rospy.Subscriber("/frcrobot_rio/joint_states", JointState, callback)
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, Claw2024Action, execute_cb=self.execute_cb, auto_start = False)
+        self._as = actionlib.SimpleActionServer(self._action_name, Clawster2024Action, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
 
-    def execute_cb(self, goal: Claw2024Goal):
+    def execute_cb(self, goal: Clawster2024Goal):
         pct_out = Float64()
         success = True
         r = rospy.Rate(10)
-        if goal.mode == goal.INTAKE_CLAW:
+        if goal.mode == goal.INTAKE:
             pct_out.data = intake_speed
             self.claw_pub.publish(pct_out)
             while switch == 0 and (not rospy.is_shutdown()):
@@ -64,10 +64,11 @@ class Claw2024ActionServer(object):
 
 def callback(data):
     global switch
-    if "claw_limit_switch" in data.name:
-        switch = data.position[data.name.index("claw_limit_switch")]  # Or whatever actually says when it's pressed
+
+    if switch_name in data.name:
+        switch = data.position[data.name.index(switch_name)]  # Or whatever actually says when it's pressed
     else:
-        rospy.loginfo('Warning: claw_limit_switch not found')
+        rospy.loginfo(f'Warning: {switch_name} not found')
 
 def dyn_rec_callback(config, level):
     rospy.loginfo("Received reconf call: " + str(config))
@@ -80,13 +81,14 @@ def dyn_rec_callback(config, level):
     return config
 
 if __name__ == '__main__':
-    rospy.init_node('claw_server_2024')
+    rospy.init_node('clawster_server_2024')
+    switch_name = rospy.get_param('switch_name')
 
-    ddynrec = DDynamicReconfigure("claw_dyn_rec")
-    ddynrec.add_variable("intake_speed", "float/double variable", rospy.get_param("intake_speed"), 0.0, 13.0)
-    ddynrec.add_variable("outtake_speed", "float/double variable", rospy.get_param("outtake_speed"), 0.0, 13.0)
-    ddynrec.add_variable("delay", "float/double variable", rospy.get_param("outtake_stop_delay"), 0.0, 1.0)
+    ddynrec = DDynamicReconfigure(f"{rospy.get_name()}_dyn_rec")
+    ddynrec.add_variable("intake_speed", "float/double variable", rospy.get_param("intake_speed"), -1.0, 1.0)
+    ddynrec.add_variable("outtake_speed", "float/double variable", rospy.get_param("outtake_speed"), -1.0, 1.0)
+    ddynrec.add_variable("delay", "float/double variable", rospy.get_param("outtake_stop_delay"), 0.0, 2.0)
     ddynrec.start(dyn_rec_callback)
     
-    server = Claw2024ActionServer(rospy.get_name())
+    server = Clawster2024ActionServer(rospy.get_name())
     rospy.spin()
