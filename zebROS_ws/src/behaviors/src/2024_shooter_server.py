9a4aeaa01de1158cfef91b2b2c4c8b83a669d914
BenBean18 <beakerthespeaker@gmail.com>
2024-02-20 19:27:49 -0500

    Shooting server works in simulation

--- zebROS_ws/src/behaviors/src/2024_shooter_server.py
+++ zebROS_ws/src/behaviors/src/2024_shooter_server.py
@@ -1,14 +1,10 @@
-#! /usr/bin/env python3
-
+#!/usr/bin/env python3
 import rospy
 import actionlib
 import std_msgs.msg
 
 from talon_state_msgs.msg import TalonFXProState
 from behavior_actions.msg import Shooter2024Action, Shooter2024Goal, Shooter2024Feedback, Shooter2024Result
-
-
-
 
 global left_joint_velocity
 global right_joint_velocity
@@ -41,22 +37,22 @@ def callback(data):
     else:
         left_joint_velocity = data.velocity[left_joint_index]
         right_joint_velocity = data.velocity[right_joint_index]
-        
-
 
 class ShooterServer2024:
     _result = Shooter2024Result()
     _feedback = Shooter2024Feedback()
+    tolerance = 0.9 # percent
     def __init__(self):
         self.server = actionlib.SimpleActionServer('set_shooter_speed', Shooter2024Action, self.execute_cb, auto_start = False)
-     
 
         #figure out how client stuff works
         rospy.Subscriber('/frcrobot_jetson/talonfxpro_states', TalonFXProState, callback)
         #subscribing here so that we can figure out the actual speed of said motor at a given time, talonfxpro_states gives us these values
         self.left_joint_shooter_pub = rospy.Publisher("/frcrobot_jetson/left_shooter_voltage_velocity_controller/command", std_msgs.msg.Float64, queue_size=1)
         self.right_joint_shooter_pub = rospy.Publisher("/frcrobot_jetson/right_shooter_voltage_velocity_controller/command", std_msgs.msg.Float64, queue_size=1)
-   
+    
+        self.tolerance = rospy.get_param("tolerance")
+
         self.server.start()
         
     def execute_cb(self, goal):
@@ -69,8 +65,6 @@ class ShooterServer2024:
 
         self.left_joint_shooter_pub.publish(std_msgs.msg.Float64(goal.left_shooter_speed))
         self.right_joint_shooter_pub.publish(std_msgs.msg.Float64(goal.right_shooter_speed))
-
-     
         
         while True:
             if rospy.is_shutdown():
@@ -103,25 +97,21 @@ class ShooterServer2024:
                 self._feedback.right_percent_complete = (((right_joint_velocity - initial_right_speed) / (goal.right_shooter_speed - initial_right_speed))) * 100
                 right_percent_difference = (((abs(right_joint_velocity - goal.right_shooter_speed)) / ((right_joint_velocity + goal.right_shooter_speed))) / 2) * 100
 
-            tolerance = .9
             self.server.publish_feedback(self._feedback)
 
             if self.server.is_preempt_requested():
-               self.left_joint_shooter_pub.publish(std_msgs.msg.Float64(goal.left_shooter_speed))
-               self.right_joint_shooter_pub.publish(std_msgs.msg.Float64(goal.right_shooter_speed))
-               self.server.set_preempted()
-               break
-            
+                self.left_joint_shooter_pub.publish(std_msgs.msg.Float64(0))
+                self.right_joint_shooter_pub.publish(std_msgs.msg.Float64(0))
+                self.server.set_preempted()
+                break
 
-            elif ((left_percent_difference < tolerance) and (right_percent_difference < tolerance)):
+            elif ((left_percent_difference < self.tolerance) and (right_percent_difference < self.tolerance)):
                 self._result.success = True
                 self._feedback.left_percent_complete = 100.0
                 self._feedback.right_percent_complete = 100.0
                 self._feedback.is_shooting_at_speed = True
                 self.server.publish_feedback(self._feedback)
                 r.sleep()
-                self.server.set_succeeded(self._result)
-                break
 
 if __name__ == '__main__':
     rospy.init_node('shooter_server_2024')
--- zebROS_ws/src/behaviors/src/2024_shooting_server.py
+++ zebROS_ws/src/behaviors/src/2024_shooting_server.py
@@ -8,7 +8,7 @@ from ddynamic_reconfigure_python.ddynamic_reconfigure import DDynamicReconfigure
 from behavior_actions.msg import Shooting2024Goal, Shooting2024Feedback, Shooting2024Result, Shooting2024Action
 from behavior_actions.msg import Shooter2024Goal, Shooter2024Feedback, Shooter2024Result, Shooter2024Action
 from behavior_actions.msg import ShooterPivot2024Goal, ShooterPivot2024Feedback, ShooterPivot2024Result, ShooterPivot2024Action
-from behavior_actions.msg import Claw2024Goal, Claw2024Feedback, Claw2024Result, Claw2024Action
+from behavior_actions.msg import Clawster2024Goal, Clawster2024Feedback, Clawster2024Result, Clawster2024Action
 from std_msgs.msg import Float64
 from interpolating_map import InterpolatingMap
 
@@ -18,10 +18,16 @@ class ShootingServer(object):
         self.action_name = name
         self.result = Shooting2024Result()
         self.feedback = Shooting2024Feedback()
-        self.shooter_client = actionlib.SimpleActionClient('/shooter/shooter_server_2024', Shooter2024Action)
-        self.pivot_client = actionlib.SimpleActionClient('/pivot/pivot_server_2024', ShooterPivot2024Action)
+        self.shooter_client = actionlib.SimpleActionClient('/shooter/set_shooter_speed', Shooter2024Action)
+        rospy.loginfo("2024_shooting_server: waiting for shooter")
+        self.shooter_client.wait_for_server()
+        self.pivot_client = actionlib.SimpleActionClient('/shooter/set_shooter_pivot', ShooterPivot2024Action)
+        rospy.loginfo("2024_shooting_server: waiting for pivot")
+        self.pivot_client.wait_for_server()
         # The preshooter and claw are very similar (drive motor until limit switch pressed). They'll probably be the same server.
-        self.preshooter_client = actionlib.SimpleActionClient('/preshooter/preshooter_server_2024', Claw2024Action)
+        self.preshooter_client = actionlib.SimpleActionClient('/preshooter/preshooter_server_2024', Clawster2024Action)
+        rospy.loginfo("2024_shooting_server: waiting for preshooter")
+        self.preshooter_client.wait_for_server()
 
         # speeds_map: [[distance: [left_speed, right_speed]], ...]
         speeds_map_param = rospy.get_param("speeds_map")
@@ -34,6 +40,8 @@ class ShootingServer(object):
 
         self.angle_map = InterpolatingMap()
         self.angle_map.container = {l[0]: l[1] for l in rospy.get_param("angle_map")}
+
+        self.delay_after_shooting = rospy.get_param("delay_after_shooting")
 
         self.server = actionlib.SimpleActionServer(self.action_name, Shooting2024Action, execute_cb=self.execute_cb, auto_start = False)
         self.server.start()
@@ -53,7 +61,11 @@ class ShootingServer(object):
         shooter_goal.left_shooter_speed = left_speed
         shooter_goal.right_shooter_speed = right_speed
 
-        self.shooter_client.send_goal(shooter_goal)
+        shooter_done = False
+        def shooter_feedback_cb(feedback: Shooter2024Feedback):
+            nonlocal shooter_done
+            shooter_done = feedback.is_shooting_at_speed
+        self.shooter_client.send_goal(shooter_goal, feedback_cb=shooter_feedback_cb)
 
         rospy.loginfo("2024_shooting_server: pivoting")
 
@@ -63,10 +75,29 @@ class ShootingServer(object):
         pivot_goal = ShooterPivot2024Goal()
         pivot_goal.pivot_position = pivot_angle
 
-        self.pivot_client.send_goal(pivot_goal)
+        pivot_done = False
+        def pivot_done_cb(state, result):
+            nonlocal pivot_done
+            pivot_done = True
+        self.pivot_client.send_goal(pivot_goal, done_cb=pivot_done_cb)
 
-        while self.shooter_client.get_state() 
-        self.pivot_client.wait_for_result()
+        r = rospy.Rate(60.0)
+
+        while not (shooter_done and pivot_done):
+            rospy.loginfo_throttle(0.5, "2024_shooting_server: waiting for shooter and pivot")
+            if self.server.is_preempt_requested():
+                rospy.loginfo("2024_shooting_server: preempted")
+
+                # ensure shooter turned off
+                self.shooter_client.cancel_goals_at_and_before_time(rospy.Time())
+
+                # ensure pivot stopped
+                self.pivot_client.cancel_goals_at_and_before_time(rospy.Time())
+
+                self.server.set_preempted()
+
+                return
+            r.sleep()
 
         rospy.loginfo("2024_shooting_server: shooting")
 
@@ -74,34 +105,57 @@ class ShootingServer(object):
             self.feedback.current_stage = self.feedback.SHOOTING
             self.server.publish_feedback(self.feedback)
 
-            preshooter_goal = Claw2024Goal()
-            preshooter_goal.mode = preshooter_goal.OUTTAKE_CLAW
+            preshooter_goal = Clawster2024Goal()
+            preshooter_goal.mode = preshooter_goal.OUTTAKE
 
             self.preshooter_client.send_goal(preshooter_goal)
 
-            self.preshooter_client.wait_for_result()
+            preshooter_done = False
+            def preshooter_done_cb(state, result):
+                nonlocal preshooter_done
+                preshooter_done = True
+            self.preshooter_client.send_goal(preshooter_goal, done_cb=preshooter_done_cb)
+
+            while not preshooter_done and not rospy.is_shutdown():
+                rospy.loginfo_throttle(0.5, "2024_shooting_server: waiting for preshooter")
+                if self.server.is_preempt_requested():
+                    rospy.loginfo("2024_shooting_server: preempted")
+
+                    # ensure shooter turned off
+                    self.shooter_client.cancel_goals_at_and_before_time(rospy.Time())
+
+                    # ensure pivot stopped
+                    self.pivot_client.cancel_goals_at_and_before_time(rospy.Time())
+
+                    # stop preshooter
+                    self.preshooter_client.cancel_goals_at_and_before_time(rospy.Time())
+
+                    self.server.set_preempted()
+
+                    return
+                r.sleep()
 
             rospy.loginfo("2024_shooting_server: +5 points hopefully")
+        
+        if not goal.leave_spinning:
+            rospy.loginfo("2024_shooting_server: spinning down")
+            rospy.sleep(rospy.Duration(self.delay_after_shooting))
+
+            # ensure shooter turned off
+            self.shooter_client.cancel_goals_at_and_before_time(rospy.Time())
+
+            # ensure pivot stopped
+            self.pivot_client.cancel_goals_at_and_before_time(rospy.Time())
+
+        # stop preshooter
+        self.preshooter_client.cancel_goals_at_and_before_time(rospy.Time())
 
         self.result.success = True
         rospy.loginfo("2024_shooting_server: succeeded")
         self.server.set_succeeded(self.result)
 
-# def dyn_rec_callback(config, level):
-#     rospy.loginfo("Received reconf call: " + str(config))
-#     global conveyor_speed
-#     global diverter_speed
-#     conveyor_speed = config["conveyor_speed"]
-#     diverter_speed = config["diverter_speed"]
-#     return config
-
 if __name__ == '__main__':
     rospy.init_node('shooting_server_2024')
     
-    # ddynrec = DDynamicReconfigure("diverter_dyn_rec")
-    # ddynrec.add_variable("conveyor_speed", "float/double variable", rospy.get_param("note_conveyor_speed"), 0.0, 13.0)
-    # ddynrec.add_variable("diverter_speed", "float/double variable", rospy.get_param("note_diverter_speed"), 0.0, 13.0)
-    # ddynrec.start(dyn_rec_callback)
-
     server = ShootingServer(rospy.get_name())
     rospy.spin()
