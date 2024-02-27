ba3b1d57ef965f25613407e8bfc6101322632b78
May <MejeC10@gmail.com>
2024-02-26 19:33:27 -0500

    Add config files for nodelet (this needs testing)

--- zebROS_ws/src/tf_object_detection/src/screen_to_world_node.cpp
+++ zebROS_ws/src/tf_object_detection/src/screen_to_world_node.cpp
@@ -16,14 +16,12 @@
 
 #include "depth_algorithms.h"
 
-
 int main (int argc, char **argv) {
-	ros::init(argc, argv, "@(node)"); // FIXME I don't know what '@(node)' should be replaced with
+	ros::init(argc, argv, "screen_to_world_node"); // FIXME I don't know what '@(node)' should be replaced with
 	nodelet::Loader nodelet;
 	nodelet::M_string remap(ros::names::getRemappings());
 	nodelet::V_string nargv;
 	std::string nodelet_name = ros::this_node::getName();
-	nodelet.load(nodelet_name, "Package/Nodelet", remap, nargv); // FIXME I don't know what 'Package/Nodelet' should be replaced with
-	ros::spin();
+	nodelet.load(nodelet_name, "tf_object_detection/ScreenToWorld", remap, nargv); // FIXME Is this the right Nodelet name?
 	return 0;
 }
