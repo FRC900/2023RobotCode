3f56b5313d6ca51b0f6490162ae161435d10c48b
Alan An <alananofficial@gmail.com>
2024-03-07 19:06:37 -0500

    rebaed

---
+++ zebROS_ws/src/beginner_tutorials/src/fibonacci_client.py
@@ -0,0 +1,42 @@
+ #! /usr/bin/env python
+
+import rospy
+
+# Brings in the SimpleActionClient
+import actionlib
+
+# Brings in the messages used by the fibonacci action, including the
+# goal message and the result message.
+import beginner_tutorials
+
+def fibonacci_client():
+    # Creates the SimpleActionClient, passing the type of the action
+    # (FibonacciAction) to the constructor.
+    client = actionlib.SimpleActionClient('fibonacci', actionlib_tutorials.msg.FibonacciAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = beginner_tutorials.msg.FibonacciGoal()
+    goal.order = int(input("Enter the order of the Fibonacci sequence: "))
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()  # A FibonacciResult
+
+if __name__ == '__main__':
+    try:
+        # Initializes a rospy node so that the SimpleActionClient can
+        # publish and subscribe over ROS.
+        rospy.init_node('fibonacci_client_py')
+        result = fibonacci_client()
+        print(f"Result:= {result.sequence}")
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion")
