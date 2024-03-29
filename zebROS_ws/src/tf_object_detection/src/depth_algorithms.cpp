0cae3ea7bfb2ad9350f0c44e4a1e6c6ab1b94cbc
Kevin Jaget <kjaget@nc.rr.com>
2024-03-29 18:24:40 -0400

    Revert unintentional ZED2 resolution change Pull in fixes for screen to world depth NAN checking

--- zebROS_ws/src/tf_object_detection/src/depth_algorithms.cpp
+++ zebROS_ws/src/tf_object_detection/src/depth_algorithms.cpp
@@ -41,14 +41,11 @@ float contoursDepthMat(const cv::Mat& depth_, const cv::Rect& bound_rect, bool d
 	cv::Mat depth = depth_(bound_rect);
 
 	// set very large outliers and nan to 0 so they can be removed later. TODO see if the ZED actually reports negative depth
-	float nan_ = std::numeric_limits<float>::quiet_NaN();
 	cv::Mat inf = depth>=900;
 	cv::Mat neg_inf = depth<=-900;
-	cv::Mat nan = depth!=depth;
 	depth.setTo(0, inf);
 	depth.setTo(0, neg_inf);
-	depth.setTo(0, nan);
-	//ROS_INFO_STREAM_THROTTLE(2, "DEPTH MAT\n" << depth);
+	cv::patchNaNs(depth, 0);
 
 	if (debug) {
 		double min, max; // cv::minMaxLoc requires doubles which is why a double is used here. Also, this is only enabled when debug==true.
