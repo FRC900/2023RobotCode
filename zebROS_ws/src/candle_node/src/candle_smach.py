8fd2210a9003492cf2e3ece04c9f679d08d887eb
Gaolynx <phillipsfamily.jonathan@gmail.com>
2024-04-01 23:15:54 +0000

    led changes

--- zebROS_ws/src/candle_node/src/candle_smach.py
+++ zebROS_ws/src/candle_node/src/candle_smach.py
@@ -4,15 +4,27 @@ import rospy
 import smach
 import smach_ros
 import sys
+import time
 from candle_controller_msgs.srv import Colour, ColourRequest, Animation, AnimationRequest
-from time import sleep
+from frc_msgs.msg import MatchSpecificData
+from behavior_actions.msg import AutoMode
+
 
 def team_colour_callback(msg):
-    global disabled, updated
+    global disabled
     
     disabled = msg.disabled
     
     if msg.disabled != disabled:
+        print("Updating auto LEDs...")
+        disabled = msg.disabled
+
+def auto_mode_callback(msg):
+    global auto_mode
+    
+    auto_mode = msg.auto_mode
+    
+    if msg.auto_mode != auto_mode:
         print("Updating auto LEDs...")
         disabled = msg.disabled
 
@@ -48,8 +60,8 @@ def make_animation(speed, start, count, animation_type):
 # This is going to be our default
 
 class team_colour(smach.state):
-    def __init(self):
-        smach.State.__init__(self, outcomes=['green_light', 'rainbow_animation', 'emote_1', 'termination', 'white_light'], input_keys = ['allianceColor', 'auto_mode']) # Outcomes are where we can go from this state, input keys are items we get from our publishers
+    def __init__(self):
+        smach.State.__init__(self, outcomes=['green_light', 'rainbow_animation', 'termination', 'white_light'], input_keys = ['allianceColor', 'auto_mode']) # Outcomes are where we can go from this state, input keys are items we get from our publishers
 
     def execute(self,  message):
         rospy.loginfo('Executing state team_colour')
@@ -59,9 +71,9 @@ class team_colour(smach.state):
             make_colour_obj(0, 6, 0, 0, 255) # Make us blue
         if message.allianceColor == -1: # If we don't know, 
             make_animation(1, 0, 6, 6) # Go crazy (Strobe)
-        if auto_mode < 4: # if auto_mode 1-3,
+        if message.auto_mode < 4: # if auto_mode 1-3,
             return 'rainbow_animation' # Go Rainbow
-        elif auto_mode > 3 # if any other auto mode,
+        elif message.auto_mode > 3: # if any other auto mode,
             return 'white_light' # Go White
 
 # define state green_light
@@ -72,7 +84,7 @@ class green_light(smach.State):
         smach.State.__init__(self, outcomes=['team_colour'])
         
 
-    def execute(self, userdata):
+    def execute(self):
         rospy.loginfo('Executing state green_light, piece acquired')
         make_colour_obj(0, 6, 0, 255, 0) # Green :)
         time.sleep(2) # Leave the green up for a little bit
@@ -85,7 +97,7 @@ class rainbow_animation(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['team_colours'],  input_keys = ['Autonomous', 'Disable'])
 
-    def execute(self, userdata):
+    def execute(self, message):
         rospy.loginfo('Executing state rainbow_animation, prepare for enlightenment')
         while message.Autonomous == True:
             make_animation(1, 0, 6, 3)
@@ -98,7 +110,7 @@ class white_light(smach.State):
         smach.State.__init__(self, outcomes=['team_colour'], input_keys = ['Autonomous'])
         
 
-    def execute(self, userdata):
+    def execute(self, message):
         rospy.loginfo('Executing state white_light, not cool auto :(')
         while message.Autonomous == True:
             make_colour_obj(0, 6, 255, 255, 255) # white :)
@@ -111,8 +123,9 @@ class white_light(smach.State):
 
 def main():
     rospy.init_node('leds_state_machine')
-    rospy.Subscriber("/frcrobot_rio/match_data", MatchSpecificData, )
-    rospy.Subscriber("/auto/auto_mode", AutoMode, )
+    rospy.Subscriber("/frcrobot_rio/match_data", MatchSpecificData, team_colour_callback)
+    rospy.Subscriber("/auto/auto_mode", AutoMode, auto_mode_callback)
+    rospy.Subscriber("/speaker_align/dist_and_ang")
 
     # Create a SMACH state machine
     sm = smach.StateMachine(outcomes=['termination'])
@@ -122,8 +135,8 @@ def main():
         # Add states to the container
         smach.StateMachine.add('team_colour', team_colour(), transitions={'piece_pickup':'green_light', 'vibin':'rainbow_animation', 'doom':'termination'}) # in the format ['name of transition':'actual state it goes to']
         smach.StateMachine.add('green_light', green_light(), transitions={'default':'team_colour'})
-        smach.StateMachine.add('white_light', white_light(), transitions=('default':'team_colour'))
-        smach.StateMachine.add('rainbow_animation', rainbow_animation(), transitions=('default':'team_colour'))
+        smach.StateMachine.add('white_light', white_light(), transitions={'default':'team_colour'})
+        smach.StateMachine.add('rainbow_animation', rainbow_animation(), transitions={'default':'team_colour'})
 
     # Execute SMACH plan
     outcome = sm.execute()
