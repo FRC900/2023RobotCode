813e741956737374aa5b6214d5cbedf34ffc1f68
Kevin Jaget <kjaget@nc.rr.com>
2024-06-01 15:57:28 -0400

    More map_to_odom cleanup
    Use a TransformPublisher rather than a StaticTransformPublisher
    Fix potential int division error in timer period calc
    Remove unused vars

--- zebROS_ws/src/tagslam_launch/src/map_to_odom.cpp
+++ zebROS_ws/src/tagslam_launch/src/map_to_odom.cpp
@@ -1,19 +1,9 @@
+#include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <tf2_ros/static_transform_broadcaster.h>
-#include <sensor_msgs/Imu.h>
-#include <iostream>
-#include <ros/ros.h>
-#include <vector>
 
-#include <std_srvs/Empty.h>
-#include <std_srvs/SetBool.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <std_msgs/Bool.h>
 #include "behavior_actions/RelocalizePoint.h"
-#include "field_obj/Detection.h"
-#include <boost/circular_buffer.hpp>
 
 namespace tf2
 {
@@ -28,7 +18,7 @@ namespace tf2
 
     inline
     void convert(const geometry_msgs::Pose& pose, geometry_msgs::Transform& trans)
-      {
+    {
         trans.rotation = pose.orientation;
         trans.translation.x = pose.position.x;
         trans.translation.y = pose.position.y;
@@ -54,22 +44,15 @@ std::string odom_frame_id = "odom";
 std::string map_frame_id = "map";
 std::string tagslam_baselink = "frc_robot";
 
-std::unique_ptr<tf2_ros::StaticTransformBroadcaster> tfbr;
-ros::Publisher last_relocalized_pub;
+tf2_ros::TransformBroadcaster tfbr;
 
 tf2_ros::Buffer tf_buffer;
-geometry_msgs::TransformStamped map_odom_tf;
-geometry_msgs::TransformStamped relocalized_point_tf;
-
-ros::Subscriber front_tags_sub;
-ros::Subscriber back_tags_sub;
-
-ros::Time last_tf_pub = ros::Time(0);
-
-double transform_timeout;
 
 void updateMapOdomTf() {
-    ros::spinOnce();
+    // This will only be called from a timer event, meaning that we should
+    // already be up to date with the latest tagslam transform
+    // ros::spinOnce();
+
     // lookup odom->base_link transform
     geometry_msgs::TransformStamped base_link_to_odom_tf;
     // lookup map->base_link transform
@@ -86,11 +69,6 @@ void updateMapOdomTf() {
       return;
     }
 
-    geometry_msgs::PoseStamped odom_to_map; // ans
-    geometry_msgs::PoseStamped base_link_to_map;
-    tf2::Transform base_link_to_odom_transform;
-    geometry_msgs::Pose pose;
-
     try {
       // Subtract out odom->base_link from calculated prediction map->base_link,
       // leaving a map->odom transform to broadcast
@@ -101,53 +79,51 @@ void updateMapOdomTf() {
       // odom -> base link
 
       // baselink_to_map transformed from base_link to odom == odom->map
+      geometry_msgs::PoseStamped base_link_to_map;
       tf2::convert(base_link_to_map_tf, base_link_to_map);
       ROS_INFO_STREAM_THROTTLE(2, "Base link to map " << base_link_to_map);
+
+      tf2::Transform base_link_to_odom_transform;
       tf2::convert(base_link_to_odom_tf.transform, base_link_to_odom_transform);
       ROS_INFO_STREAM_THROTTLE(2, "Base link to odom " << base_link_to_odom_tf.transform);
-      //tf2::convert(base_link_to_odom_tf, base_link_to_odom_transform);
-      base_link_to_odom_transform = base_link_to_odom_transform.inverse(); 
 
+      base_link_to_odom_transform = base_link_to_odom_transform.inverse(); 
       tf2::convert(base_link_to_odom_transform, base_link_to_odom_tf.transform);
       ROS_INFO_STREAM_THROTTLE(2, "Base link to odom after inv " << base_link_to_odom_tf.transform);
 
-      //tf_buffer.transform(baselink_to_map, odom_to_map, odom_frame_id);
       // odom to map is out param (supposdly)
-
+      geometry_msgs::PoseStamped odom_to_map; // ans
 	    tf2::doTransform(base_link_to_map, odom_to_map, base_link_to_odom_tf);
       
       geometry_msgs::TransformStamped transformStamped;
-
       transformStamped.header.stamp = base_link_to_map_tf.header.stamp;
       transformStamped.header.frame_id = map_frame_id;
       transformStamped.child_frame_id = odom_frame_id;
 
       // Computed transform is odom->map, but need to invert
       // it to publish map->odom instead
-      // TODO, should zero Z right here
+      // TODO, should zero Z right here?
       tf2::Transform odom_to_map_tf;
       tf2::convert(odom_to_map.pose, odom_to_map_tf);
       tf2::convert(odom_to_map_tf.inverse(), transformStamped.transform);
 
       // ROS_INFO_STREAM("map_to_odom: transform x = " << transformStamped.transform.translation.x << ", y = " << transformStamped.transform.translation.y << ", z = " << transformStamped.transform.translation.z << " at time " << transformStamped.header.stamp << " from " << base_link_to_map_tf.header.stamp << " to " << base_link_to_odom_tf.header.stamp);
 
-      map_odom_tf = transformStamped;
-      //tfbr->sendTransform(transformStamped);
+      tfbr.sendTransform(transformStamped);
     } catch (const tf2::TransformException &ex) {
-      ROS_ERROR_STREAM_THROTTLE(5, "Line 99 Map to odom : transform from base_link to " << odom_frame_id << " failed in map->odom broadcaser : " << ex.what());
+      ROS_ERROR_STREAM_THROTTLE(5, "Line " << __LINE__ << " Map to odom : transform from base_link to " << odom_frame_id << " failed in map->odom broadcaser : " << ex.what());
     }
-    tfbr->sendTransform(map_odom_tf);
 }
 
-void timer_cb(const ros::TimerEvent& event) {
-  last_tf_pub = ros::Time::now();
+void timer_cb(const ros::TimerEvent& /*event*/) {
   updateMapOdomTf();
 }
 
 // Should never have to "fake" localization
 bool relocalize_to_point_cb(behavior_actions::RelocalizePoint::Request &req, behavior_actions::RelocalizePoint::Response &/*res*/) {
   ROS_INFO_STREAM("map_to_odom: relocalizing to a point at x,y " << req.pose.position.x << ", " << req.pose.position.y);
-  
+
+  geometry_msgs::TransformStamped relocalized_point_tf;
   relocalized_point_tf.header.stamp = ros::Time::now();
   relocalized_point_tf.header.frame_id = map_frame_id;
   relocalized_point_tf.child_frame_id = odom_frame_id;
@@ -155,8 +131,7 @@ bool relocalize_to_point_cb(behavior_actions::RelocalizePoint::Request &req, beh
   relocalized_point_tf.transform.translation.x = req.pose.position.x;
   relocalized_point_tf.transform.translation.y = req.pose.position.y;
 
-  tfbr->sendTransform(relocalized_point_tf);
-  last_tf_pub = ros::Time::now();
+  tfbr.sendTransform(relocalized_point_tf);
   return true;
 }
 
@@ -164,21 +139,12 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "map_to_odom_node");
   ros::NodeHandle nh_;
   tf2_ros::TransformListener tf_listener(tf_buffer);
-  tfbr = std::make_unique<tf2_ros::StaticTransformBroadcaster>();
 
-  if (!nh_.getParam("transform_timeout", transform_timeout))
-  {
-    ROS_ERROR_STREAM("map_to_odom: could not find transform_timeout, exiting");
-    return -1;
-  }
-  
-  updateMapOdomTf();
-  if (map_odom_tf.header.frame_id == map_frame_id) {
-    tfbr->sendTransform(map_odom_tf);
-    last_tf_pub = ros::Time::now();
-  }
-  // make a timer that runs at 250hz and updates the tagslam transform 
-  ros::Timer timer = nh_.createTimer(ros::Duration(1/250), timer_cb);
+  double map_to_odom_rate;
+  nh_.param<double>("map_to_odom_rate", map_to_odom_rate, 250.0);
+
+  // make a timer that runs at the requested rate and updates the tagslam transform 
+  ros::Timer timer = nh_.createTimer(ros::Duration(1./map_to_odom_rate), timer_cb);
 
   ros::spin();
 
