2ac6e2b0d56de1a6f4ba9cd8b1ba31e91891c41e
BenBean18 <beakerthespeaker@gmail.com>
2024-10-28 18:38:02 -0400

    code works now

--- zebROS_ws/src/behaviors/src/tutorial_test_server.py
+++ zebROS_ws/src/behaviors/src/tutorial_test_server.py
@@ -22,4 +22,82 @@ def find_joint_state(msg: JointState, name: str) -> float:
             return msg.position[i]
     return None
 
-# Add code here :)
+class TestServer:
+    def __init__(self):
+        self.joint_state_sub = rospy.Subscriber('/frcrobot_jetson/joint_states', JointState, self.joint_state_callback)
+        self.test_motor_client = rospy.ServiceProxy('/frcrobot_jetson/test_motor_controller/command', Command)
+        self.limit_switch_state = False
+
+        self.server = actionlib.SimpleActionServer('tutorial_test', TutorialTestAction, execute_cb=self.execute_cb, auto_start=False)
+        self.server.start()
+
+    def joint_state_callback(self, msg):
+        self.limit_switch_state = find_joint_state(msg, 'diverter_limit_switch') > 0.0
+
+    def turn_off_motor(self):
+        command = CommandRequest()
+        command.command = 0.0
+        response = self.test_motor_client(command)
+
+        # Check if the motor ran successfully
+        return response.success
+
+    def execute_cb(self, goal):
+        # This callback is called when the action server is called
+        # The goal is the input to the action server
+        # The result is what the action server returns
+        # The feedback is what the action server sends back to the client during execution
+
+        # This is the feedback message that we'll be sending back to the client
+        feedback = TutorialTestFeedback()
+        feedback.current_state = feedback.WAITING_FOR_TOGGLE
+
+        self.server.publish_feedback(feedback)
+
+        # This is the result message that we'll be sending back to the client
+        result = TutorialTestResult()
+
+        # Check if the limit switch is toggled
+        r = rospy.Rate(100)
+        initial_state = self.limit_switch_state
+        while initial_state == self.limit_switch_state:
+            r.sleep()
+            if self.server.is_preempt_requested():
+                result.success = False
+                self.server.set_preempted(result)
+                return
+
+        # Update the feedback message
+        feedback.current_state = feedback.RUNNING_MOTOR
+
+        self.server.publish_feedback(feedback)
+
+        # Run the motor at the given output
+        command = CommandRequest()
+        command.command = goal.output
+        response = self.test_motor_client(command)
+
+        # Check if the motor ran successfully
+        if response.success:
+            result.success = True
+        else:
+            result.success = False
+
+        # Wait ten seconds while checking for preempt, then turn off the motor
+        start = rospy.Time.now()
+        while (rospy.Time.now() - start) < rospy.Duration(10):
+            if self.server.is_preempt_requested():
+                self.turn_off_motor()
+                result.success = False
+                self.server.set_preempted(result)
+                return
+
+        result.success = self.turn_off_motor()
+
+        # Send the result back to the client
+        self.server.set_succeeded(result)
+
+if __name__ == '__main__':
+    rospy.init_node('tutorial_test_server', anonymous=True)
+    server = TestServer()
+    rospy.spin()
